f1d819d0a9722b7e0604e4b5ed03a097ab99abf6
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-17 13:54:18 -0600

    Implement diagnostic SA1500 CurlyBracketsForMultiLineStatementsMustNotShareLine (fixes #101)

--- StyleCop.Analyzers/StyleCop.Analyzers/LayoutRules/SA1500CurlyBracketsForMultiLineStatementsMustNotShareLine.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/LayoutRules/SA1500CurlyBracketsForMultiLineStatementsMustNotShareLine.cs
@@ -2,6 +2,8 @@
 {
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     /// <summary>
@@ -56,7 +58,7 @@
         /// </summary>
         public const string DiagnosticId = "SA1500";
         private const string Title = "Curly brackets for multi-line statements must not share line";
-        private const string MessageFormat = "TODO: Message format";
+        private const string MessageFormat = "Curly brackets for multi-line statements must not share line";
         private const string Category = "StyleCop.CSharp.LayoutRules";
         private const string Description = "The opening or closing curly bracket within a C# statement, element, or expression is not placed on its own line.";
         private const string HelpLink = "http://www.stylecop.com/docs/SA1500.html";
@@ -79,7 +81,82 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeAction(HandleNamespaceDeclarationSyntax, SyntaxKind.NamespaceDeclaration);
+            context.RegisterSyntaxNodeAction(HandleClassDeclarationSyntax, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeAction(HandleEnumDeclarationSyntax, SyntaxKind.EnumDeclaration);
+            context.RegisterSyntaxNodeAction(HandleInterfaceDeclarationSyntax, SyntaxKind.InterfaceDeclaration);
+            context.RegisterSyntaxNodeAction(HandleStructDeclarationSyntax, SyntaxKind.StructDeclaration);
+            context.RegisterSyntaxNodeAction(HandleAccessorListSyntax, SyntaxKind.AccessorList);
+            context.RegisterSyntaxNodeAction(HandleBlockSyntax, SyntaxKind.Block);
+        }
+
+        private void HandleNamespaceDeclarationSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((NamespaceDeclarationSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((NamespaceDeclarationSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void HandleClassDeclarationSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((ClassDeclarationSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((ClassDeclarationSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void HandleEnumDeclarationSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((EnumDeclarationSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((EnumDeclarationSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void HandleInterfaceDeclarationSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((InterfaceDeclarationSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((InterfaceDeclarationSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void HandleStructDeclarationSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((StructDeclarationSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((StructDeclarationSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void HandleAccessorListSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((AccessorListSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((AccessorListSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void HandleBlockSyntax(SyntaxNodeAnalysisContext context)
+        {
+            CheckCurlyBracketToken(context, ((BlockSyntax)context.Node).OpenBraceToken);
+            CheckCurlyBracketToken(context, ((BlockSyntax)context.Node).CloseBraceToken);
+        }
+
+        private void CheckCurlyBracketToken(SyntaxNodeAnalysisContext context, SyntaxToken token)
+        {
+            if (token.IsMissing)
+                return;
+
+            Location location = token.GetLocation();
+            int line = location.GetLineSpan().StartLinePosition.Line;
+
+            SyntaxToken previousToken = token.GetPreviousToken();
+            if (!previousToken.IsMissing)
+            {
+                if (previousToken.GetLocation().GetLineSpan().StartLinePosition.Line == line)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, location));
+                    // no need to report more than one instance for this token
+                    return;
+                }
+            }
+
+            SyntaxToken nextToken = token.GetNextToken();
+            if (!nextToken.IsMissing)
+            {
+                if (nextToken.GetLocation().GetLineSpan().StartLinePosition.Line == line)
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, location));
+            }
         }
     }
 }
