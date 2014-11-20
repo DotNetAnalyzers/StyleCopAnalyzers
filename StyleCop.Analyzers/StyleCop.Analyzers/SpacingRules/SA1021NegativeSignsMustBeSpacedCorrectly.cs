f3f52f5a5572b228efe683b09e9a15ac8aed2564
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-20 12:55:40 -0600

    Implement diagnostic SA1021 NegativeSignsMustBeSpacedCorrectly (fixes #27)

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/SpacingRules/SA1021NegativeSignsMustBeSpacedCorrectly.cs
@@ -0,0 +1,116 @@
+﻿namespace StyleCop.Analyzers.SpacingRules
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// A negative sign within a C# element is not spaced correctly.
+    /// </summary>
+    /// <remarks>
+    /// <para>A violation of this rule occurs when the spacing around a negative sign is not correct.</para>
+    ///
+    /// <para>A negative sign should always be preceded by a single space, unless it comes after an opening square
+    /// bracket, a parenthesis, or is the first character on the line.</para>
+    ///
+    /// <para>A negative sign should never be followed by whitespace, and should never be the last character on a
+    /// line.</para>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SA1021NegativeSignsMustBeSpacedCorrectly : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SA1021";
+        internal const string Title = "Negative Signs Must Be Spaced Correctly";
+        internal const string MessageFormat = "Negative sign must{0} be {1} by a space.";
+        internal const string Category = "StyleCop.CSharp.Spacing";
+        internal const string Description = "A negative sign within a C# element is not spaced correctly.";
+        internal const string HelpLink = "http://www.stylecop.com/docs/SA1021.html";
+
+        public static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+
+        private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return _supportedDiagnostics;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxTreeAction(HandleSyntaxTree);
+        }
+
+        private void HandleSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+            foreach (var token in root.DescendantTokens())
+            {
+                switch (token.CSharpKind())
+                {
+                case SyntaxKind.MinusToken:
+                    HandleMinusToken(context, token);
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+
+        private void HandleMinusToken(SyntaxTreeAnalysisContext context, SyntaxToken token)
+        {
+            if (token.IsMissing)
+                return;
+
+            if (!token.Parent.IsKind(SyntaxKind.UnaryMinusExpression))
+                return;
+
+            bool precededBySpace;
+            bool firstInLine;
+            bool followsSpecialCharacter;
+
+            bool followedBySpace;
+            bool lastInLine;
+
+            firstInLine = token.HasLeadingTrivia || token.GetLocation()?.GetMappedLineSpan().StartLinePosition.Character == 0;
+            if (firstInLine)
+            {
+                precededBySpace = true;
+                followsSpecialCharacter = false;
+            }
+            else
+            {
+                SyntaxToken precedingToken = token.GetPreviousToken();
+                precededBySpace = precedingToken.HasTrailingTrivia;
+
+                followsSpecialCharacter =
+                    precedingToken.IsKind(SyntaxKind.OpenBracketToken)
+                    || precedingToken.IsKind(SyntaxKind.OpenParenToken)
+                    || precedingToken.IsKind(SyntaxKind.CloseParenToken);
+            }
+
+            followedBySpace = token.HasTrailingTrivia;
+            lastInLine = token.TrailingTrivia.Any(SyntaxKind.EndOfLineTrivia);
+
+            if (!firstInLine && !followsSpecialCharacter && !precededBySpace)
+            {
+                // Negative sign must{} be {preceded} by a space.
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, token.GetLocation(), string.Empty, "preceded"));
+            }
+
+            if (lastInLine || followedBySpace)
+            {
+                // Negative sign must{ not} be {followed} by a space.
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, token.GetLocation(), " not", "followed"));
+            }
+        }
+    }
+}
