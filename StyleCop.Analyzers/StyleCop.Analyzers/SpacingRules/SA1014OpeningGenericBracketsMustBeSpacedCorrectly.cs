39f8aec42113c21ecb6a1e1af207701cca7d6d8b
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-20 12:52:38 -0600

    Implement diagnostic SA1014 OpeningGenericBracketsMustBeSpacedCorrectly (fixes #20)

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/SpacingRules/SA1014OpeningGenericBracketsMustBeSpacedCorrectly.cs
@@ -0,0 +1,112 @@
+﻿namespace StyleCop.Analyzers.SpacingRules
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// An opening generic bracket within a C# element is not spaced correctly.
+    /// </summary>
+    /// <remarks>
+    /// <para>A violation of this rule occurs when the spacing around an opening generic bracket is not correct.</para>
+    ///
+    /// <para>An opening generic bracket should never be preceded or followed by whitespace, unless the bracket is the
+    /// first or last character on the line.</para>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SA1014OpeningGenericBracketsMustBeSpacedCorrectly : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SA1014";
+        internal const string Title = "Opening Generic Brackets Must Be Spaced Correctly";
+        internal const string MessageFormat = "Opening generic brackets must not be {0} by a space.";
+        internal const string Category = "StyleCop.CSharp.Spacing";
+        internal const string Description = "An opening generic bracket within a C# element is not spaced correctly.";
+        internal const string HelpLink = "http://www.stylecop.com/docs/SA1014.html";
+
+        public static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+
+        private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return _supportedDiagnostics;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxTreeAction(HandleSyntaxTree);
+        }
+
+        private void HandleSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+            foreach (var token in root.DescendantTokens())
+            {
+                switch (token.CSharpKind())
+                {
+                case SyntaxKind.LessThanToken:
+                    HandleLessThanToken(context, token);
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+
+        private void HandleLessThanToken(SyntaxTreeAnalysisContext context, SyntaxToken token)
+        {
+            if (token.IsMissing)
+                return;
+
+            switch (token.Parent.CSharpKind())
+            {
+            case SyntaxKind.TypeArgumentList:
+            case SyntaxKind.TypeParameterList:
+                break;
+
+            default:
+                // not a generic bracket
+                return;
+            }
+
+            bool precededBySpace;
+            bool firstInLine;
+
+            bool followedBySpace;
+
+            firstInLine = token.HasLeadingTrivia || token.GetLocation()?.GetMappedLineSpan().StartLinePosition.Character == 0;
+            if (firstInLine)
+            {
+                precededBySpace = true;
+            }
+            else
+            {
+                SyntaxToken precedingToken = token.GetPreviousToken();
+                precededBySpace = precedingToken.HasTrailingTrivia;
+            }
+
+            followedBySpace = token.HasTrailingTrivia;
+
+            if (!firstInLine && precededBySpace)
+            {
+                // Opening generic brackets must not be {preceded} by a space.
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, token.GetLocation(), "preceded"));
+            }
+
+            if (followedBySpace)
+            {
+                // Opening generic brackets must not be {followed} by a space.
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, token.GetLocation(), "followed"));
+            }
+        }
+    }
+}
