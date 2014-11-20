f40a25e988a1de13fbd63440370ddffce52096ad
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-20 12:54:00 -0600

    Implement diagnostic SA1017 ClosingAttributeBracketsMustBeSpacedCorrectly (fixes #23)

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/SpacingRules/SA1017ClosingAttributeBracketsMustBeSpacedCorrectly.cs
@@ -0,0 +1,89 @@
+﻿namespace StyleCop.Analyzers.SpacingRules
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// A closing attribute bracket within a C# element is not spaced correctly.
+    /// </summary>
+    /// <remarks>
+    /// <para>A violation of this rule occurs when the spacing around a closing attribute bracket is not correct.</para>
+    ///
+    /// <para>A closing attribute bracket should never be preceded by whitespace, unless the bracket is the first
+    /// character on the line.</para>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SA1017ClosingAttributeBracketsMustBeSpacedCorrectly : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SA1017";
+        internal const string Title = "Closing Attribute Brackets Must Be Spaced Correctly";
+        internal const string MessageFormat = "Closing attribute brackets must not be preceded by a space.";
+        internal const string Category = "StyleCop.CSharp.Spacing";
+        internal const string Description = "A closing attribute bracket within a C# element is not spaced correctly.";
+        internal const string HelpLink = "http://www.stylecop.com/docs/SA1017.html";
+
+        public static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+
+        private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return _supportedDiagnostics;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxTreeAction(HandleSyntaxTree);
+        }
+
+        private void HandleSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+            foreach (var token in root.DescendantTokens())
+            {
+                switch (token.CSharpKind())
+                {
+                case SyntaxKind.CloseBracketToken:
+                    HandleCloseBracketToken(context, token);
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+
+        private void HandleCloseBracketToken(SyntaxTreeAnalysisContext context, SyntaxToken token)
+        {
+            if (token.IsMissing)
+                return;
+
+            if (!token.Parent.IsKind(SyntaxKind.AttributeList))
+                return;
+
+            bool hasPrecedingSpace = false;
+            if (!token.HasLeadingTrivia)
+            {
+                // only the first token on the line has leading trivia, and those are ignored
+                SyntaxToken precedingToken = token.GetPreviousToken();
+                if (precedingToken.HasTrailingTrivia)
+                    hasPrecedingSpace = true;
+            }
+
+            if (hasPrecedingSpace)
+            {
+                // Closing attribute brackets must not be preceded by a space.
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, token.GetLocation()));
+            }
+        }
+    }
+}
