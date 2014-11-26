5256b4d9f236730cdcaf370e58b3bcc096e022a6
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-25 21:04:52 -0600

    Add unit tests for SA1021 and SA1022 (fixes #207, #208, #271, #272)

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/SpacingRules/SA1021UnitTests.cs
@@ -0,0 +1,45 @@
+﻿namespace StyleCop.Analyzers.Test.SpacingRules
+{
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StyleCop.Analyzers.SpacingRules;
+
+    [TestClass]
+    public class SA1021UnitTests : NumberSignSpacingTestBase
+    {
+        protected override string DiagnosticId
+        {
+            get
+            {
+                return SA1021NegativeSignsMustBeSpacedCorrectly.DiagnosticId;
+            }
+        }
+
+        protected override string Sign
+        {
+            get
+            {
+                return "-";
+            }
+        }
+
+        protected override string SignName
+        {
+            get
+            {
+                return "Negative";
+            }
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SA1021CodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1021NegativeSignsMustBeSpacedCorrectly();
+        }
+    }
+}
