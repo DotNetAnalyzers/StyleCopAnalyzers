3f1b921630c0aaf3daf333518911b7d93be3cbc6
Sam Harwell <sam@tunnelvisionlabs.com>
2014-12-02 11:25:33 -0600

    Implement diagnostic SA1211 UsingAliasDirectivesMustBeOrderedAlphabeticallyByAliasName (fixes #71)

--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1211UsingAliasDirectivesMustBeOrderedAlphabeticallyByAliasName.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1211UsingAliasDirectivesMustBeOrderedAlphabeticallyByAliasName.cs
@@ -1,7 +1,10 @@
 ﻿namespace StyleCop.Analyzers.OrderingRules
 {
+    using System;
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     /// <summary>
@@ -16,8 +19,8 @@
     public class SA1211UsingAliasDirectivesMustBeOrderedAlphabeticallyByAliasName : DiagnosticAnalyzer
     {
         public const string DiagnosticId = "SA1211";
-        internal const string Title = "The using-alias directives within a C# code file are not sorted alphabetically by alias name.";
-        internal const string MessageFormat = "TODO: Message format";
+        internal const string Title = "Using alias directives must be ordered alphabetically by alias name";
+        internal const string MessageFormat = "Using alias directive for '{0}' must appear before using alias directive for '{1}'";
         internal const string Category = "StyleCop.CSharp.OrderingRules";
         internal const string Description = "The using-alias directives within a C# code file are not sorted alphabetically by alias name.";
         internal const string HelpLink = "http://www.stylecop.com/docs/SA1211.html";
@@ -40,7 +43,46 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeAction(HandleUsingDirectiveSyntax, SyntaxKind.UsingDirective);
+        }
+
+        private void HandleUsingDirectiveSyntax(SyntaxNodeAnalysisContext context)
+        {
+            UsingDirectiveSyntax syntax = context.Node as UsingDirectiveSyntax;
+            if (syntax.Alias?.Name?.IsMissing != false)
+                return;
+
+            CompilationUnitSyntax compilationUnit = syntax.Parent as CompilationUnitSyntax;
+            SyntaxList<UsingDirectiveSyntax>? usingDirectives = compilationUnit?.Usings;
+            if (!usingDirectives.HasValue)
+            {
+                NamespaceDeclarationSyntax namespaceDeclaration = syntax.Parent as NamespaceDeclarationSyntax;
+                usingDirectives = namespaceDeclaration?.Usings;
+            }
+
+            if (!usingDirectives.HasValue)
+                return;
+
+            foreach (var usingDirective in usingDirectives)
+            {
+                // we are only interested in nodes before the current node
+                if (usingDirective == syntax)
+                    continue;
+
+                // only interested in using alias directives
+                if (usingDirective.Alias?.Name?.IsMissing != false)
+                    continue;
+
+                string alias = syntax.Alias.Name.ToString();
+                string precedingAlias = usingDirective.Alias.Name.ToString();
+                if (string.Compare(alias, precedingAlias, StringComparison.OrdinalIgnoreCase) >= 0)
+                    continue;
+
+
+                // Using alias directive for '{alias}' must appear before using alias directive for '{precedingAlias}'
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, syntax.GetLocation(), alias, precedingAlias));
+                break;
+            }
         }
     }
 }
