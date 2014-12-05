ef31132e5bfa2982f0b03bb22ef5160e02e03b85
Sam Harwell <sam@tunnelvisionlabs.com>
2014-12-04 21:18:28 -0600

    Implement diagnostic SA1106 CodeMustNotContainEmptyStatements
    Fixes #40
    Fixes #314

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1106UnitTests.cs
@@ -0,0 +1,186 @@
+﻿namespace StyleCop.Analyzers.Test.ReadabilityRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StyleCop.Analyzers.ReadabilityRules;
+    using TestHelper;
+
+    [TestClass]
+    public class SA1106UnitTests : CodeFixVerifier
+    {
+        private const string DiagnosticId = SA1106CodeMustNotContainEmptyStatements.DiagnosticId;
+
+        [TestMethod]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestEmptyStatementAsBlock()
+        {
+            var testCode = @"
+class TestClass
+{
+    public void TestMethod()
+    {
+        for (int i = 0; i < 10; i++)
+            ;
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Code must not contain empty statements",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 7, 13)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestEmptyStatementInForStatement()
+        {
+            var testCode = @"
+class TestClass
+{
+    public void TestMethod()
+    {
+        for (;;)
+        {
+        }
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestEmptyStatement()
+        {
+            var testCode = @"
+class TestClass
+{
+    public void TestMethod()
+    {
+        ;
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Code must not contain empty statements",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 6, 9)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestLabeledEmptyStatement()
+        {
+            var testCode = @"
+class TestClass
+{
+    public void TestMethod()
+    {
+    label:
+        ;
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestLabeledEmptyStatementFollowedByEmptyStatement()
+        {
+            var testCode = @"
+class TestClass
+{
+    public void TestMethod()
+    {
+    label:
+        ;
+        ;
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Code must not contain empty statements",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 8, 9)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestLabeledEmptyStatementFollowedByNonEmptyStatement()
+        {
+            var testCode = @"
+class TestClass
+{
+    public void TestMethod()
+    {
+    label:
+        ;
+        int x = 3;
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Code must not contain empty statements",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 7, 9)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1106CodeMustNotContainEmptyStatements();
+        }
+    }
+}
--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1106CodeMustNotContainEmptyStatements.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1106CodeMustNotContainEmptyStatements.cs
@@ -2,6 +2,8 @@
 {
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     /// <summary>
@@ -16,13 +18,13 @@
     {
         public const string DiagnosticId = "SA1106";
         private const string Title = "Code must not contain empty statements";
-        private const string MessageFormat = "TODO: Message format";
+        private const string MessageFormat = "Code must not contain empty statements";
         private const string Category = "StyleCop.CSharp.ReadabilityRules";
         private const string Description = "The C# code contains an extra semicolon.";
         private const string HelpLink = "http://www.stylecop.com/docs/SA1106.html";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink, customTags: new[] { WellKnownDiagnosticTags.Unnecessary });
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description, HelpLink, WellKnownDiagnosticTags.Unnecessary);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
             ImmutableArray.Create(Descriptor);
@@ -39,7 +41,38 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeAction(HandleEmptyStatementSyntax, SyntaxKind.EmptyStatement);
+        }
+
+        private void HandleEmptyStatementSyntax(SyntaxNodeAnalysisContext context)
+        {
+            EmptyStatementSyntax syntax = context.Node as EmptyStatementSyntax;
+            if (syntax == null)
+                return;
+
+            LabeledStatementSyntax labeledStatementSyntax = syntax.Parent as LabeledStatementSyntax;
+            if (labeledStatementSyntax != null)
+            {
+                BlockSyntax blockSyntax = labeledStatementSyntax.Parent as BlockSyntax;
+                if (blockSyntax != null)
+                {
+                    for (int i = blockSyntax.Statements.Count - 1; i >= 0; i--)
+                    {
+                        StatementSyntax statement = blockSyntax.Statements[i];
+
+                        // allow an empty statement to be used for a label, but only if no non-empty statements exist
+                        // before the end of the block
+                        if (blockSyntax.Statements[i] == labeledStatementSyntax)
+                            return;
+
+                        if (!statement.IsKind(SyntaxKind.EmptyStatement))
+                            break;
+                    }
+                }
+            }
+
+            // Code must not contain empty statements
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, syntax.GetLocation()));
         }
     }
 }
