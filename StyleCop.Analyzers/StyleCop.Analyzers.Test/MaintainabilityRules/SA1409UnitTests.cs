0a143326fc5e66e7ae5918a472de770104d0db05
Dennis Fischer <fischer_dennis@live.de>
2014-12-27 13:03:07 +0100

    Implemented SA1409 Code Fix

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/MaintainabilityRules/SA1409UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/MaintainabilityRules/SA1409UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using StyleCop.Analyzers.MaintainabilityRules;
 using TestHelper;
@@ -250,9 +251,176 @@ namespace StyleCop.Analyzers.Test.MaintainabilityRules
             await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
         }
 
+        // Code Fixes
+
+        private async Task TestKeywordCodeFixNoFix(string keyword)
+        {
+            string code = @"class Foo
+{
+    public void Bar()
+    {
+        %keyword%
+        {
+            Bar();
+        }
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(code.Replace("%keyword%", keyword), code.Replace("%keyword%", keyword));
+        }
+
+        private async Task TestKeywordCodeFix(string keyword)
+        {
+            string oldSource = @"class Foo
+{
+    public void Bar()
+    {
+        %keyword%
+        {
+
+        }
+    }
+}";
+            string newSource = @"class Foo
+{
+    public void Bar()
+    {
+        
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(oldSource.Replace("%keyword%", keyword), newSource.Replace("%keyword%", keyword));
+        }
+
+        [Fact]
+        public async Task TestUnsafeCodeFixNoFix()
+        {
+            await this.TestKeywordCodeFixNoFix("unsafe");
+        }
+
+        [Fact]
+        public async Task TestUnsafeCodeFix()
+        {
+            await this.TestKeywordCodeFix("unsafe");
+        }
+
+        [Fact]
+        public async Task TestCheckedCodeFixNoFix()
+        {
+            await this.TestKeywordCodeFixNoFix("checked");
+        }
+
+        [Fact]
+        public async Task TestCheckedCodeFix()
+        {
+            await this.TestKeywordCodeFix("checked");
+        }
+
+        [Fact]
+        public async Task TestUncheckedCodeFixNoFix()
+        {
+            await this.TestKeywordCodeFixNoFix("unchecked");
+        }
+
+        [Fact]
+        public async Task TestUncheckedCodeFix()
+        {
+            await this.TestKeywordCodeFix("unchecked");
+        }
+
+        [Fact]
+        public async Task TestStaticConstructorCodeFixNoFix()
+        {
+            string code = @"class Foo
+{
+    static Foo()
+    {
+        System.Console.WriteLine();
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(code, code);
+        }
+
+        [Fact]
+        public async Task TestStaticConstructorCodeFix()
+        {
+            string oldSource = @"class Foo
+{
+    static Foo()
+    {
+
+    }
+}";
+            string newSource = @"class Foo
+{
+    
+}";
+
+            await this.VerifyCSharpFixAsync(oldSource, newSource);
+        }
+
+        [Fact]
+        public async Task TestIfElseCodeFixNoFix()
+        {
+            string code = @"class Foo
+{
+    public void Bar()
+    {
+        if(true)
+        {
+            Bar();
+        }
+        else
+        {
+            Bar();
+        }
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(code, code);
+        }
+
+        [Fact]
+        public async Task TestIfElseCodeFix()
+        {
+            string oldSource = @"class Foo
+{
+    public void Bar()
+    {
+        if(true)
+        {
+            Bar();
+        }
+        else
+        {
+
+        }
+    }
+}";
+            string newSource = @"class Foo
+{
+    public void Bar()
+    {
+        if(true)
+        {
+            Bar();
+        }
+        
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(oldSource, newSource);
+        }
+
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new SA1409RemoveUnnecessaryCode();
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SA1409CodeFixProvider();
         }
     }
 }
