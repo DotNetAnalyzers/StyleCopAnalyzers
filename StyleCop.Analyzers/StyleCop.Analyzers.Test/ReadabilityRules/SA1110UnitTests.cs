e7ee19a0d34582ac1023fe0d9930a3f97ced99cb
Maciej Pakulski <pakulski.maciej@gmail.com>
2015-01-02 16:26:07 +0000

    SA1110 - diagnostics for method/constructor declaration/call

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1110UnitTests.cs
@@ -0,0 +1,347 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StyleCop.Analyzers.ReadabilityRules;
+using StyleCop.Analyzers.SpacingRules;
+using TestHelper;
+
+namespace StyleCop.Analyzers.Test.ReadabilityRules
+{
+    [TestClass]
+    public class SA1110UnitTests : CodeFixVerifier
+    {
+        protected static readonly DiagnosticResult[] EmptyDiagnosticResults = { };
+
+        public string DiagnosticId { get; } = SA1110OpeningParenthesisMustBeOnDeclarationLine.DiagnosticId;
+
+        [TestMethod]
+        public async Task TestEmptySource()
+        {
+            var testCode = @"";
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMethodDeclarationOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Bar
+                    ()
+    {
+
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 5, 21)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestConstructorDeclarationOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+public class Foo
+{
+    public Foo
+        ()
+    {
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 5, 9)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMethodDeclarationOpeningBracketInTheSameLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void 
+                    Bar()
+    {
+
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestConstructorDeclarationOpeningBracketInTheSameLine()
+        {
+            var testCode = @"
+public class Foo
+{
+    public Foo()
+    {
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMethodCallOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Bar()
+    {
+        var s = ToString
+            ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 7, 13)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMethodCallUsingThisOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Bar()
+    {
+        var s = this.ToString
+            ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 7, 13)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMethodCallUsingBaseOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Bar()
+    {
+        var s = base.ToString
+            ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 7, 13)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMethodCallUsingVariableOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Bar()
+    {
+        var name = ""qwe"";
+        var s = name.ToString
+            ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 8, 13)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestStaticMethodCallOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public static void Baz()
+    {
+    }
+
+    public void Bar()
+    {
+        Foo.Baz
+            ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 11, 13)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestConstructorCallOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Bar()
+    {
+        var f = new Foo
+        ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 7, 9)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestConstructorWithQualifiedNameCallOpeningBracketInTheNextLine()
+        {
+            var testCode = @"
+class Foo
+{
+    class FooInner
+    {
+    }
+    public void Bar()
+    {
+        var f = new Foo.FooInner
+        ();
+    }
+}";
+
+            var expected = new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 10, 9)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1110OpeningParenthesisMustBeOnDeclarationLine();
+        }
+    }
+}
--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1110OpeningParenthesisMustBeOnDeclarationLine.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1110OpeningParenthesisMustBeOnDeclarationLine.cs
@@ -1,4 +1,9 @@
-﻿namespace StyleCop.Analyzers.ReadabilityRules
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace StyleCop.Analyzers.ReadabilityRules
 {
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
@@ -30,7 +35,7 @@
     {
         public const string DiagnosticId = "SA1110";
         internal const string Title = "Opening parenthesis must be on declaration line";
-        internal const string MessageFormat = "TODO: Message format";
+        internal const string MessageFormat = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.";
         internal const string Category = "StyleCop.CSharp.ReadabilityRules";
         internal const string Description = "The opening parenthesis or bracket in a call to a C# method or indexer, or the declaration of a method or indexer, is not placed on the same line as the method or indexer name.";
         internal const string HelpLink = "http://www.stylecop.com/docs/SA1110.html";
@@ -53,7 +58,100 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeAction(HandleMethodDeclaration,SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(HandleConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
+            context.RegisterSyntaxNodeAction(HandleInvocationExpression, SyntaxKind.InvocationExpression);
+            context.RegisterSyntaxNodeAction(HandleObjectCreationExpression, SyntaxKind.ObjectCreationExpression);
+        }
+
+        private void HandleObjectCreationExpression(SyntaxNodeAnalysisContext context)
+        {
+            var objectCreation = (ObjectCreationExpressionSyntax) context.Node;
+            var qualifiedNameSyntax = objectCreation.ChildNodes().OfType<QualifiedNameSyntax>().FirstOrDefault();
+            IdentifierNameSyntax identifierNameSyntax = null;
+            if (qualifiedNameSyntax != null)
+            {
+                identifierNameSyntax = qualifiedNameSyntax.DescendantNodes().OfType<IdentifierNameSyntax>().FirstOrDefault();
+            }
+            else
+            {
+                identifierNameSyntax = objectCreation.DescendantNodes().OfType<IdentifierNameSyntax>().FirstOrDefault();
+            }
+
+            if (identifierNameSyntax != null)
+            {
+                if (objectCreation.ArgumentList.OpenParenToken.IsMissing == false &&
+                    identifierNameSyntax.Identifier.IsMissing == false)
+                {
+                    CheckIfLocationOfIdentifierNameAndOpenTokenAreTheSame(context,
+                        objectCreation.ArgumentList.OpenParenToken, identifierNameSyntax.Identifier);
+                }
+            }
+        }
+
+        private void HandleInvocationExpression(SyntaxNodeAnalysisContext context)
+        {
+            var invocationExpression = (InvocationExpressionSyntax) context.Node;
+            var identifierNameSyntax = invocationExpression.DescendantNodes().OfType<IdentifierNameSyntax>().FirstOrDefault();
+            if (identifierNameSyntax != null)
+            {
+                if (invocationExpression.ArgumentList.OpenParenToken.IsMissing == false &&
+                    identifierNameSyntax.Identifier.IsMissing == false)
+                {
+                    CheckIfLocationOfIdentifierNameAndOpenTokenAreTheSame(context,
+                        invocationExpression.ArgumentList.OpenParenToken, identifierNameSyntax.Identifier);
+                }
+            }
+        }
+
+        private void HandleConstructorDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var constructotDeclarationSyntax = (ConstructorDeclarationSyntax) context.Node;
+            HandleBaseMethodDeclaration(context, constructotDeclarationSyntax);
+        }
+
+        private void HandleMethodDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax) context.Node;
+            HandleBaseMethodDeclaration(context, methodDeclaration);
+        }
+
+        private static void HandleBaseMethodDeclaration(SyntaxNodeAnalysisContext context,
+            BaseMethodDeclarationSyntax baseMethodDeclarationSyntax)
+        {
+            var identifierTokens =
+                baseMethodDeclarationSyntax.ChildTokens()
+                    .Where(t => t.CSharpKind() == SyntaxKind.IdentifierToken)
+                    .ToList();
+            var parameterListSyntax = baseMethodDeclarationSyntax.ChildNodes().OfType<ParameterListSyntax>().SingleOrDefault();
+
+            if (identifierTokens.Any() && parameterListSyntax != null)
+            {
+                var identifierToken = identifierTokens.First();
+                var openParenTokens =
+                    parameterListSyntax.ChildTokens()
+                        .Where(t => t.CSharpKind() == SyntaxKind.OpenParenToken)
+                        .ToList();
+
+                if (openParenTokens.Any())
+                {
+                    CheckIfLocationOfIdentifierNameAndOpenTokenAreTheSame(context, openParenTokens.First(), identifierToken);
+                }
+            }
+        }
+
+        private static void CheckIfLocationOfIdentifierNameAndOpenTokenAreTheSame(SyntaxNodeAnalysisContext context,
+            SyntaxToken openToken, SyntaxToken identifierToken)
+        {
+            var identifierLine = identifierToken.GetLocation().GetLineSpan();
+            var openParenLocation = openToken.GetLocation();
+            var openParenLine = openParenLocation.GetLineSpan();
+            if (identifierLine.IsValid &&
+                openParenLine.IsValid &&
+                openParenLine.StartLinePosition.Line != identifierLine.StartLinePosition.Line)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, openParenLocation));
+            }
         }
     }
 }
