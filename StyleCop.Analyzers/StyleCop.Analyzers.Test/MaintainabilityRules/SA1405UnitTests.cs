68025289ced30247a7b68f23b4a2518950ced541
Dennis Fischer <fischer_dennis@live.de>
2015-01-03 15:15:47 +0100

    Implemented SA1405 and SA1406

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/MaintainabilityRules/SA1405UnitTests.cs
@@ -0,0 +1,188 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StyleCop.Analyzers.MaintainabilityRules;
+using TestHelper;
+
+namespace StyleCop.Analyzers.Test.MaintainabilityRules
+{
+    [TestClass]
+    public class SA1405UnitTests : CodeFixVerifier
+    {
+        private const string DiagnosticId = SA1405DebugAssertMustProvideMessageText.DiagnosticId;
+        protected static readonly DiagnosticResult[] EmptyDiagnosticResults = { };
+
+        [TestMethod]
+        public async Task TestEmptySource()
+        {
+            var testCode = @"";
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWhitespaceMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Assert(true != false, ""     "");
+    }
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = DiagnosticId,
+                    Message = "Debug.Assert must provide message text",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 6, 9)
+                        }
+                }
+            };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestNullMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Assert(true != false, null);
+    }
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = DiagnosticId,
+                    Message = "Debug.Assert must provide message text",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 6, 9)
+                        }
+                }
+            };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWrongOverload()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Assert(true != false);
+    }
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = DiagnosticId,
+                    Message = "Debug.Assert must provide message text",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 6, 9)
+                        }
+                }
+            };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestConstantMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Assert(true != false, ""A message"");
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestNotConstantMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Assert(true != false, message);
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWrongDebugClass()
+        {
+            var testCode = @"
+public class Foo
+{
+    public void Bar()
+    {
+        string message = ""A message"";
+        Debug.Assert(true != false, message);
+    }
+}
+class Debug
+{
+    public static void Assert(bool b, string s)
+    {
+
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWrongMethod()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Write(null);
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1405DebugAssertMustProvideMessageText();
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/MaintainabilityRules/SA1406UnitTests.cs
@@ -0,0 +1,158 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StyleCop.Analyzers.MaintainabilityRules;
+using TestHelper;
+
+namespace StyleCop.Analyzers.Test.MaintainabilityRules
+{
+    [TestClass]
+    public class SA1406UnitTests : CodeFixVerifier
+    {
+        private const string DiagnosticId = SA1406DebugFailMustProvideMessageText.DiagnosticId;
+        protected static readonly DiagnosticResult[] EmptyDiagnosticResults = { };
+
+        [TestMethod]
+        public async Task TestEmptySource()
+        {
+            var testCode = @"";
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWhitespaceMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Fail(""     "");
+    }
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = DiagnosticId,
+                    Message = "Debug.Fail must provide message text",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 6, 9)
+                        }
+                }
+            };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestNullMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Fail(null);
+    }
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = DiagnosticId,
+                    Message = "Debug.Fail must provide message text",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 6, 9)
+                        }
+                }
+            };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestConstantMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Fail(""A message"");
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestNotConstantMessage()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        string message = ""A message"";
+        Debug.Fail(message);
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWrongMethod()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar()
+    {
+        Debug.Write(null);
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestWrongDebugClass()
+        {
+            var testCode = @"
+public class Foo
+{
+    public void Bar()
+    {
+        string message = ""A message"";
+        Debug.Fail(message);
+    }
+}
+class Debug
+{
+    public static void Fail(string s)
+    {
+
+    }
+}
+";
+            await VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1406DebugFailMustProvideMessageText();
+        }
+    }
+}
