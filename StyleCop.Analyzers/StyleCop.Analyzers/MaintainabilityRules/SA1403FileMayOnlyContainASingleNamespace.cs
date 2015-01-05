ecc24a2e5e3cad48f295fe0000a2b8a150c603e3
Dennis Fischer <fischer_dennis@live.de>
2015-01-05 08:12:44 +0100

    Implemented SA1402 and SA1403

--- StyleCop.Analyzers/StyleCop.Analyzers/MaintainabilityRules/SA1403FileMayOnlyContainASingleNamespace.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/MaintainabilityRules/SA1403FileMayOnlyContainASingleNamespace.cs
@@ -3,6 +3,12 @@
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using System;
+
+
+
 
     /// <summary>
     /// A C# code file contains more than one namespace.
@@ -16,13 +22,13 @@
     {
         public const string DiagnosticId = "SA1403";
         internal const string Title = "File may only contain a single namespace";
-        internal const string MessageFormat = "TODO: Message format";
+        internal const string MessageFormat = "File may only contain a single namespace";
         internal const string Category = "StyleCop.CSharp.MaintainabilityRules";
         internal const string Description = "A C# code file contains more than one namespace.";
         internal const string HelpLink = "http://www.stylecop.com/docs/SA1403.html";
 
         public static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description, HelpLink);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
             ImmutableArray.Create(Descriptor);
@@ -39,7 +45,41 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxTreeAction(HandleSyntaxTree);
+        }
+
+        private async void HandleSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            var syntaxRoot = await context.Tree.GetRootAsync(context.CancellationToken);
+
+            var descentNodes = syntaxRoot.DescendantNodes(descendIntoChildren: node => node != null && !node.IsKind(SyntaxKind.ClassDeclaration));
+
+            bool foundNode = false;
+
+            foreach (var node in descentNodes)
+            {
+                if (node.IsKind(SyntaxKind.NamespaceDeclaration))
+                {
+                    if (foundNode)
+                    {
+                        var location = GetNamespaceLocation(node);
+                        if (location != null)
+                        {
+                            context.ReportDiagnostic(Diagnostic.Create(Descriptor, location));
+                        }
+                    }
+                    else
+                    {
+                        foundNode = true;
+                    }
+                }
+            }
+        }
+
+        private Location GetNamespaceLocation(SyntaxNode node)
+        {
+            var namespaceDeclaration = node as NamespaceDeclarationSyntax;
+            return namespaceDeclaration?.Name?.GetLocation();
         }
     }
 }
