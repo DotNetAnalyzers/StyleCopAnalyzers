eedc55ee46479adf1212fd44a2ffb54d43f132e9
Dennis Fischer <fischer_dennis@live.de>
2015-01-12 00:31:43 +0100

    Add more SA1121 unit tests for diagnostic and code fix

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1121UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1121UnitTests.cs
@@ -20,35 +20,35 @@
     {
         static readonly Tuple<string, string>[] _referenceTypes = new Tuple<string, string>[]
         {
-            new Tuple<string,string>("object", "Object"),
-            new Tuple<string,string>("string", "String")
+            new Tuple<string,string>("object", nameof(Object)),
+            new Tuple<string,string>("string", nameof(String))
         };
         static readonly Tuple<string, string>[] _valueTypes = new Tuple<string, string>[]
         {
-            new Tuple<string,string>("bool", "Boolean"),
-            new Tuple<string,string>("byte", "Byte"),
-            new Tuple<string,string>("char", "Char"),
-            new Tuple<string,string>("decimal", "Decimal"),
-            new Tuple<string,string>("double", "Double"),
-            new Tuple<string,string>("short", "Int16"),
-            new Tuple<string,string>("int", "Int32"),
-            new Tuple<string,string>("long", "Int64"),
-            new Tuple<string,string>("sbyte", "SByte"),
-            new Tuple<string,string>("float", "Single"),
-            new Tuple<string,string>("ushort", "UInt16"),
-            new Tuple<string,string>("uint", "UInt32"),
-            new Tuple<string,string>("ulong", "UInt64")
+            new Tuple<string,string>("bool", nameof(Boolean)),
+            new Tuple<string,string>("byte", nameof(Byte)),
+            new Tuple<string,string>("char", nameof(Char)),
+            new Tuple<string,string>("decimal", nameof(Decimal)),
+            new Tuple<string,string>("double", nameof(Double)),
+            new Tuple<string,string>("short", nameof(Int16)),
+            new Tuple<string,string>("int", nameof(Int32)),
+            new Tuple<string,string>("long", nameof(Int64)),
+            new Tuple<string,string>("sbyte", nameof(SByte)),
+            new Tuple<string,string>("float", nameof(Single)),
+            new Tuple<string,string>("ushort", nameof(UInt16)),
+            new Tuple<string,string>("uint", nameof(UInt32)),
+            new Tuple<string,string>("ulong", nameof(UInt64))
         };
         static readonly Tuple<string, string>[] _enumBaseTypes = new Tuple<string, string>[]
         {
-            new Tuple<string,string>("byte", "Byte"),
-            new Tuple<string,string>("short", "Int16"),
-            new Tuple<string,string>("int", "Int32"),
-            new Tuple<string,string>("long", "Int64"),
-            new Tuple<string,string>("sbyte", "SByte"),
-            new Tuple<string,string>("ushort", "UInt16"),
-            new Tuple<string,string>("uint", "UInt32"),
-            new Tuple<string,string>("ulong", "UInt64")
+            new Tuple<string,string>("byte", nameof(Byte)),
+            new Tuple<string,string>("short", nameof(Int16)),
+            new Tuple<string,string>("int", nameof(Int32)),
+            new Tuple<string,string>("long", nameof(Int64)),
+            new Tuple<string,string>("sbyte", nameof(SByte)),
+            new Tuple<string,string>("ushort", nameof(UInt16)),
+            new Tuple<string,string>("uint", nameof(UInt32)),
+            new Tuple<string,string>("ulong", nameof(UInt64))
         };
         static readonly Tuple<string, string>[] _allTypes = _referenceTypes.Concat(_valueTypes).ToArray();
 
@@ -73,21 +73,55 @@
                 }
             }
         }
+        private async Task VerifyFixes(string testSource, Tuple<string, string>[] types)
+        {
+            foreach (var item in types)
+            {
+                // Allow CS8019 here because the code fix makes the using directive unnecessary
+                await VerifyCSharpFixAsync(string.Format(testSource, item.Item2), string.Format(testSource, item.Item1), allowNewCompilerDiagnostics: true);
+                await VerifyCSharpFixAsync(string.Format(testSource, "System." + item.Item2), string.Format(testSource, item.Item1));
+                await VerifyCSharpFixAsync(string.Format(testSource, "global::System." + item.Item2), string.Format(testSource, item.Item1));
+            }
+        }
+
         private async Task TestAllCases(Func<string, string, Task> func)
         {
             await TestCases(func, _allTypes);
         }
+
+        private async Task VerifyAllFixes(string testSource)
+        {
+            await VerifyFixes(testSource, _allTypes);
+        }
+
         private async Task TestEnumTypeCases(Func<string, string, Task> func)
         {
             await TestCases(func, _enumBaseTypes);
         }
+
+        private async Task VerifyEnumTypeFixes(string testSource)
+        {
+            await VerifyFixes(testSource, _allTypes);
+        }
+
         private async Task TestValueTypeCases(Func<string, string, Task> func)
         {
             await TestCases(func, _valueTypes);
         }
+
+        private async Task VerifyValueTypeFixes(string testSource)
+        {
+            await VerifyFixes(testSource, _allTypes);
+        }
+
         private async Task TestReferenceTypeCases(Func<string, string, Task> func)
         {
             await TestCases(func, _referenceTypes);
+        }
+
+        private async Task VerifyReferenceTypeFixes(string testSource)
+        {
+            await VerifyFixes(testSource, _referenceTypes);
         }
 
 
@@ -128,6 +162,21 @@ public class Foo
 
             await VerifyCSharpDiagnosticAsync(string.Format(testCode, predefined), EmptyDiagnosticResults, CancellationToken.None);
             await VerifyCSharpDiagnosticAsync(string.Format(testCode, fullName), expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestVariableDeclarationCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        {0} test;
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
         }
 
         [TestMethod]
@@ -173,6 +222,21 @@ public class Foo
             await TestAllCases(TestDefaultDeclaration);
         }
 
+        [TestMethod]
+        public async Task TestDefaultDeclarationCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        var test = default({0});
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
+        }
+
         private async Task TestTypeOf(string predefined, string fullName)
         {
             string testCode = @"using System;
@@ -208,6 +272,21 @@ public class Foo
         public async Task TestTypeOf()
         {
             await TestAllCases(TestTypeOf);
+        }
+
+        [TestMethod]
+        public async Task TestTypeOfCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        var test = typeof({0});
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
         }
 
         private async Task TestReturnType(string predefined, string fullName)
@@ -246,6 +325,20 @@ public class Foo
             await TestAllCases(TestReturnType);
         }
 
+        [TestMethod]
+        public async Task TestReturnTypeCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public {0} Bar()
+    {{
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
+        }
+
         private async Task TestEnumBaseType(string predefined, string fullName)
         {
             string testCode = @"using System;
@@ -280,6 +373,20 @@ public class Foo
         public async Task TestEnumBaseType()
         {
             await TestEnumTypeCases(TestEnumBaseType);
+        }
+
+        [TestMethod]
+        public async Task TestEnumBaseTypeCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public enum Bar : {0}
+    {{
+    }}
+}}";
+
+            await VerifyEnumTypeFixes(testSource);
         }
 
         private async Task TestPointerDeclaration(string predefined, string fullName)
@@ -319,6 +426,21 @@ public class Foo
             await TestValueTypeCases(TestPointerDeclaration);
         }
 
+        [TestMethod]
+        public async Task TestPointerDeclarationCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public unsafe void Bar()
+    {{
+        {0}* test;
+    }}
+}}";
+
+            await VerifyValueTypeFixes(testSource);
+        }
+
         private async Task TestArgument(string predefined, string fullName)
         {
             string testCode = @"using System;
@@ -353,6 +475,20 @@ public class Foo
         public async Task TestArgument()
         {
             await TestAllCases(TestArgument);
+        }
+
+        [TestMethod]
+        public async Task TestArgumentCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar({0} test)
+    {{
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
         }
 
         private async Task TestIndexer(string predefined, string fullName)
@@ -413,6 +549,22 @@ public class Foo
         public async Task TestIndexer()
         {
             await TestAllCases(TestIndexer);
+        }
+
+        [TestMethod]
+        public async Task TestIndexerCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public {0} this
+            [{0} test]
+    {{
+        get {{ return default({0}); }}
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
         }
 
         private async Task TestGenericAndLambda(string predefined, string fullName)
@@ -476,6 +628,23 @@ public class Foo
             await TestAllCases(TestGenericAndLambda);
         }
 
+        [TestMethod]
+        public async Task TestGenericAndLambdaCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        Func<{0}, 
+                {0}> f = 
+                    ({0} param) => param;
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
+        }
+
         private async Task TestArray(string predefined, string fullName)
         {
             string testCode = @"using System;
@@ -511,6 +680,21 @@ public class Foo
         public async Task TestArray()
         {
             await TestAllCases(TestArray);
+        }
+
+        [TestMethod]
+        public async Task TestArrayCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        var array = new {0}[0];
+    }}
+}}";
+
+            await VerifyAllFixes(testSource);
         }
 
         private async Task TestStackAllocArray(string predefined, string fullName)
@@ -550,12 +734,27 @@ public class Foo
             await TestAllCases(TestStackAllocArray);
         }
 
+        [TestMethod]
+        public async Task TestStackAllocArrayCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public unsafe void Bar()
+    {{
+        var array = stackalloc {0}[0];
+    }}
+}}";
+
+            await VerifyValueTypeFixes(testSource);
+        }
+
         private async Task TestImplicitCast(string predefined, string fullName)
         {
             string testCode = @"using System;
 public class Foo
 {{
-    public unsafe void Bar()
+    public void Bar()
     {{
         var t = ({0})
                     default({0});
@@ -599,12 +798,28 @@ public class Foo
             await TestAllCases(TestImplicitCast);
         }
 
+        [TestMethod]
+        public async Task TestImplicitCastCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        var t = ({0})
+                    default({0});
+    }}
+}}";
+
+            await VerifyValueTypeFixes(testSource);
+        }
+
         private async Task TestExplicitCast(string predefined, string fullName)
         {
             string testCode = @"using System;
 public class Foo
 {{
-    public unsafe void Bar()
+    public void Bar()
     {{
         var t = null as {0};
     }}
@@ -636,12 +851,27 @@ public class Foo
             await TestReferenceTypeCases(TestExplicitCast);
         }
 
+        [TestMethod]
+        public async Task TestExplicitCastCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        var t = null as {0};
+    }}
+}}";
+
+            await VerifyValueTypeFixes(testSource);
+        }
+
         private async Task TestNullable(string predefined, string fullName)
         {
             string testCode = @"using System;
 public class Foo
 {{
-    public unsafe void Bar()
+    public void Bar()
     {{
         {0}? t = null;
     }}
@@ -671,6 +901,161 @@ public class Foo
         public async Task TestNullable()
         {
             await TestValueTypeCases(TestNullable);
+        }
+
+        [TestMethod]
+        public async Task TestNullableCodeFix()
+        {
+            string testSource = @"using System;
+public class Foo
+{{
+    public void Bar()
+    {{
+        {0}? t = null;
+    }}
+}}";
+
+            await VerifyValueTypeFixes(testSource);
+        }
+
+        [TestMethod]
+        public async Task TestMissleadingUsing()
+        {
+            string testCode = @"namespace Foo
+{
+  using Int32 = System.UInt32;
+  class Bar
+  {
+    Int32 value = 3;
+  }
+}
+";
+            DiagnosticResult[] expected;
+
+            expected =
+                new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Use built-in type alias",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 6, 5)
+                            }
+                    },
+                // Workaround because the diagnostic is called twice for the SyntaxNode
+                new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Use built-in type alias",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 6, 5)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestMissleadingUsingCodeFix()
+        {
+            string oldSource = @"namespace Foo
+{
+  using Int32 = System.UInt32;
+  class Bar
+  {
+    Int32 value = 3;
+  }
+}
+";
+            string newSource = @"namespace Foo
+{
+  using Int32 = System.UInt32;
+  class Bar
+  {
+    uint value = 3;
+  }
+}
+";
+
+            await VerifyCSharpFixAsync(oldSource, newSource, allowNewCompilerDiagnostics: true);
+        }
+
+        [TestMethod]
+        public async Task TestUsingNameChange()
+        {
+            string testCode = @"namespace Foo
+{
+  using MyInt = System.UInt32;
+  class Bar
+  {
+    MyInt value = 3;
+  }
+}
+";
+            DiagnosticResult[] expected;
+
+            expected =
+                new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Use built-in type alias",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 6, 5)
+                            }
+                    },
+                // Workaround because the diagnostic is called twice for the SyntaxNode
+                new DiagnosticResult
+                    {
+                        Id = DiagnosticId,
+                        Message = "Use built-in type alias",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 6, 5)
+                            }
+                    }
+                };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestUsingNameChangeCodeFix()
+        {
+            string oldSource = @"namespace Foo
+{
+  using MyInt = System.UInt32;
+  class Bar
+  {
+    MyInt value = 3;
+  }
+}
+";
+            string newSource = @"namespace Foo
+{
+  using MyInt = System.UInt32;
+  class Bar
+  {
+    uint value = 3;
+  }
+}
+";
+
+            await VerifyCSharpFixAsync(oldSource, newSource, allowNewCompilerDiagnostics: true);
         }
 
         [TestMethod]
@@ -719,6 +1104,11 @@ public namespace {0}
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new SA1121UseBuiltInTypeAlias();
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SA1121CodeFixProvider();
         }
     }
 }
