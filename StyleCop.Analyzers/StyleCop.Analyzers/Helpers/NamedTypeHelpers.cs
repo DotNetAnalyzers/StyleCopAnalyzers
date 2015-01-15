eb68669501026e10d48c320f5a514d9df0c1daf5
Sam Harwell <sam@tunnelvisionlabs.com>
2015-01-14 23:41:38 -0600

    Update NamedTypeHelpers to support working on the syntax tree

--- StyleCop.Analyzers/StyleCop.Analyzers/Helpers/NamedTypeHelpers.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/Helpers/NamedTypeHelpers.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     internal static class NamedTypeHelpers
     {
@@ -20,6 +21,15 @@
             return false;
         }
 
+        internal static bool IsNativeMethodsClass(ClassDeclarationSyntax syntax)
+        {
+            string name = syntax?.Identifier.ValueText;
+            if (name == null)
+                return false;
+
+            return name.EndsWith("NativeMethods", StringComparison.Ordinal);
+        }
+
         internal static bool IsContainedInNativeMethodsClass(INamedTypeSymbol type)
         {
             if(IsNativeMethodsClass(type))
@@ -36,6 +46,20 @@
                 }
 
                 
+            }
+
+            return false;
+        }
+
+        internal static bool IsContainedInNativeMethodsClass(SyntaxNode syntax)
+        {
+            while (syntax != null)
+            {
+                ClassDeclarationSyntax classDeclarationSyntax = syntax.FirstAncestorOrSelf<ClassDeclarationSyntax>();
+                if (IsNativeMethodsClass(classDeclarationSyntax))
+                    return true;
+
+                syntax = syntax?.Parent;
             }
 
             return false;
