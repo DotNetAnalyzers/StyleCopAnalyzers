eeb4eb4d610d2600577aa919dde7b3a7ff4bf97e
Sam Harwell <sam@tunnelvisionlabs.com>
2015-01-21 08:58:58 -0600

    Update metadata and add build scripts and strong name keys

---
+++ build/push.ps1
@@ -0,0 +1,8 @@
+. .\version.ps1
+
+If ($Version.EndsWith('-dev')) {
+	$host.ui.WriteErrorLine("Cannot push development version '$Version' to NuGet.")
+	Exit 1
+}
+
+..\.nuget\NuGet.exe 'push' ".\nuget\StyleCop.Analyzers.$Version.nupkg"
