1f9de5795c6e61d9d11ae265f1a2bffe0a143eea
Maciej Pakulski <pakulski.maciej@gmail.com>
2015-02-14 19:15:56 +0000

    Test if ExportCodeFixProviderAttribute is applied correctly

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ExportCodeFixProviderAttributeNameTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StyleCop.Analyzers.ReadabilityRules;
+
+namespace StyleCop.Analyzers.Test
+{
+    [TestClass]
+    public class ExportCodeFixProviderAttributeNameTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var issues = new List<string>();
+
+            var codeFixProviders = typeof(SA1110OpeningParenthesisMustBeOnDeclarationLine)
+                .Assembly
+                .GetExportedTypes()
+                .Where(t => typeof(CodeFixProvider).IsAssignableFrom(t))
+                .ToList();
+
+            foreach (var codeFixProvider in codeFixProviders)
+            {
+                var exportCodeFixProviderAttribute = codeFixProvider.GetCustomAttributes(false)
+                    .OfType<ExportCodeFixProviderAttribute>()
+                    .FirstOrDefault();
+                if (exportCodeFixProviderAttribute == null)
+                {
+                    issues.Add(string.Format("{0} should have ExportCodeFixProviderAttribute attribute", codeFixProvider.Name));
+                    continue;
+                }
+
+                if (!string.Equals(exportCodeFixProviderAttribute.Name, codeFixProvider.Name,
+                        StringComparison.InvariantCulture))
+                {
+                    issues.Add(string.Format("Name parameter of ExportCodeFixProviderAttribute applied on {0} should be set to {0}", codeFixProvider.Name));
+                }
+            }
+
+            if (issues.Any())
+            {
+                Assert.Fail(string.Join("\r\t", issues));
+            }
+        }
+    }
+}
