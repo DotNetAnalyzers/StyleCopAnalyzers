1a5659fb71a259c26420cc297357f8f47389dccb
Dennis Fischer <fischer_dennis@live.de>
2015-02-16 21:43:02 +0100

    Add unit tests for SA1123

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ReadabilityRules/SA1123UnitTests.cs
@@ -0,0 +1,180 @@
+﻿namespace StyleCop.Analyzers.Test.ReadabilityRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StyleCop.Analyzers.ReadabilityRules;
+    using TestHelper;
+
+    /// <summary>
+    /// This class contains unit tests for <see cref="SA1123DoNotPlaceRegionsWithinElements"/> and
+    /// <see cref="RemoveRegionCodeFixProvider"/>.
+    /// </summary>
+    [TestClass]
+    public class SA1123UnitTests : CodeFixVerifier
+    {
+        public string DiagnosticId { get; } = SA1123DoNotPlaceRegionsWithinElements.DiagnosticId;
+
+        [TestMethod]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestRegionInMethod()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+    {
+#region Foo
+        string test = """";
+#endregion
+    }
+}";
+
+            DiagnosticResult[] expected;
+
+            expected =
+                new[]
+                {
+                    new DiagnosticResult
+                    {
+                        Id = this.DiagnosticId,
+                        Message = "Region must not be located within a code element.",
+                        Severity = DiagnosticSeverity.Warning,
+                        Locations =
+                            new[]
+                            {
+                                new DiagnosticResultLocation("Test0.cs", 5, 1)
+                            }
+                    }
+                };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            string fixedCode = @"public class Foo
+{
+    public void Bar()
+    {
+        string test = """";
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode);
+        }
+
+        [TestMethod]
+        public async Task TestRegionPartialyInMethod()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+    {
+#region Foo
+        string test = """";
+    }
+#endregion
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestRegionPartialyInMethod2()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+#region Foo
+    {
+        string test = """";
+    }
+#endregion
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestRegionPartialyMultipleMethods()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+    {
+#region Foo
+        string test = """";
+    }
+    public void FooBar()
+    {
+        string test = """";
+#endregion
+    }
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestEndRegionInMethod()
+        {
+            var testCode = @"public class Foo
+{
+#region Foo
+    public void Bar()
+    {
+        string test = """";
+#endregion
+    }
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestRegionOutsideMethod()
+        {
+            var testCode = @"public class Foo
+{
+#region Foo
+#endregion
+    public void Bar()
+    {
+        string test = """";
+    }
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestRegionOutsideMethod2()
+        {
+            var testCode = @"public class Foo
+{
+#region Foo
+    public void Bar()
+    {
+        string test = """";
+    }
+#endregion
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new RemoveRegionCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1123DoNotPlaceRegionsWithinElements();
+        }
+    }
+}
