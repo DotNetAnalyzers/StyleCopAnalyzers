ca1783313626ae53d7ca13cb87afb732007fd1eb
Dennis Fischer <fischer_dennis@live.de>
2015-02-25 17:58:36 +0100

    Ignore failing tests for now

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/Helpers/OpenIssueAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace StyleCop.Analyzers.Test.Helpers
+{
+    /// <summary>
+    /// Indicates that a specific test is related to an open issue.
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public sealed class OpenIssueAttribute : System.Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenIssueAttribute"/> class.
+        /// </summary>
+        /// <param name="issue">A link to the open issue.</param>
+        public OpenIssueAttribute(string issue)
+        {
+            this.Issue = issue;
+        }
+
+        public string Issue { get; }
+    }
+}
