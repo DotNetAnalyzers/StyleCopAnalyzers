9ba7fa885065a5cd3e22ee336b80f827bf33db3f
Maciej Pakulski <pakulski.maciej@gmail.com>
2015-03-07 15:36:42 +0000

    SA1214 - diagnostic implemented

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1214UnitTests.cs
@@ -0,0 +1,204 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StyleCop.Analyzers.OrderingRules;
+using TestHelper;
+
+namespace StyleCop.Analyzers.Test.OrderingRules
+{
+    [TestClass]
+    public class SA1214UnitTests : CodeFixVerifier
+    {
+        public string DiagnosticId { get; } = SA1214StaticReadonlyElementsMustAppearBeforeStaticNonReadonlyElements.DiagnosticId;
+
+        [TestMethod]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestTwoFieldsInClassStaticReadonlyFieldPlacedAfterStaticNonReadonly()
+        {
+            var testCode = @"
+public class Foo
+{
+    private static int i = 0;
+    private static readonly int j = 0;
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = this.DiagnosticId,
+                    Message = "Static readonly elements must appear before static non-readonly elements.",
+                    Severity =  DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 5, 33)
+                        }
+                }
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestTwoFieldsInClassStaticReadonlyFieldPlacedAfterStaticNonReadonlyOneLine()
+        {
+            var testCode = @"
+public class Foo
+{
+    private static int i = 0;private static readonly int j = 0;
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = this.DiagnosticId,
+                    Message = "Static readonly elements must appear before static non-readonly elements.",
+                    Severity =  DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 4, 58)
+                        }
+                }
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestTwoFieldsInClassStaticReadonlyFieldPlacedBeforeStaticNonReadonly()
+        {
+            var testCode = @"
+public class Foo
+{
+    private static readonly int i = 0;
+    private static int j = 0;
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestTwoFieldsInClassNonStaticReadonlyFieldPlacedAfterNonStaticNonReadonly()
+        {
+            var testCode = @"
+public class Foo
+{
+    private int i = 0;
+    private readonly int j = 0;
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task TestTwoFieldsInStructStaticReadonlyFieldPlacedAfterStaticNonReadonly()
+        {
+            var testCode = @"
+public struct Foo
+{
+    private static int i = 0;
+    private static readonly int j = 0;
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = this.DiagnosticId,
+                    Message = "Static readonly elements must appear before static non-readonly elements.",
+                    Severity =  DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 5, 33)
+                        }
+                }
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task ComplexExample()
+        {
+            var testCode = @"
+public class Foo
+{
+    public string s = ""qwe"";
+    private static readonly int i = 0;
+
+    public void Ff() {}
+
+    public static string s2 = ""qwe"";
+
+    public static readonly int  u = 5;
+
+    public class FooInner 
+    {
+        private int aa = 0;
+        public static readonly int t = 2;
+        private static int z = 999;
+        private static readonly int e = 1;
+    }
+
+    public static readonly int j = 0;
+}";
+
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = this.DiagnosticId,
+                    Message = "Static readonly elements must appear before static non-readonly elements.",
+                    Severity =  DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 11, 33)
+                        }
+                },
+                new DiagnosticResult
+                {
+                    Id = this.DiagnosticId,
+                    Message = "Static readonly elements must appear before static non-readonly elements.",
+                    Severity =  DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 18, 37)
+                        }
+                },
+                new DiagnosticResult
+                {
+                    Id = this.DiagnosticId,
+                    Message = "Static readonly elements must appear before static non-readonly elements.",
+                    Severity =  DiagnosticSeverity.Warning,
+                    Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 21, 32)
+                        }
+                },
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1214StaticReadonlyElementsMustAppearBeforeStaticNonReadonlyElements();
+        }
+    }
+}
--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1214StaticReadonlyElementsMustAppearBeforeStaticNonReadonlyElements.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1214StaticReadonlyElementsMustAppearBeforeStaticNonReadonlyElements.cs
@@ -1,7 +1,10 @@
 ﻿namespace StyleCop.Analyzers.OrderingRules
 {
     using System.Collections.Immutable;
+    using System.Linq;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     /// <summary>
@@ -20,7 +23,7 @@
         /// </summary>
         public const string DiagnosticId = "SA1214";
         private const string Title = "Static readonly elements must appear before static non-readonly elements";
-        private const string MessageFormat = "TODO: Message format";
+        private const string MessageFormat = "Static readonly elements must appear before static non-readonly elements.";
         private const string Category = "StyleCop.CSharp.OrderingRules";
         private const string Description = "A static readonly element is positioned beneath a static non-readonly element of the same type.";
         private const string HelpLink = "http://www.stylecop.com/docs/SA1214.html";
@@ -43,7 +46,61 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeAction(this.AnalyzeClass, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeAction(this.AnalyzeStruct, SyntaxKind.StructDeclaration);
+        }
+
+        private void AnalyzeStruct(SyntaxNodeAnalysisContext context)
+        {
+            var structDeclaration = (StructDeclarationSyntax)context.Node;
+
+            AnalyzeType(context, structDeclaration);
+        }
+
+        private void AnalyzeClass(SyntaxNodeAnalysisContext context)
+        {
+            var classDeclaration = (ClassDeclarationSyntax) context.Node;
+
+            AnalyzeType(context, classDeclaration);
+        }
+
+        private static void AnalyzeType(SyntaxNodeAnalysisContext context, TypeDeclarationSyntax typeDeclaration)
+        {
+            var staticFields = typeDeclaration.Members
+                .OfType<BaseFieldDeclarationSyntax>()
+                .Where(f => f.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword)))
+                .ToList();
+
+            var endLocationsOfStaticNonReadonlyFields =
+                staticFields.Where(f => f.Modifiers.Any(m => m.IsKind(SyntaxKind.ReadOnlyKeyword)) == false)
+                    .Select(f => f.GetLocation().GetLineSpan())
+                    .Where(l => l.IsValid)
+                    .Select(l => l.EndLinePosition)
+                    .ToList();
+
+            var readonlyFieldLocations =
+                staticFields.Where(f => f.Modifiers.Any(m => m.IsKind(SyntaxKind.ReadOnlyKeyword)))
+                    .Where(f => f.GetLocation().GetLineSpan().IsValid)
+                    .Where(f => f.Declaration != null && f.Declaration.Variables.Any())
+                    .Select(f => new {
+                        FieldLocations = f.Declaration.Variables
+                                                      .Where(v => !v.IsMissing && !v.Identifier.IsMissing)
+                                                      .Select(v => v.Identifier.GetLocation())
+                                                      .ToList(),
+                        FieldEndLinePosition = f.GetLocation().GetLineSpan().EndLinePosition
+                    })
+                    .ToList();
+
+            foreach (var location in readonlyFieldLocations)
+            {
+                if (endLocationsOfStaticNonReadonlyFields.Any(e => e < location.FieldEndLinePosition))
+                {
+                    foreach (var fieldLocation in location.FieldLocations)
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(Descriptor, fieldLocation));
+                    }
+                }
+            }
         }
     }
 }
