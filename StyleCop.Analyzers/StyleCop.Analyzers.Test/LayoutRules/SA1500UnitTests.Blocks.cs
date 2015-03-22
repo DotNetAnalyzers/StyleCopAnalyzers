31e453b1a16e91b8456be7182a8573b97991ec34
vweijsters <vweijsters@outlook.com>
2015-03-22 22:27:01 +0100

    Implemented tests for blocks (try #2)

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/LayoutRules/SA1500UnitTests.Blocks.cs
@@ -0,0 +1,84 @@
+﻿namespace StyleCop.Analyzers.Test.LayoutRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using StyleCop.Analyzers.LayoutRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="SA1500CurlyBracketsForMultiLineStatementsMustNotShareLine"/>.
+    /// </summary>
+    public partial class SA1500UnitTests : DiagnosticVerifier
+    {
+        /// <summary>
+        /// Verifies that no diagnostics are reported for the valid block defined in this test.
+        /// </summary>
+        /// <remarks>
+        /// These blocks are valid for SA1500 only, some will report other diagnostics from the layout (SA15xx) series.
+        /// </remarks>
+        [Fact]
+        public async Task TestValidBlocks()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+    {
+        // valid block #1
+        {
+        }
+
+        // valid block #2
+        {
+            int a;
+        }
+
+        // valid block #3
+        { }
+
+        // valid block #4
+        { int b; }
+    }
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that not having the opening curly bracket on its own line will report a diagnostic.
+        /// </summary>
+        [Fact]
+        public async Task TestInvalidOpeningCurlyBracket()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+    {
+        { int a;
+        }
+    }
+}";
+            var expectedDiagnostic = this.CSharpDiagnostic().WithLocation(5, 9);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expectedDiagnostic, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that not having the closing curly bracket on its own line will report a diagnostic.
+        /// </summary>
+        [Fact]
+        public async Task TestInvalidClosingCurlyBracket()
+        {
+            var testCode = @"public class Foo
+{
+    public void Bar()
+    {
+        { 
+            int a; }
+    }
+}";
+            var expectedDiagnostic = this.CSharpDiagnostic().WithLocation(6, 20);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expectedDiagnostic, CancellationToken.None).ConfigureAwait(false);
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/LayoutRules/SA1500UnitTests.cs
@@ -0,0 +1,31 @@
+﻿namespace StyleCop.Analyzers.Test.LayoutRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.LayoutRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="SA1500CurlyBracketsForMultiLineStatementsMustNotShareLine"/>.
+    /// </summary>
+    public partial class SA1500UnitTests : DiagnosticVerifier
+    {
+        /// <summary>
+        /// Verifies that the analyzer will properly handle an empty source.
+        /// </summary>
+        [Fact]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        /// <inheritdoc/>
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1500CurlyBracketsForMultiLineStatementsMustNotShareLine();
+        }
+    }
+}
