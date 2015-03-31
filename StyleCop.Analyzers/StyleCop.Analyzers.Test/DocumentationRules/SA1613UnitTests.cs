86402228ab81eb3228c99b183cc2f922581e6330
Dennis Fischer <fischer_dennis@live.de>
2015-03-31 11:12:56 +0200

    Implement SA1613 Unit Tests

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/DocumentationRules/SA1613UnitTests.cs
@@ -0,0 +1,124 @@
+﻿namespace StyleCop.Analyzers.Test.DocumentationRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Analyzers.DocumentationRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// This class contains unit tests for <see cref="SA1617VoidReturnValueMustNotBeDocumented"/>-
+    /// </summary>
+    public class SA1613UnitTests : CodeFixVerifier
+    {
+        public string DiagnosticId { get; } = SA1617VoidReturnValueMustNotBeDocumented.DiagnosticId;
+
+        [Fact]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestMemberWithoutDocumentation()
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    public ClassName Method() { return null; }
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestMemberWithInheritDoc()
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <inheritdoc/>
+    public ClassName Method() { return null; }
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestMemberWithoutParams()
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    public ClassName Method(string foo, string bar) { return null; }
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestMemberWithValidParams()
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    ///<param name=""foo"">Test</param>
+    ///<param name=""bar"">Test</param>
+    public ClassName Method(string foo, string bar) { return null; }
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        public async Task TestMemberWithInvalidParams()
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    ///<param>Test</param>
+    ///<param/>
+    ///<param name="""">Test</param>
+    ///<param name=""    "">Test</param>
+    public ClassName Method(string foo, string bar) { return null; }
+}";
+
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(10, 8),
+                this.CSharpDiagnostic().WithLocation(11, 8),
+                this.CSharpDiagnostic().WithLocation(12, 15),
+                this.CSharpDiagnostic().WithLocation(13, 15)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1613ElementParameterDocumentationMustDeclareParameterName();
+        }
+    }
+}
+
