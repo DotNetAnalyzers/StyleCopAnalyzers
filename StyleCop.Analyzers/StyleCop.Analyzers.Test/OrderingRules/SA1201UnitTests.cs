885f1e5378b57705d01d792903c1119cbf232bab
Dennis Fischer <fischer_dennis@live.de>
2015-03-31 11:51:17 +0200

    Fix SA1201 unit tests to not contain compiler errors

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1201UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1201UnitTests.cs
@@ -53,18 +53,14 @@ public struct FooStruct { }
             await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
             await this.VerifyCSharpDiagnosticAsync("namespace OuterNamespace { " + testCode + " }", expected, CancellationToken.None);
         }
-
-        [Theory]
-        [InlineData("class")]
-        [InlineData("struct")]
-        [InlineData("interface")]
-        public async Task TestTypeMemberOrderCorrectOrder(string type)
+        
+        [Fact]
+        public async Task TestTypeMemberOrderCorrectOrderClass()
         {
-            // Constructor and destructors might not be valid for interfaces/structs, but the code does not have to be valid to test the diagnostic
-            string testCode = @"public %type% OuterType
+            string testCode = @"public class OuterType
 {
     public string TestField;
-    public OuterType() { }
+    public OuterType() { TestField = ""foo""; TestProperty = """"; }
     ~OuterType() { }
     public delegate void TestDelegate();
     public event TestDelegate TestEvent { add { } remove { } }
@@ -78,17 +74,50 @@ public struct FooStruct { }
 }
 ";
 
-            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("%type%", type), EmptyDiagnosticResults, CancellationToken.None);
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
         }
 
-        [Theory]
-        [InlineData("class")]
-        [InlineData("struct")]
-        [InlineData("interface")]
-        public async Task TestTypeMemberOrderWrongOrder(string type)
+        [Fact]
+        public async Task TestTypeMemberOrderCorrectOrderStruct()
         {
-            // Constructor and destructors might not be valid for interfaces/structs, but the code does not have to be valid to test the diagnostic
-            string testCode = @"public %type% OuterType
+            string testCode = @"public struct OuterType
+{
+    public string TestField;
+    public OuterType() { TestField = ""foo""; TestProperty = """"; }
+    public delegate void TestDelegate();
+    public event TestDelegate TestEvent { add { } remove { } }
+    public enum TestEnum { }
+    public interface ITest { }
+    public string TestProperty { get; set; }
+    public string this[string arg] { get { return ""foo""; } set { } }
+    public void TestMethod () { }
+    public struct TestStruct { }
+    public class TestClass { }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestTypeMemberOrderCorrectOrderInterface()
+        {
+            string testCode = @"public interface OuterType
+{
+    event System.Action TestEvent;
+    string TestProperty { get; set; }
+    string this[string arg] { get; set; }
+    void TestMethod ();
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestTypeMemberOrderWrongOrderClass()
+        {
+            string testCode = @"public class OuterType
 {
     public string TestField;
     ~OuterType() { }
@@ -112,7 +141,56 @@ public struct FooStruct { }
                 this.CSharpDiagnostic().WithLocation(13, 19).WithArguments("indexer", "method")
             };
 
-            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("%type%", type), expected, CancellationToken.None);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestTypeMemberOrderWrongOrderStruct()
+        {
+            // Constructor and destructors might not be valid for interfaces/structs, but the code does not have to be valid to test the diagnostic
+            string testCode = @"public struct OuterType
+{
+    public string TestField;
+    public OuterType() { TestField = ""foo""; TestProperty = ""bar""; }
+    public interface ITest { }
+    public delegate void TestDelegate();
+    public event TestDelegate TestEvent { add { } remove { } }
+    public enum TestEnum { }
+    public string TestProperty { get; set; }
+    public struct TestStruct { }
+    public void TestMethod () { }
+    public string this[string arg] { get { return ""foo""; } set { } }
+    public class TestClass { }
+}
+";
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(6, 26).WithArguments("delegate", "interface"),
+                this.CSharpDiagnostic().WithLocation(11, 17).WithArguments("method", "struct"),
+                this.CSharpDiagnostic().WithLocation(12, 19).WithArguments("indexer", "method")
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestTypeMemberOrderWrongOrderInterface()
+        {
+            // Constructor and destructors might not be valid for interfaces/structs, but the code does not have to be valid to test the diagnostic
+            string testCode = @"public interface OuterType
+{
+    event System.Action TestEvent;
+    string TestProperty { get; set; }
+    void TestMethod ();
+    string this[string arg] { get; set; }
+}
+";
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(6, 12).WithArguments("indexer", "method")
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
