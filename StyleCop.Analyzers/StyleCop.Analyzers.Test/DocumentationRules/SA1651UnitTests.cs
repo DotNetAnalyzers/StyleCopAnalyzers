16246a77172a70f7ed40b87a0c5444290110ce12
Sam Harwell <sam@tunnelvisionlabs.com>
2015-04-03 11:34:43 -0500

    Implement "Do not use placeholder elements" as new diagnostic SA1651
    Fixes #536

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/DocumentationRules/SA1651UnitTests.cs
@@ -0,0 +1,247 @@
+﻿namespace StyleCop.Analyzers.Test.DocumentationRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.DocumentationRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// This class contains unit tests for <see cref="SA1651DoNotUsePlaceholderElements"/>.
+    /// </summary>
+    public class SA1651UnitTests : CodeFixVerifier
+    {
+        public string DiagnosticId { get; } = SA1651DoNotUsePlaceholderElements.DiagnosticId;
+
+        [Fact]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestEmptyDocumentation()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    ///
+    ///
+    ///
+    public class ClassName
+    {
+    }
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestDocumentationWithoutPlaceholders()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// Content.
+    /// </summary>
+    public class ClassName
+    {
+    }
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestTopLevelPlaceholder()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    /// <placeholder><summary>
+    /// Content.
+    /// </summary></placeholder>
+    public class ClassName
+    {
+    }
+}";
+
+            var fixedCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// Content.
+    /// </summary>
+    public class ClassName
+    {
+    }
+}";
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(3, 9);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestTopLevelEmptyPlaceholder()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    /// <placeholder/>
+    public class ClassName
+    {
+    }
+}";
+
+            // Empty placeholders are not altered by the current code fix.
+            var fixedCode = testCode;
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(3, 9);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestEmbeddedPlaceholder()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// <placeholder>Content.</placeholder>
+    /// </summary>
+    public class ClassName
+    {
+    }
+}";
+
+            var fixedCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// Content.
+    /// </summary>
+    public class ClassName
+    {
+    }
+}";
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(4, 9);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestEmbeddedEmptyPlaceholder()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// Content.<placeholder/>
+    /// </summary>
+    public class ClassName
+    {
+    }
+}";
+
+            // Empty placeholders are not altered by the current code fix.
+            var fixedCode = testCode;
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(4, 17);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestDeeplyEmbeddedPlaceholder()
+        {
+            var testCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// Content.
+    /// </summary>
+    /// <remarks>
+    /// <list type=""bullet"">
+    /// <item><placeholder>Nested content.</placeholder></item>
+    /// </list>
+    /// </remarks>
+    public class ClassName
+    {
+    }
+}";
+
+            var fixedCode = @"namespace FooNamespace
+{
+    /// <summary>
+    /// Content.
+    /// </summary>
+    /// <remarks>
+    /// <list type=""bullet"">
+    /// <item>Nested content.</item>
+    /// </list>
+    /// </remarks>
+    public class ClassName
+    {
+    }
+}";
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(8, 15);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestFormattingPreserved()
+        {
+            var testCode = @"namespace FooNamespace
+{
+   ///  <placeholder> <summary>
+     /// Content <placeholder
+            /// >.</placeholder>
+  ///</summary>  </placeholder
+///> <remarks/>
+    public class ClassName
+    {
+    }
+}";
+
+            var fixedCode = @"namespace FooNamespace
+{
+   ///   <summary>
+     /// Content .
+  ///</summary>   <remarks/>
+    public class ClassName
+    {
+    }
+}";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(3, 9),
+                this.CSharpDiagnostic().WithLocation(4, 18)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1651DoNotUsePlaceholderElements();
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SA1651CodeFixProvider();
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers/DocumentationRules/SA1651DoNotUsePlaceholderElements.cs
@@ -0,0 +1,83 @@
+﻿namespace StyleCop.Analyzers.DocumentationRules
+{
+    using System;
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// The element documentation for the element contains one or more &lt;placeholder&gt; elements.
+    /// </summary>
+    /// <remarks>
+    /// <para>A violation of this rule occurs when the element documentation contains &lt;placeholder&gt;
+    /// elements:</para>
+    ///
+    /// <code language="csharp">
+    /// /// &lt;summary&gt;
+    /// /// This method &lt;placeholder&gt;performs some operation&lt;/placeholder&gt;.
+    /// /// &lt;/summary&gt;
+    /// public void SomeOperation()
+    /// {
+    ///     ...
+    /// }
+    /// </code>
+    ///
+    /// <para>Placeholder elements should be reviewed and removed from documentation.</para>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SA1651DoNotUsePlaceholderElements : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The ID for diagnostics produced by the <see cref="SA1651DoNotUsePlaceholderElements"/>
+        /// analyzer.
+        /// </summary>
+        public const string DiagnosticId = "SA1651";
+        private const string Title = "Do not use placeholder elements";
+        private const string MessageFormat = "Do not use placeholder elements";
+        private const string Category = "StyleCop.CSharp.DocumentationRules";
+        private const string Description = "The element documentation contains a <placeholder> element.";
+        private const string HelpLink = null;
+
+        private static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description, HelpLink);
+
+        private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return SupportedDiagnosticsValue;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(this.HandleXmlElement, SyntaxKind.XmlElement);
+            context.RegisterSyntaxNodeAction(this.HandleXmlEmptyElement, SyntaxKind.XmlEmptyElement);
+        }
+
+        private void HandleXmlElement(SyntaxNodeAnalysisContext context)
+        {
+            XmlElementSyntax syntax = (XmlElementSyntax)context.Node;
+            if (!string.Equals("placeholder", syntax.StartTag?.Name?.ToString(), StringComparison.Ordinal))
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, syntax.GetLocation()));
+        }
+
+        private void HandleXmlEmptyElement(SyntaxNodeAnalysisContext context)
+        {
+            XmlEmptyElementSyntax syntax = (XmlEmptyElementSyntax)context.Node;
+            if (!string.Equals("placeholder", syntax.Name?.ToString(), StringComparison.Ordinal))
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, syntax.GetLocation()));
+        }
+    }
+}
