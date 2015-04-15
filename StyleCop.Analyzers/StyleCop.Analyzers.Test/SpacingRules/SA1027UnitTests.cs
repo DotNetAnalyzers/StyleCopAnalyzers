5622c6cda33704a4afbc84529d8223f240d9f6d0
vweijsters <vweijsters@outlook.com>
2015-04-15 23:01:14 +0200

    Implemented unit tests + code fix

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/SpacingRules/SA1027UnitTests.cs
@@ -0,0 +1,99 @@
+﻿namespace StyleCop.Analyzers.Test.SpacingRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.SpacingRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="SA1027TabsMustNotBeUsed"/>
+    /// </summary>
+
+    public class SA1027UnitTests : CodeFixVerifier
+    {
+        /// <summary>
+        /// Verifies that the analyzer will properly handle an empty source.
+        /// </summary>
+        [Fact]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Verifies that tabs used inside string and char literals are not producing diagnostics.
+        /// </summary>
+        [Fact]
+        public async Task TestValidTabs()
+        {
+            var testCode =
+                "public class Foo\r\n" +
+                "{\r\n" +
+                "    public const string ValidTestString = \"\tText\";\r\n" +
+                "    public const char ValidTestChar = '\t';\r\n" +
+                "}\r\n";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task TestInvalidTabs()
+        {
+            var testCode =
+                "using\tSystem.Diagnostics;\r\n" +
+                "\r\n" +
+                "public class Foo\r\n" +
+                "{\r\n" +
+                "\tpublic void Bar()\r\n" +
+                "\t{\r\n" +
+                "\t\t// Comment\r\n" +
+                "\t\tDebug.Indent();\r\n" +
+                "\t}\r\n" +
+                "}\r\n";
+
+            // There is only a single space between the using and the System.Diagnostics, 
+            // as the auto formatting that happens after a codefix will remove superfluous spaces.
+            var fixedTestCode = @"using System.Diagnostics;
+
+public class Foo
+{
+    public void Bar()
+    {
+        // Comment
+        Debug.Indent();
+    }
+}
+";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(1, 6),
+                this.CSharpDiagnostic().WithLocation(5, 1),
+                this.CSharpDiagnostic().WithLocation(6, 1),
+                this.CSharpDiagnostic().WithLocation(7, 1),
+                this.CSharpDiagnostic().WithLocation(8, 1),
+                this.CSharpDiagnostic().WithLocation(9, 1)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None);
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode);
+        }
+
+        /// <inheritdoc/>
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1027TabsMustNotBeUsed();
+        }
+
+        /// <inheritdoc/>
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SA1027CodeFixProvider();
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers/SpacingRules/SA1027CodeFixProvider.cs
@@ -0,0 +1,62 @@
+﻿namespace StyleCop.Analyzers.SpacingRules
+{
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.CSharp;
+
+    using StyleCop.Analyzers.Helpers;
+
+
+    /// <summary>
+    /// Implements a code fix for <see cref="SA1005SingleLineCommentsMustBeginWithSingleSpace"/>.
+    /// </summary>
+    [ExportCodeFixProvider(nameof(SA1027CodeFixProvider), LanguageNames.CSharp)]
+    [Shared]
+    public class SA1027CodeFixProvider : CodeFixProvider
+    {
+        private static readonly ImmutableArray<string> FixableDiagnostics =
+            ImmutableArray.Create(SA1027TabsMustNotBeUsed.DiagnosticId);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<string> FixableDiagnosticIds => FixableDiagnostics;
+
+        /// <inheritdoc/>
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        /// <inheritdoc/>
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            foreach (Diagnostic diagnostic in context.Diagnostics.Where(d => FixableDiagnostics.Contains(d.Id)))
+            {
+                context.RegisterCodeFix(CodeAction.Create("Replace tabs with spaces", token => GetTransformedDocumentAsync(context.Document, diagnostic, token)), diagnostic);
+            }
+
+            return Task.FromResult(true);
+        }
+
+        private static async Task<Document> GetTransformedDocumentAsync(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {
+            var indentationOptions = IndentationOptions.FromDocument(document);
+            var syntaxRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            var violatingTrivia = syntaxRoot.FindTrivia(diagnostic.Location.SourceSpan.Start);
+
+            var tabReplacement = new string(' ', indentationOptions.TabSize);
+            var replacementText = violatingTrivia.ToFullString().Replace("\t", tabReplacement);
+
+            var newSyntaxRoot = syntaxRoot.ReplaceTrivia(violatingTrivia, SyntaxFactory.Whitespace(replacementText));
+
+            return document.WithSyntaxRoot(newSyntaxRoot);
+        }
+    }
+}
