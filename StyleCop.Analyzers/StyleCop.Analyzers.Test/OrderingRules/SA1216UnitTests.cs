29d9f253270d6e8c9b17d960857b40203353c2b0
vweijsters <vweijsters@outlook.com>
2015-05-03 23:17:00 +0200

    Implemented SA1216 + unit tests

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1216UnitTests.cs
@@ -0,0 +1,125 @@
+﻿namespace StyleCop.Analyzers.Test.OrderingRules
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.OrderingRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="SA1216UsingStaticDirectivesMustBePlacedAtCorrectLocation"/>.
+    /// </summary>
+    public class SA1216UnitTests : CodeFixVerifier
+    {
+        /// <summary>
+        /// Verifies that the analyzer will properly handle an empty source.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestEmptySource()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Verifies that the analyzer will not produce diagnostics for correctly ordered using directives inside a namespace.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestValidUsingDirectivesInNamespace()
+        {
+            var testCode = @"namespace Foo
+{
+    using System;
+    using Execute = System.Action;
+    using static System.Math;
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Verifies that the analyzer will not produce diagnostics for correctly ordered using directives inside a namespace.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestValidUsingDirectivesInMultipleNamespaces()
+        {
+            var testCode = @"namespace Foo
+{
+    using System;
+    using Execute = System.Action;
+    using static System.Math;
+}
+
+namespace Bar
+{
+    using System;
+    using Execute = System.Action;
+    using static System.Math;
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Verifies that the analyzer will not produce diagnostics for correctly ordered using directives in the compilation unit.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestValidUsingDirectivesInCompilationUnit()
+        {
+            var testCode = @"using System;
+using Execute = System.Action;
+using static System.Math;
+
+public class Foo
+{
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Verifies that the analyzer will produce the proper diagnostics when the using directives are ordered wrong.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestInvalidUsingDirectivesOrdering()
+        {
+            var testCode = @"namespace Foo
+{
+    using static System.Math;
+    using Execute = System.Action;
+    using System;
+}
+
+namespace Bar
+{
+    using Execute = System.Action;
+    using static System.Math;
+    using System;
+}
+";
+
+            DiagnosticResult[] expectedDiagnostics =
+            {
+                this.CSharpDiagnostic().WithLocation(3, 5),
+                this.CSharpDiagnostic().WithLocation(11, 5)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expectedDiagnostics, CancellationToken.None);
+        }
+
+        /// <inheritdoc/>
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SA1216UsingStaticDirectivesMustBePlacedAtCorrectLocation();
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1216UsingStaticDirectivesMustBePlacedAtCorrectLocation.cs
@@ -0,0 +1,79 @@
+﻿namespace StyleCop.Analyzers.OrderingRules
+{
+    using System.Collections.Immutable;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// A static using directive is positioned at the wrong location.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A violation of this rule occurs when a using static directive is placed before a normal or an alias using directive. 
+    /// Placing the using static directives together below normal and alias using-directives can make the code cleaner and easier to read,
+    /// and can help make it easier to identify the static members used throughout the code.
+    /// </para>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SA1216UsingStaticDirectivesMustBePlacedAtCorrectLocation : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The ID for diagnostics produced by the <see cref="SA1216UsingStaticDirectivesMustBePlacedAtCorrectLocation"/> analyzer.
+        /// </summary>
+        public const string DiagnosticId = "SA1216";
+        private const string Title = "Using static directives must be placed at correct location";
+        private const string MessageFormat = "The using static directive must appear after regular or alias using directives";
+        private const string Category = "StyleCop.CSharp.OrderingRules";
+        private const string Description = "A using static directive is positioned before a regular or alias using directive.";
+        private const string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/wiki/SA1216";
+
+        private static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+
+        private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => SupportedDiagnosticsValue;
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionHonorExclusions(this.HandleUsingDirectiveSyntax, SyntaxKind.UsingDirective);
+        }
+
+        private void HandleUsingDirectiveSyntax(SyntaxNodeAnalysisContext context)
+        {
+            UsingDirectiveSyntax usingDirective = context.Node as UsingDirectiveSyntax;
+            if (usingDirective.StaticKeyword.IsKind(SyntaxKind.None))
+            {
+                return;
+            }
+
+            SyntaxList<UsingDirectiveSyntax> usingDirectives;
+            CompilationUnitSyntax compilationUnit = usingDirective.Parent as CompilationUnitSyntax;
+            if (compilationUnit != null)
+            {
+                usingDirectives = compilationUnit.Usings;
+            }
+            else
+            {
+                var namespaceDeclaration = (NamespaceDeclarationSyntax)usingDirective.Parent;
+                usingDirectives = namespaceDeclaration.Usings;
+            }
+
+            for (var i = usingDirectives.IndexOf(usingDirective) + 1; i < usingDirectives.Count; i++)
+            {
+                var currentUsingDirective = usingDirectives[i];
+                if (currentUsingDirective.StaticKeyword.IsKind(SyntaxKind.None))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, usingDirective.GetLocation()));
+                    break;
+                }
+            }
+        }
+    }
+}
