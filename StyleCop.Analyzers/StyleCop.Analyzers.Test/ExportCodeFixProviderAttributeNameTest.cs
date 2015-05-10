f7f6ab83a6d132c0d81e0d2818dc1f8d3ca1f609
Maciej Pakulski <pakulski.maciej@gmail.com>
2015-05-10 09:41:19 +0000

    Test if ExportCodeFixProviderAttribute is applied correctly - adjusted/corrected

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/ExportCodeFixProviderAttributeNameTest.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/ExportCodeFixProviderAttributeNameTest.cs
@@ -1,52 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StyleCop.Analyzers.ReadabilityRules;
-
-namespace StyleCop.Analyzers.Test
+﻿namespace StyleCop.Analyzers.Test
 {
-    [TestClass]
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using StyleCop.Analyzers.ReadabilityRules;
+    using Xunit;
+
     public class ExportCodeFixProviderAttributeNameTest
     {
-        [TestMethod]
-        public void Test()
+        public static IEnumerable<object[]> CodeFixProviderTypeData
         {
-            var issues = new List<string>();
-
-            var codeFixProviders = typeof(SA1110OpeningParenthesisMustBeOnDeclarationLine)
-                .Assembly
-                .GetExportedTypes()
-                .Where(t => typeof(CodeFixProvider).IsAssignableFrom(t))
-                .ToList();
-
-            foreach (var codeFixProvider in codeFixProviders)
+            get
             {
-                var exportCodeFixProviderAttribute = codeFixProvider.GetCustomAttributes(false)
-                    .OfType<ExportCodeFixProviderAttribute>()
-                    .FirstOrDefault();
-                if (exportCodeFixProviderAttribute == null)
-                {
-                    issues.Add(string.Format("{0} should have ExportCodeFixProviderAttribute attribute", codeFixProvider.Name));
-                    continue;
-                }
+                var codeFixProviders = typeof(SA1110OpeningParenthesisMustBeOnDeclarationLine)
+                    .Assembly
+                    .GetTypes()
+                    .Where(t => typeof(CodeFixProvider).IsAssignableFrom(t));
 
-                if (!string.Equals(exportCodeFixProviderAttribute.Name, codeFixProvider.Name,
-                        StringComparison.InvariantCulture))
-                {
-                    issues.Add(string.Format("Name parameter of ExportCodeFixProviderAttribute applied on {0} should be set to {0}", codeFixProvider.Name));
-                }
+                return codeFixProviders.Select(x => new[] { x });
             }
+        }
 
-            if (issues.Any())
-            {
-                Assert.Fail(string.Join("\r\t", issues));
-            }
+        [Theory]
+        [MemberData(nameof(CodeFixProviderTypeData))]
+        public void Test(Type codeFixProvider)
+        {
+            var exportCodeFixProviderAttribute = codeFixProvider.GetCustomAttributes<ExportCodeFixProviderAttribute>(false).FirstOrDefault();
+
+            Assert.NotNull(exportCodeFixProviderAttribute);
+            Assert.Equal(codeFixProvider.Name, exportCodeFixProviderAttribute.Name);
+            Assert.Equal(1, exportCodeFixProviderAttribute.Languages.Length);
+            Assert.Equal(LanguageNames.CSharp, exportCodeFixProviderAttribute.Languages[0]);
         }
     }
 }
