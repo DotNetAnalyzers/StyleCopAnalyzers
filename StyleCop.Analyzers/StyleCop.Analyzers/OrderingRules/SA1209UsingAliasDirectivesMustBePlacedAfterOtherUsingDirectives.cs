7ea1e23b29cab8c47293bc66e078eced9d9f36ff
baks <arkadiusz.komisarczuk@gmail.com>
2015-05-11 15:09:44 +0200

    SA1209 EnabledByDefault
    Changed SA1209 DiagnosticDescriptor to EnabledByDefault

--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1209UsingAliasDirectivesMustBePlacedAfterOtherUsingDirectives.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1209UsingAliasDirectivesMustBePlacedAfterOtherUsingDirectives.cs
@@ -34,7 +34,7 @@
         private const string HelpLink = "http://www.stylecop.com/docs/SA1209.html";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
             ImmutableArray.Create(Descriptor);
