63bb7ed2a8aacf9347572e0031b53a590db8c7c6
Dennis Fischer <fischer_dennis@live.de>
2015-05-24 18:30:01 +0200

    Implement SA1622 diagnostic + tests

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/DocumentationRules/SA1622UnitTests.cs
@@ -0,0 +1,229 @@
+﻿namespace StyleCop.Analyzers.Test.DocumentationRules
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Analyzers.DocumentationRules;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// This class contains unit tests for <see cref="SA1622GenericTypeParameterDocumentationMustHaveText"/>-
+    /// </summary>
+    public class SA1622UnitTests : DiagnosticVerifier
+    {
+        public static IEnumerable<object[]> Members
+        {
+            get
+            {
+                // These method names are chosen so that the position of the parameters are always the same. This makes testing easier
+                yield return new object[] { "void          Foo<Ta, Tb>() { }" };
+                yield return new object[] { "delegate void Foo<Ta, Tb>();" };
+                yield return new object[] { "void          Foo<Ta, T\\u0062>() { }" };
+                yield return new object[] { "delegate void Foo<Ta, T\\u0062>();" };
+            }
+        }
+
+        public static IEnumerable<object[]> Types
+        {
+            get
+            {
+                yield return new object[] { "class     Foo<Ta, Tb> { }" };
+                yield return new object[] { "struct    Foo<Ta, Tb> { }" };
+                yield return new object[] { "interface Foo<Ta, Tb> { }" };
+                yield return new object[] { "class     Foo<Ta, T\\u0062> { }" };
+                yield return new object[] { "struct    Foo<Ta, T\\u0062> { }" };
+                yield return new object[] { "interface Foo<Ta, T\\u0062> { }" };
+            }
+        }
+
+        [Fact]
+        public async Task TestEmptySourceAsync()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Members))]
+        public async Task TestMembersWithNoDocumentationAsync(string p)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    public ##
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("##", p), EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Types))]
+        public async Task TestTypesWithNoDocumentationAsync(string p)
+        {
+            var testCode = @"
+public ##";
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("##", p), EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestTypesWithoutTypeParametersAsync()
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class Foo { }";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Members))]
+        public async Task TestMembersWithAllDocumentationAsync(string p)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    /// <typeparam name=""Ta"">Param 1</param>
+    /// <typeparam name=""Tb"">Param 2</param>
+    public ##
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("##", p), EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Types))]
+        public async Task TestTypesWithAllDocumentationAsync(string p)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+/// <typeparam name=""Ta"">Param 1</param>
+/// <typeparam name=""Tb"">Param 2</param>
+public ##";
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("##", p), EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Members))]
+        public async Task TestMemberWithEmptyParamsAsync(string declaration)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    ///<typeparam name=""foo""></typeparam>
+    ///<typeparam name=""bar"">   
+
+    ///</typeparam>
+$$
+}";
+
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(10, 8),
+                this.CSharpDiagnostic().WithLocation(11, 8)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("$$", declaration), expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Types))]
+        public async Task TestTypesWithEmptyParamsAsync(string declaration)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+///<typeparam name=""foo""></typeparam>
+///<typeparam name=""bar"">   
+
+///</typeparam>
+public $$";
+
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(5, 4),
+                this.CSharpDiagnostic().WithLocation(6, 4)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("$$", declaration), expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Members))]
+        public async Task TestMemberWithEmptyParams2Async(string declaration)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+public class ClassName
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    ///<typeparam name=""foo""/>
+    ///<typeparam name=""bar"">
+    ///<para>
+    ///     
+    ///</para>
+    ///</typeparam>
+$$
+}";
+
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(10, 8),
+                this.CSharpDiagnostic().WithLocation(11, 8)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("$$", declaration), expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(Types))]
+        public async Task TestTypesWithEmptyParams2Async(string declaration)
+        {
+            var testCode = @"
+/// <summary>
+/// Foo
+/// </summary>
+///<typeparam name=""foo""/>
+///<typeparam name=""bar"">
+///<para>
+///     
+///</para>
+public $$";
+
+            var expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(5, 4),
+                this.CSharpDiagnostic().WithLocation(6, 4)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("$$", declaration), expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1622GenericTypeParameterDocumentationMustHaveText();
+        }
+    }
+}
