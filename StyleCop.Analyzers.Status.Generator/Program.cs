4a2b788ffb92ec63c3e9b510fb62553a7f857f01
Dennis Fischer <fischer_dennis@live.de>
2015-05-26 00:08:24 +0200

    Initial commit

---
+++ StyleCop.Analyzers.Status.Generator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StyleCop.Analyzers.Status.Generator
+{
+    /// <summary>
+    /// The starting point of this application.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The starting point of this application.
+        /// </summary>
+        /// <param name="args">The command line parameters.</param>
+        internal static void Main(string[] args)
+        {
+        }
+    }
+}
