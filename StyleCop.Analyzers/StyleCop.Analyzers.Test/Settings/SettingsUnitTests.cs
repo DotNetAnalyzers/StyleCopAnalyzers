11c14989f7fdd0d46718bd99257ece558a586779
vweijsters <vweijsters@outlook.com>
2015-05-27 23:19:49 +0200

    Implementing Settings helper classes

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/Settings/SettingsUnitTests.cs
@@ -0,0 +1,114 @@
+﻿namespace StyleCop.Analyzers.Test.Settings
+{
+    using System.Collections.Immutable;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.CodeAnalysis.Text;
+    using Xunit;
+
+    public class SettingsUnitTests
+    {
+        private const string TestProjectName = "TestProject";
+
+        /// <summary>
+        /// Verifies that proper defaults are returned when there are no additional files.
+        /// </summary>
+        [Fact]
+        public void VerifySettingsDefaults()
+        {
+            var styleCopSettings = SettingsHelper.GetStyleCopSettings(default(SyntaxTreeAnalysisContext));
+
+            Assert.Equal("PlaceHolderCompany", styleCopSettings.CompanyName);
+            Assert.Equal("Copyright (c) PlaceHolderCompany. All rights reserved.", styleCopSettings.CopyrightText);
+        }
+
+        /// <summary>
+        /// Verifies that the settings are properly read.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifySettingsAreReadCorrectlyAsync()
+        {
+            var settings = @"
+{
+  ""settings"": {
+    ""documentationRules"": {
+      ""companyName"": ""TestCompany"",
+      ""copyrightText"": ""Custom copyright text.""
+    }
+  }
+}
+";
+            var context = await CreateAnalysisContextAsync(settings).ConfigureAwait(false);
+
+            var styleCopSettings = context.GetStyleCopSettings();
+
+            Assert.Equal("TestCompany", styleCopSettings.CompanyName);
+            Assert.Equal("Custom copyright text.", styleCopSettings.CopyrightText);
+        }
+
+        /// <summary>
+        /// Verifies that the settings will use the read company name in the default copyright text.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifySettingsWillUseCompanyNameInDefaultCopyrightTextAsync()
+        {
+            var settings = @"
+{
+  ""settings"": {
+    ""documentationRules"": {
+      ""companyName"": ""TestCompany""
+    }
+  }
+}
+";
+            var context = await CreateAnalysisContextAsync(settings).ConfigureAwait(false);
+
+            var styleCopSettings = context.GetStyleCopSettings();
+
+            Assert.Equal("TestCompany", styleCopSettings.CompanyName);
+            Assert.Equal("Copyright (c) TestCompany. All rights reserved.", styleCopSettings.CopyrightText);
+        }
+
+        private static async Task<SyntaxTreeAnalysisContext> CreateAnalysisContextAsync(string stylecopJSON)
+        {
+            var projectId = ProjectId.CreateNewId();
+            var documentId = DocumentId.CreateNewId(projectId);
+
+            var solution = new AdhocWorkspace()
+                .CurrentSolution
+                .AddProject(projectId, TestProjectName, TestProjectName, LanguageNames.CSharp)
+                .AddDocument(documentId, "Test0.cs", SourceText.From(string.Empty));
+
+            var document = solution.GetDocument(documentId);
+            var syntaxTree = await document.GetSyntaxTreeAsync().ConfigureAwait(false);
+
+            var stylecopJSONFile = new AdditionalTextHelper("stylecop.json", stylecopJSON);
+            var additionalFiles = ImmutableArray.Create<AdditionalText>(stylecopJSONFile);
+            var analyzerOptions = new AnalyzerOptions(additionalFiles);
+
+            return new SyntaxTreeAnalysisContext(syntaxTree, analyzerOptions, rd => { }, isd => { return true; }, CancellationToken.None);
+        }
+
+        private class AdditionalTextHelper : AdditionalText
+        {
+            private SourceText sourceText;
+
+            public AdditionalTextHelper(string path, string text)
+            {
+                this.Path = path;
+                this.sourceText = SourceText.From(text);
+            }
+
+            public override string Path { get; }
+
+            public override SourceText GetText(CancellationToken cancellationToken = default(CancellationToken))
+            {
+                return this.sourceText;
+            }
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers/Settings/SettingsHelper.cs
@@ -0,0 +1,64 @@
+﻿namespace StyleCop.Analyzers
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.IO;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Class that manages the settings files for StyleCopAnalyzers.
+    /// </summary>
+    internal static class SettingsHelper
+    {
+        private const string SettingsFileName = "stylecop.json";
+        private const string DefaultCompanyName = "PlaceHolderCompany";
+
+        private const string SettingsToken = "settings";
+        private const string DocumentationRulesToken = "documentationRules";
+        private const string CompanyNameToken = "companyName";
+        private const string CopyrightTextToken = "copyrightText";
+
+        /// <summary>
+        /// Gets the stylecop settings.
+        /// </summary>
+        /// <param name="context">The context that will be used to determine the StyleCop settings.</param>
+        /// <returns>A <see cref="StyleCopSettings"/> instance that represents the StyleCop settings for the given context.</returns>
+        internal static StyleCopSettings GetStyleCopSettings(this SyntaxTreeAnalysisContext context)
+        {
+            return GetStyleCopSettings(context.Options != null ? context.Options.AdditionalFiles : ImmutableArray.Create<AdditionalText>());
+        }
+
+        private static StyleCopSettings GetStyleCopSettings(ImmutableArray<AdditionalText> additionalFiles)
+        {
+            try
+            {
+                foreach (var additionalFile in additionalFiles)
+                {
+                    if (Path.GetFileName(additionalFile.Path).ToLowerInvariant() == SettingsFileName)
+                    {
+                        var root = JObject.Parse(additionalFile.GetText().ToString());
+
+                        var companyName = (string)root.SelectToken($"{SettingsToken}.{DocumentationRulesToken}.{CompanyNameToken}") ?? DefaultCompanyName;
+                        var copyrightText = (string)root.SelectToken($"{SettingsToken}.{DocumentationRulesToken}.{CopyrightTextToken}") ?? GenerateDefaultCopyrightText(companyName);
+
+                        return new StyleCopSettings(companyName, copyrightText ?? GenerateDefaultCopyrightText(companyName));
+                    }
+                }
+            }
+            catch (JsonException)
+            {
+                // The settings file is invalid -> return the default settings.
+            }
+
+            return new StyleCopSettings(DefaultCompanyName, GenerateDefaultCopyrightText(DefaultCompanyName));
+        }
+
+        private static string GenerateDefaultCopyrightText(string companyName)
+        {
+            return $"Copyright (c) {companyName}. All rights reserved.";
+        }
+    }
+}
