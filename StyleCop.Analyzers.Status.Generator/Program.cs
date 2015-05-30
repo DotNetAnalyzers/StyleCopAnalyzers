fc5de2f84628e4d84f91ccf02003202917ff2cb3
Dennis Fischer <fischer_dennis@live.de>
2015-05-31 00:02:18 +0200

    Add git support

--- StyleCop.Analyzers.Status.Generator/Program.cs
+++ StyleCop.Analyzers.Status.Generator/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace StyleCop.Analyzers.Status.Generator
 {
     using System;
+    using System.IO;
+    using LibGit2Sharp;
     using Newtonsoft.Json;
 
     /// <summary>
@@ -22,7 +24,22 @@
 
             SolutionReader reader = SolutionReader.CreateAsync(args[0]).Result;
 
-            Console.WriteLine(JsonConvert.SerializeObject(reader.GetDiagnosticsAsync().Result));
+            var diagnostics = reader.GetDiagnosticsAsync().Result;
+
+            string commitId;
+
+            using (Repository repository = new Repository(Path.GetDirectoryName(args[0])))
+            {
+                commitId = repository.Head.Tip.Sha;
+            }
+
+            var output = new
+            {
+                diagnostics,
+                commitId
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(output));
         }
     }
 }
