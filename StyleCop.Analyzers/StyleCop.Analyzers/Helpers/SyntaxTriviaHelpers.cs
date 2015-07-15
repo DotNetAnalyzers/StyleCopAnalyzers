4c66ff1fd65d946e0db4c0a95b726be06198ef6c
Dennis Fischer <fischer_dennis@live.de>
2015-07-15 15:12:52 +0200

    Remove unnecessary using statement

--- StyleCop.Analyzers/StyleCop.Analyzers/Helpers/SyntaxTriviaHelpers.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/Helpers/SyntaxTriviaHelpers.cs
@@ -5,7 +5,6 @@
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
-    using Foo = Microsoft.CodeAnalysis.CSharp;
 
     internal static class SyntaxTreeHelpers
     {
