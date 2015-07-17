67c333d8a3b6afd6149c48676ab019e2f4973e66
Sam Harwell <sam@tunnelvisionlabs.com>
2015-07-16 20:48:25 -0500

    Add unit tests for SA1016 (OpeningAttributeBracketsMustBeSpacedCorrectly)
    Fixes #202

--- StyleCop.Analyzers/StyleCop.Analyzers/SpacingRules/SA1016OpeningAttributeBracketsMustBeSpacedCorrectly.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/SpacingRules/SA1016OpeningAttributeBracketsMustBeSpacedCorrectly.cs
@@ -29,7 +29,7 @@
         private static readonly string HelpLink = "http://www.stylecop.com/docs/SA1016.html";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.SpacingRules, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.SpacingRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
             ImmutableArray.Create(Descriptor);
