b8fae0e05f3862f28ca626db30940203110d140f
Sam Harwell <sam@tunnelvisionlabs.com>
2015-07-16 20:52:50 -0500

    Add unit tests for SA1017 (ClosingAttributeBracketsMustBeSpacedCorrectly)
    Fixes #203

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/SpacingRules/SA1017UnitTests.cs
@@ -0,0 +1,124 @@
+﻿namespace StyleCop.Analyzers.Test.SpacingRules
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.SpacingRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="SA1017ClosingAttributeBracketsMustBeSpacedCorrectly"/>.
+    /// </summary>
+    public class SA1017UnitTests : CodeFixVerifier
+    {
+        /// <summary>
+        /// Verifies that the analyzer will properly handle an empty source.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestEmptySourceAsync()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the analyzer will properly valid bracket placement.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestValidBracketsAsync()
+        {
+            var testCode = @"
+[System.Obsolete]
+class ClassName
+{
+}
+
+[System.Obsolete
+]
+class ClassName3
+{
+}
+
+[System.Obsolete
+ ]
+class ClassName4
+{
+}
+
+class ClassName5<[MyAttribute] T>
+{
+}
+
+class ClassName6<[MyAttribute
+] T>
+{
+    [return: MyAttribute]
+    int MethodName([MyAttribute] int x) { return 0; }
+}
+
+[System.AttributeUsage(System.AttributeTargets.All)]
+sealed class MyAttribute : System.Attribute { }
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the analyzer will properly report invalid bracket placements.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestInvalidBracketsAsync()
+        {
+            var testCode = @"
+[System.Obsolete ]
+class ClassName
+{
+}
+
+[System.Obsolete  ]
+class ClassName2
+{
+}
+";
+            var fixedCode = @"
+[System.Obsolete]
+class ClassName
+{
+}
+
+[System.Obsolete]
+class ClassName2
+{
+}
+";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(2, 18),
+                this.CSharpDiagnostic().WithLocation(7, 19),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1017ClosingAttributeBracketsMustBeSpacedCorrectly();
+        }
+
+        /// <inheritdoc/>
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SA1017CodeFixProvider();
+        }
+    }
+}
