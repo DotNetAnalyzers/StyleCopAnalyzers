2024e3357ee2bc2fe1fb1f0e0a7b1ac59c8b53af
Sam Harwell <sam@tunnelvisionlabs.com>
2015-07-20 08:50:28 -0500

    Fix violations of SA1210 (UsingDirectivesMustBeOrderedAlphabeticallyByNamespace)

--- StyleCop.Analyzers/StyleCop.Analyzers/DocumentationRules/SA1602EnumerationItemsMustBeDocumented.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/DocumentationRules/SA1602EnumerationItemsMustBeDocumented.cs
@@ -1,11 +1,11 @@
 ﻿namespace StyleCop.Analyzers.DocumentationRules
 {
-    using System.Linq;
     using System.Collections.Immutable;
+    using System.Linq;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.Diagnostics;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
     using StyleCop.Analyzers.Helpers;
 
     /// <summary>
