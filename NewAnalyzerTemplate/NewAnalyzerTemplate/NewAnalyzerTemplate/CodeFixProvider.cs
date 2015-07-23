dcd7c2cd42d28f5499033aaa8b8d4c28099d09d8
zoepetard <t-zopet@microsoft.com>
2015-07-23 16:53:17 -0700

    Comments in codefix template

--- NewAnalyzerTemplate/NewAnalyzerTemplate/NewAnalyzerTemplate/CodeFixProvider.cs
+++ NewAnalyzerTemplate/NewAnalyzerTemplate/NewAnalyzerTemplate/CodeFixProvider.cs
@@ -26,6 +26,8 @@ namespace NewAnalyzerTemplate
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(NewAnalyzerTemplateCodeFixProvider)), Shared]
     public class NewAnalyzerTemplateCodeFixProvider : CodeFixProvider
     {
+        // In this tutorial, FixableDiagnosticIds should return the diagnostic ID defined in DiagnosticAnalyzer.cs.
+        // However, it is possible to provide fixes for diagnostics coming from other analyzers.
         public override ImmutableArray<string> FixableDiagnosticIds
         {
             get
@@ -41,13 +43,18 @@ namespace NewAnalyzerTemplate
 
         public override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
+            // This statement gets the top of the syntax tree of the file on which the analyzer is running.
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
+            // This tutorial only includes one diagnostic, therefore it suffices to take the first diagnostic from the context.
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
+            // The node this code fix aims to fix is of type 'IfStatementSyntax'.
+            // The following statement extracts the node of type IfStatementSyntax closest to the reported diagnostic's location.
             var ifStatement = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<IfStatementSyntax>().First();
 
+            // The IfStatementSyntax node is now passed to a method, FixSpacingAsync, that will generate the suggested new code.
             context.RegisterCodeFix(
                 CodeAction.Create("Fix spacing", c => FixSpacingAsync(context.Document, ifStatement, c)),
                 diagnostic);
@@ -55,19 +62,30 @@ namespace NewAnalyzerTemplate
 
         private async Task<Document> FixSpacingAsync(Document document, IfStatementSyntax ifStatement, CancellationToken c)
         {
+            // This method will generate a new if-statement node with a single space between the if-keyword and the opening parenthesis.
             var generator = SyntaxGenerator.GetGenerator(document);
         
-            var ifKeyword = ifStatement.IfKeyword;
+            // The new if-statement will need to retain the same statements as the old if-statement.
             var ifBlock = ifStatement.Statement as BlockSyntax;
-            var closeBrace = ifBlock.CloseBraceToken;
             var ifBlockStatements = ifBlock.Statements;
-        
+
+            // The new if-statement should retain the formatting of the trivia before and after the if-statement.
+            // The following statements extract the original trivia.
+            var ifKeyword = ifStatement.IfKeyword;
             var leadingTrivia = ifKeyword.LeadingTrivia;
+            var closeBrace = ifBlock.CloseBraceToken;
             var trailingTrivia = closeBrace.TrailingTrivia;
+
+            // If-statements generated using SyntaxGenerator are formatted with the desired spacing by default so this is not specified explicitly.
             var newIfStatement = generator.IfStatement(ifStatement.Condition, ifBlockStatements).WithLeadingTrivia(leadingTrivia).WithTrailingTrivia(trailingTrivia);
-        
+
+            // This statement gets the top of the syntax tree so that the old if-statement node can be replaced with the new one.
             var root = await document.GetSyntaxRootAsync();
+
+            // A new root is created with the old if-statement replaced by the new one.
             var newRoot = root.ReplaceNode(ifStatement, newIfStatement);
+
+            // A new document with the new root is returned.
             var newDocument = document.WithSyntaxRoot(newRoot);
             return newDocument;
     }
