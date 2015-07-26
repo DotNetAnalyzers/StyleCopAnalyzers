76618d1d8e4632deb5cf96fe8c0803d8dec744e9
Dennis Fischer <fischer_dennis@live.de>
2015-07-26 22:35:39 +0200

    Update RegionFixAllProvider

--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/RemoveRegionFixAllProvider.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/RemoveRegionFixAllProvider.cs
@@ -2,10 +2,10 @@
 {
     using System.Linq;
     using System.Threading.Tasks;
+    using Helpers;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CodeFixes;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
-    using Helpers;
 
     internal sealed class RemoveRegionFixAllProvider : DocumentBasedFixAllProvider
     {
@@ -13,24 +13,16 @@
 
         protected override async Task<SyntaxNode> FixAllInDocumentAsync(FixAllContext fixAllContext, Document document)
         {
-            try
-            {
-                var diagnostics = await fixAllContext.GetDocumentDiagnosticsAsync(document).ConfigureAwait(false);
-                SyntaxNode root = await document.GetSyntaxRootAsync().ConfigureAwait(false);
+            var diagnostics = await fixAllContext.GetDocumentDiagnosticsAsync(document).ConfigureAwait(false);
+            SyntaxNode root = await document.GetSyntaxRootAsync().ConfigureAwait(false);
 
-                var nodesToRemove = diagnostics.Select(d => root.FindNode(d.Location.SourceSpan, findInsideTrivia: true))
-                    .Where(node => node != null && !node.IsMissing)
-                    .OfType<RegionDirectiveTriviaSyntax>()
-                    .SelectMany(node => node.GetRelatedDirectives())
-                    .Where(node => !node.IsMissing);
+            var nodesToRemove = diagnostics.Select(d => root.FindNode(d.Location.SourceSpan, findInsideTrivia: true))
+                .Where(node => node != null && !node.IsMissing)
+                .OfType<RegionDirectiveTriviaSyntax>()
+                .SelectMany(node => node.GetRelatedDirectives())
+                .Where(node => !node.IsMissing);
 
-                return root.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.AddElasticMarker);
-            }
-            catch (System.Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex);
-                return null;
-            }
+            return root.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.AddElasticMarker);
         }
     }
 }
