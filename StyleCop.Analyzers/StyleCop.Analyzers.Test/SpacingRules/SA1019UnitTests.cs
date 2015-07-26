6f985a4df112e132f1760c038edc219eb4d4db65
Matt Bird <matthew.bird@icarehealth.co.uk>
2015-07-26 10:39:55 +0100

    Added some additional tests to handle comments on either side of the member access operator.

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/SpacingRules/SA1019UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/SpacingRules/SA1019UnitTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -19,6 +20,19 @@
     public class SA1019UnitTests : CodeFixVerifier
     {
         /// <summary>
+        /// The members access operators to test.
+        /// </summary>
+        /// <value>The members access operators to test.</value>
+        public static IEnumerable<object[]> Operators
+        {
+            get
+            {
+                yield return new object[] { "." };
+                yield return new object[] { "?." };
+            }
+        }
+
+        /// <summary>
         /// Verifies that the analyzer will properly handle an empty source.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
@@ -30,72 +44,122 @@
         }
 
         /// <summary>
-        /// Asserts that a space before a period member access symbol reports correctly.
+        /// Asserts that a space before a member access operator reports correctly.
         /// </summary>
+        /// <param name="op">The member access operator.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestSpaceBeforeDotAsync()
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestSpaceBeforeOperatorAsync(string op)
         {
-            string template = this.GetTemplate(" .");
-            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 27).WithArguments(".", "preceded");
+            string template = this.GetTemplate($" {op}");
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 27).WithArguments(op[0], "preceded");
             await this.VerifyCSharpDiagnosticAsync(template, expected, CancellationToken.None).ConfigureAwait(false);
         }
 
         /// <summary>
-        /// Asserts that a space after a period member access symbol reports correctly.
+        /// Asserts that a space after a member access operator reports correctly.
         /// </summary>
+        /// <param name="op">The member access operator.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestSpaceAfterDotAsync()
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestSpaceAfterOperatorAsync(string op)
         {
-            string template = this.GetTemplate(". ");
-            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 26).WithArguments(".", "followed");
+            string template = this.GetTemplate($"{op} ");
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 25 + op.Length).WithArguments(op.Last(), "followed");
             await this.VerifyCSharpDiagnosticAsync(template, expected, CancellationToken.None).ConfigureAwait(false);
         }
 
         /// <summary>
-        /// Asserts that a space before a null conditional access symbol reports correctly.
+        /// Asserts that a member access operator at the end of a line does not report.
         /// </summary>
+        /// <param name="op">The member access operator.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestSpaceBeforeNullConditionalAsync()
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestOperatorAtEndOfLineDoesNotReportAsync(string op)
         {
-            string template = this.GetTemplate(" ?.");
-            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 27).WithArguments("?", "preceded");
-            await this.VerifyCSharpDiagnosticAsync(template, expected, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Asserts that a space after a null conditional access symbol reports correctly.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestSpaceAfterNullConditionalAsync()
-        {
-            string template = this.GetTemplate("?. ");
-            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 27).WithArguments(".", "followed");
-            await this.VerifyCSharpDiagnosticAsync(template, expected, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Asserts that a dot at the end of a line does not report.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestDotAtEndOfLineDoesNotReportAsync()
-        {
-            string template = this.GetTemplate($".{Environment.NewLine}");
+            string template = this.GetTemplate($"{op}{Environment.NewLine}");
             await this.VerifyCSharpDiagnosticAsync(template, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
         }
 
         /// <summary>
-        /// Asserts that a dot at the start of a line does not report.
+        /// Asserts that a member access operator at the start of a line does not report.
         /// </summary>
+        /// <param name="op">The member access operator.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestDotAtStartOfLineDoesNotReportAsync()
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestOperatorAtStartOfLineDoesNotReportAsync(string op)
         {
-            string template = this.GetTemplate($"{Environment.NewLine}.");
+            string template = this.GetTemplate($"{Environment.NewLine}{op}");
+            await this.VerifyCSharpDiagnosticAsync(template, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that block comments either side of the operator does not report a diagnostic.
+        /// </summary>
+        /// <param name="op">The member access operator.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestBlockCommentsEitherSideOfOperatorDoesNotReportAsync(string op)
+        {
+            string template = this.GetTemplate($"/**/{op}/**/");
+            await this.VerifyCSharpDiagnosticAsync(template, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that a comment on the line preceding the operator does not report.
+        /// </summary>
+        /// <param name="op">The member access operator.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestCommentOnLinePrecedingOperatorDoesNotReportAsync(string op)
+        {
+            string template = this.GetTemplate($"// This is a comment{Environment.NewLine}{op}");
+            await this.VerifyCSharpDiagnosticAsync(template, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that a comment on the line following the operator does not report.
+        /// </summary>
+        /// <param name="op">The member access operator.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestCommentOnLineFollowingOperatorDoesNotReportAsync(string op)
+        {
+            string template = this.GetTemplate($"{op}{Environment.NewLine}// This is a comment{Environment.NewLine}");
+            await this.VerifyCSharpDiagnosticAsync(template, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that a comment on the same line as the operator separated by whitespace does report a diagnostic.
+        /// </summary>
+        /// <param name="op">The member access operator.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestCommentOnSameLineSeparatedByWhitespaceReportsAsync(string op)
+        {
+            string template = this.GetTemplate($"{op} // This is a comment{Environment.NewLine}");
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(16, 25 + op.Length).WithArguments(".", "followed");
+            await this.VerifyCSharpDiagnosticAsync(template, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that a comment on the same line as and immediately after the operator does report a diagnostic.
+        /// </summary>
+        /// <param name="op">The member access operator.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(Operators))]
+        public async Task TestCommentOnSameLineImmediatelyAfterOperatorDoesNotReportsAsync(string op)
+        {
+            string template = this.GetTemplate($"{op}// This is a comment{Environment.NewLine}");
             await this.VerifyCSharpDiagnosticAsync(template, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
         }
 
