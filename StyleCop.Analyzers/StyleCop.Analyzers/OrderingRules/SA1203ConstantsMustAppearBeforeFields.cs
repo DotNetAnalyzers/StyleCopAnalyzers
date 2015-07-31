23910ad936f37b156fac0ad836419a35ce03dc85
johnkoerner <github@riddlepop.com>
2015-07-30 23:40:03 -0400

    Implementing SA1203 Analyzer.  Fixes #63

--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1203ConstantsMustAppearBeforeFields.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1203ConstantsMustAppearBeforeFields.cs
@@ -2,7 +2,10 @@
 {
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
+    using Helpers;
 
     /// <summary>
     /// A constant field is placed beneath a non-constant field.
@@ -20,12 +23,12 @@
         /// </summary>
         public const string DiagnosticId = "SA1203";
         private const string Title = "Constants must appear before fields";
-        private const string MessageFormat = "TODO: Message format";
+        private const string MessageFormat = "Constants must appear before fields";
         private const string Description = "A constant field is placed beneath a non-constant field.";
         private const string HelpLink = "http://www.stylecop.com/docs/SA1203.html";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.OrderingRules, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.OrderingRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
             ImmutableArray.Create(Descriptor);
@@ -42,7 +45,50 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeActionHonorExclusions(this.HandleTypeDeclaration, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeActionHonorExclusions(this.HandleTypeDeclaration, SyntaxKind.StructDeclaration);
+        }
+
+        private void HandleTypeDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var typeDeclaration = context.Node as TypeDeclarationSyntax;
+            var members = typeDeclaration.Members;
+            bool nonConstFieldFound = false;
+
+            for (int i = 0; i < members.Count; i++)
+            {
+                if (members[i].IsKind(SyntaxKind.IncompleteMember))
+                {
+                    continue;
+                }
+
+                if (!members[i].IsKind(SyntaxKind.FieldDeclaration))
+                {
+                    continue;
+                }
+
+                var field = members[i] as FieldDeclarationSyntax;
+
+                bool thisFieldIsConstant = false;
+                foreach (var modifier in field.Modifiers)
+                {
+                    if (modifier.IsKind(SyntaxKind.ConstKeyword))
+                    {
+                        thisFieldIsConstant = true;
+                        break;
+                    }
+                }
+
+                if (!thisFieldIsConstant)
+                {
+                    nonConstFieldFound = true;
+                }
+
+                if (thisFieldIsConstant && nonConstFieldFound)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, NamedTypeHelpers.GetNameOrIdentifierLocation(members[i])));
+                }
+            }
         }
     }
 }
