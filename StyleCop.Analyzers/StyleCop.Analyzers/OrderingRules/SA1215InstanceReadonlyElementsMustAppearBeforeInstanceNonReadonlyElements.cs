d35f153bbf7e0ea982bc24a637059e38c7298399
John Venable <john@revenflo.com>
2015-08-02 20:39:46 -0400

    Implement SA1215 with tests

--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1215InstanceReadonlyElementsMustAppearBeforeInstanceNonReadonlyElements.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1215InstanceReadonlyElementsMustAppearBeforeInstanceNonReadonlyElements.cs
@@ -1,8 +1,12 @@
 ﻿namespace StyleCop.Analyzers.OrderingRules
 {
     using System.Collections.Immutable;
+    using System.Linq;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.Helpers;
 
     /// <summary>
     /// An instance readonly element is positioned beneath an instance non-readonly element of the same type.
@@ -20,12 +24,12 @@
         /// </summary>
         public const string DiagnosticId = "SA1215";
         private const string Title = "Instance readonly elements must appear before instance non-readonly elements";
-        private const string MessageFormat = "TODO: Message format";
+        private const string MessageFormat = "Instance readonly elements must appear before instance non-readonly elements.";
         private const string Description = "An instance readonly element is positioned beneath an instance non-readonly element of the same type.";
         private const string HelpLink = "http://www.stylecop.com/docs/SA1215.html";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.OrderingRules, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.OrderingRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
             ImmutableArray.Create(Descriptor);
@@ -42,7 +46,22 @@
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleTypeDeclaration, SyntaxKind.ClassDeclaration, SyntaxKind.StructDeclaration);
+        }
+
+        private static void HandleTypeDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var typeDeclaration = (TypeDeclarationSyntax)context.Node;
+
+            var instanceFields = typeDeclaration.Members.OfType<FieldDeclarationSyntax>().Where(f => !f.Modifiers.Any(SyntaxKind.StaticKeyword)).ToList();
+
+            for (int i = 0; i < instanceFields.Count - 1; ++i)
+            {
+                if (instanceFields[i + 1].Modifiers.Any(SyntaxKind.ReadOnlyKeyword) && !instanceFields[i].Modifiers.Any(SyntaxKind.ReadOnlyKeyword))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, NamedTypeHelpers.GetNameOrIdentifierLocation(instanceFields[i + 1])));
+                }
+            }
         }
     }
 }
