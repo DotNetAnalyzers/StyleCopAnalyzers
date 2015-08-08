8984ca0fce51309f9e32473d29aea216fb6f54c7
connected-web-development <jwvanbakel@gmail.com>
2015-08-08 14:40:18 -0700

    Merged second commit

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1203UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1203UnitTests.cs
@@ -14,80 +14,12 @@
         [Fact]
         public async Task TestNoDiagnosticAsync()
         {
-            var testCode = @"public static class TestClass1 { }
-
-public class TestClass2
+            var testCode = @"
+public class Foo
 {
-    public const int TestField1 = 1;
-    public static readonly int TestField2 = 1;
-    public static int TestField3 = 1;
-    public readonly int TestField4 = 1;
-    public int TestField5 = 1;
-    internal const int TestField6 = 1;
-    internal static readonly int TestField7 = 1;
-    internal static int TestField8 = 1;
-    internal readonly int TestField9 = 1;
-    internal int TestField10 = 1;
-    protected internal const int TestField11 = 1;
-    protected internal static readonly int TestField12 = 1;
-    protected internal static int TestField13 = 1;
-    protected internal readonly int TestField14 = 1;
-    protected internal int TestField15 = 1;
-    protected const int TestField16 = 1;
-    protected static readonly int TestField17 = 1;
-    protected static int TestField18 = 1;
-    protected readonly int TestField19 = 1;
-    protected int TestField20 = 1;
-    private const int TestField21 = 1;
-    private static readonly int TestField22 = 1;
-    private static int TestField23 = 1;
-    private readonly int TestField24 = 1;
-    private int TestField25 = 1;
-
-    public TestClass2()
-    {
-    }
-
-    private TestClass2(string a)
-    {
-    }
-
-    ~TestClass2() { }
-    
-    public static int TestProperty1 { get; set; }
-    public int TestProperty2 { get; set; }
-    internal static int TestProperty3 { get; set; }
-    internal int TestProperty4 { get; set; }
-    protected internal static int TestProperty5 { get; set; }
-    protected internal int TestProperty6 { get; set; }
-    protected static int TestProperty7 { get; set; }
-    protected int TestProperty8 { get; set; }
-    private static int TestProperty9 { get; set; }
-    private int TestProperty10 { get; set; }
-    
-    public static void TestMethod1() { }
-    public void TestMethod2() { }
-    internal static void TestMethod3() { }
-    internal void TestMethod4() { }
-    protected internal static void TestMethod5() { }
-    protected internal void TestMethod6() { }
-    protected static void TestMethod7() { }
-    protected void TestMethod8() { }
-    private static void TestMethod9() { }
-    private void TestMethod10() { }
-
-    public static class TestClass1 { }
-    public class TestClass2a { }
-    internal static class TestClass3 { }
-    internal class TestClass4 { }
-    protected internal static class TestClass5 { }
-    protected internal class TestClass6 { }
-    protected static class TestClass7 { }
-    protected class TestClass8 { }
-    private static class TestClass9 { }
-    private class TestClass10 { }
-}
-";
+    private const int Bar = 2;
+    private int Baz = 1;
+}";
 
             await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
         }
@@ -101,7 +33,7 @@ public class Foo
     private int Baz = 1;
     private const int Bar = 2;
 }";
-            var firstDiagnostic = this.CSharpDiagnostic().WithLocation(5, 23).WithArguments("private");
+            var firstDiagnostic = this.CSharpDiagnostic().WithLocation(5, 23);
             await this.VerifyCSharpDiagnosticAsync(testCode, firstDiagnostic, CancellationToken.None).ConfigureAwait(false);
 
             var fixedTestCode = @"
@@ -122,7 +54,7 @@ public struct Foo
     private int baz;
     private const int Bar = 2;
 }";
-            var firstDiagnostic = this.CSharpDiagnostic().WithLocation(5, 23).WithArguments("private");
+            var firstDiagnostic = this.CSharpDiagnostic().WithLocation(5, 23);
             await this.VerifyCSharpDiagnosticAsync(testCode, firstDiagnostic, CancellationToken.None).ConfigureAwait(false);
 
             var fixedTestCode = @"
@@ -144,7 +76,7 @@ public class Foo
     private int Baz = 1;
     private const int FooBar = 2;
 }";
-            var firstDiagnostic = this.CSharpDiagnostic().WithLocation(6, 23).WithArguments("private");
+            var firstDiagnostic = this.CSharpDiagnostic().WithLocation(6, 23);
             await this.VerifyCSharpDiagnosticAsync(testCode, firstDiagnostic, CancellationToken.None).ConfigureAwait(false);
 
             var fixedTestCode = @"
@@ -157,23 +89,6 @@ public class Foo
             await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
         }
 
-        [Fact]
-
-        public async Task TestUnqualifiedConstAsync()
-        {
-            var testCode = @"
-public class Test
-{
-    private int Test1 = 1;
-    const int Test2 = 2;
-    const int Test3 = 3;
-}";
-
-            var expected = this.CSharpDiagnostic().WithLocation(5, 15).WithArguments("private");
-
-            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
-        }
-        
         /// <summary>
         /// Verifies that the code fix will move the non constant fields before the constant ones.
         /// </summary>
