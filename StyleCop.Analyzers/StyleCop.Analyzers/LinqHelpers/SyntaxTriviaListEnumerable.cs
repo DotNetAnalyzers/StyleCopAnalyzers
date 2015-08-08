07adff8e19bf8c73bf40b28fd962ae97791cdca6
Sam Harwell <sam@tunnelvisionlabs.com>
2015-08-08 10:17:06 -0500

    Avoid boxing SyntaxTriviaList for calls to Contains()

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/LinqHelpers/SyntaxTriviaListEnumerable.cs
@@ -0,0 +1,38 @@
+﻿namespace System.Linq
+{
+    using Collections.Generic;
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// This class supports a subset of LINQ operations on <see cref="SyntaxTriviaList"/> without requiring boxing of
+    /// operands as an <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    internal static class SyntaxTriviaListEnumerable
+    {
+        /// <summary>
+        /// Determines if a <see cref="SyntaxTriviaList"/> contains a specific <see cref="SyntaxTrivia"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method allows callers to avoid boxing the <see cref="SyntaxTriviaList"/> as an
+        /// <see cref="IEnumerable{T}"/>.</para>
+        /// </remarks>
+        /// <param name="list">The source list.</param>
+        /// <param name="trivia">The element to look for in the list.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="list"/> contains <paramref name="trivia"/>; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        internal static bool Contains(this SyntaxTriviaList list, SyntaxTrivia trivia)
+        {
+            foreach (SyntaxTrivia item in list)
+            {
+                if (item.Equals(trivia))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
