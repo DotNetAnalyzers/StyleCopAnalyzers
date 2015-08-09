9315d6008da091a4f43ab9d68176ba7f433f875c
John Venable <john@revenflo.com>
2015-08-09 17:41:26 -0400

    Fix order of using directives should reset at preprocessor directives

--- StyleCop.Analyzers/StyleCop.Analyzers/Helpers/UsingDirectiveSyntaxHelpers.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/Helpers/UsingDirectiveSyntaxHelpers.cs
@@ -20,6 +20,44 @@
         /// <returns>Return true if the <see cref="UsingDirectiveSyntax"/>is system using directive, otherwise false.</returns>
         internal static bool IsSystemUsingDirective(this UsingDirectiveSyntax usingDirective) => string.Equals(SystemUsingDirectiveIdentifier, GetFirstIdentifierInUsingDirective(usingDirective)?.Text, StringComparison.Ordinal);
 
+        /// <summary>
+        /// Check if <see cref="UsingDirectiveSyntax"/> is preceded by a preprocessor directive.
+        /// </summary>
+        /// <param name="usingDirective">The using directive.</param>
+        /// <returns>True if the <see cref="UsingDirectiveSyntax"/> is preceded by a preprocessor directive, otherwise false.</returns>
+        internal static bool IsPrecededByPreprocessorDirective(this UsingDirectiveSyntax usingDirective)
+        {
+            if (!usingDirective.HasLeadingTrivia)
+            {
+                return false;
+            }
+
+            foreach (var trivia in usingDirective.GetLeadingTrivia())
+            {
+                switch (trivia.Kind())
+                {
+                case SyntaxKind.IfDirectiveTrivia:
+                case SyntaxKind.ElifDirectiveTrivia:
+                case SyntaxKind.ElseDirectiveTrivia:
+                case SyntaxKind.EndIfDirectiveTrivia:
+                case SyntaxKind.RegionDirectiveTrivia:
+                case SyntaxKind.EndRegionDirectiveTrivia:
+                case SyntaxKind.DefineDirectiveTrivia:
+                case SyntaxKind.UndefDirectiveTrivia:
+                case SyntaxKind.ErrorDirectiveTrivia:
+                case SyntaxKind.WarningDirectiveTrivia:
+                case SyntaxKind.LineDirectiveTrivia:
+                case SyntaxKind.PragmaWarningDirectiveTrivia:
+                case SyntaxKind.PragmaChecksumDirectiveTrivia:
+                case SyntaxKind.ReferenceDirectiveTrivia:
+                case SyntaxKind.BadDirectiveTrivia:
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         private static bool ExcludeGlobalKeyword(IdentifierNameSyntax token) => !token.Identifier.IsKind(SyntaxKind.GlobalKeyword);
 
         private static SyntaxToken? GetFirstIdentifierInUsingDirective(UsingDirectiveSyntax usingDirective) => usingDirective.DescendantNodes().OfType<IdentifierNameSyntax>().FirstOrDefault(ExcludeGlobalKeyword)?.Identifier;
