e7e7796ec341f753ae68f5f7817290a76d52b7cb
Sam Harwell <sam@tunnelvisionlabs.com>
2015-08-16 15:43:57 -0500

    Prepare for next development iteration

--- StyleCop.Analyzers/StyleCop.Analyzers/Properties/AssemblyInfo.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta007")]
+[assembly: AssemblyInformationalVersion("1.0.0-dev")]
