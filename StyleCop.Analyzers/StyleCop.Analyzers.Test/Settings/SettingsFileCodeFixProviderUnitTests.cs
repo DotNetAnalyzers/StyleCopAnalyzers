a3eb8633f3d4345b798fdfc2f2ce4c79f27998e7
vweijsters <vweijsters@outlook.com>
2015-08-19 22:38:58 +0200

    Initial implementation

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/Settings/SettingsFileCodeFixProviderUnitTests.cs
@@ -0,0 +1,96 @@
+﻿namespace StyleCop.Analyzers.Test.Settings
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.DocumentationRules;
+    using StyleCop.Analyzers.Settings;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for the <see cref="SettingsFileCodeFixProvider"/>
+    /// </summary>
+    public class SettingsFileCodeFixProviderUnitTests : CodeFixVerifier
+    {
+        private const string StyleCopSettingsFileName = "stylecop.json";
+        private const string TestCode = @"
+namespace Foo
+{
+}
+";
+
+        private Project originalProject;
+        private bool createSettingsFile;
+
+        /// <summary>
+        /// Verifies that a file without a header, but with leading trivia will produce the correct diagnostic message.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestMissingFileHeaderWithLeadingTriviaAsync()
+        {
+            this.createSettingsFile = false;
+
+            var expectedDiagnostic = this.CSharpDiagnostic().WithLocation(1, 1).WithArguments("is missing or not located at the top of the file.");
+            await this.VerifyCSharpDiagnosticAsync(TestCode, expectedDiagnostic, CancellationToken.None).ConfigureAwait(false);
+
+            // verify that the codefix does not alter the document
+            await this.VerifyCSharpFixAsync(TestCode, TestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that a code fix will be offered if the settings file does not exist.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestSettingsFileDoesNotExistAsync()
+        {
+            this.createSettingsFile = false;
+
+            var offeredFixes = await this.GetOfferedCSharpFixesAsync(TestCode).ConfigureAwait(false);
+            Assert.Equal(1, offeredFixes.Length);
+        }
+
+        /// <summary>
+        /// Verifies that a code fix will not be offered if the settings file is already present.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestSettingsFileAlreadyExistsAsync()
+        {
+            this.createSettingsFile = true;
+
+            var offeredFixes = await this.GetOfferedCSharpFixesAsync(TestCode).ConfigureAwait(false);
+            Assert.Empty(offeredFixes);
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1633FileMustHaveHeader();
+        }
+
+        /// <inheritdoc/>
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new SettingsFileCodeFixProvider();
+        }
+
+        /// <inheritdoc/>
+        protected override Project CreateProject(string[] sources, string language = "C#", string[] filenames = null)
+        {
+            this.originalProject = base.CreateProject(sources, language, filenames);
+            if (this.createSettingsFile)
+            {
+                this.originalProject = this.originalProject.AddAdditionalDocument(StyleCopSettingsFileName, string.Empty).Project;
+            }
+
+            return this.originalProject;
+        }
+    }
+}
