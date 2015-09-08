71637b472ee5e25cc92f5a96ebf3210d4e17d42d
Hans van Bakel <jwvanbakel@gmail.com>
2015-09-08 09:04:57 -0700

    Reused TriviaHelper function

--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1212SA1213CodeFixProvider.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1212SA1213CodeFixProvider.cs
@@ -79,7 +79,7 @@
 
         private static AccessorDeclarationSyntax GetNewAccessor(AccessorListSyntax accessorList, AccessorDeclarationSyntax firstAccessor, AccessorDeclarationSyntax secondAccessor)
         {
-            var newLeadingTrivia = GetLeadingTriviaWithoutLeadingBlankLines(secondAccessor);
+            var newLeadingTrivia = secondAccessor.GetFirstToken().WithoutLeadingBlankLines().LeadingTrivia;
             if (AccessorsAreOnTheSameLine(firstAccessor, secondAccessor))
             {
                 var leadingWhitespace = firstAccessor.GetLeadingTrivia().Where(x => x.IsKind(SyntaxKind.WhitespaceTrivia)).ToList();
@@ -96,29 +96,6 @@
             }
 
             return newAccessor;
-        }
-
-        private static SyntaxTriviaList GetLeadingTriviaWithoutLeadingBlankLines(AccessorDeclarationSyntax secondAccessor)
-        {
-            var leadingTrivia = secondAccessor.GetLeadingTrivia();
-
-            var skipIndex = 0;
-            for (var i = 0; i < leadingTrivia.Count; i++)
-            {
-                var currentTrivia = leadingTrivia[i];
-                if (currentTrivia.IsKind(SyntaxKind.EndOfLineTrivia))
-                {
-                    skipIndex = i + 1;
-                }
-                else if (!currentTrivia.IsKind(SyntaxKind.WhitespaceTrivia))
-                {
-                    // Preceded by whitespace
-                    skipIndex = i > 0 && leadingTrivia[i - 1].IsKind(SyntaxKind.WhitespaceTrivia) ? i - 1 : i;
-                    break;
-                }
-            }
-
-            return SyntaxFactory.TriviaList(leadingTrivia.Skip(skipIndex));
         }
     }
 }
