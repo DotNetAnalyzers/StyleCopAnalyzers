b60f5748251a26566447c2918f09e8532b9175aa
Dmitry Turin <turin.dmytro@gmail.com>
2015-09-14 17:30:00 +0300

    Implemented SA1117 diagnostic + unit tests

--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1117ParametersMustBeOnSameLineOrSeparateLines.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1117ParametersMustBeOnSameLineOrSeparateLines.cs
@@ -3,9 +3,13 @@
 
 namespace StyleCop.Analyzers.ReadabilityRules
 {
+    using System;
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.Helpers;
 
     /// <summary>
     /// The parameters to a C# method or indexer call or declaration are not all on the same line or each on a separate
@@ -55,24 +59,319 @@ namespace StyleCop.Analyzers.ReadabilityRules
         private static readonly string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1117.md";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.ReadabilityRules, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.ReadabilityRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnosticsValue =
             ImmutableArray.Create(Descriptor);
 
         /// <inheritdoc/>
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-        {
-            get
-            {
-                return SupportedDiagnosticsValue;
-            }
-        }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => SupportedDiagnosticsValue;
 
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterCompilationStartAction(HandleCompilationStart);
+        }
+
+        private static void HandleCompilationStart(CompilationStartAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionHonorExclusions(
+                HandleBaseMethodDeclaration,
+                new[] { SyntaxKind.ConstructorDeclaration, SyntaxKind.MethodDeclaration });
+
+            context.RegisterSyntaxNodeActionHonorExclusions(
+                HandleConstructorInitializer,
+                new[] { SyntaxKind.BaseConstructorInitializer, SyntaxKind.ThisConstructorInitializer });
+
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleDelegateDeclaration, SyntaxKind.DelegateDeclaration);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleIndexerDeclaration, SyntaxKind.IndexerDeclaration);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleInvocation, SyntaxKind.InvocationExpression);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleObjectCreation, SyntaxKind.ObjectCreationExpression);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleElementAccess, SyntaxKind.ElementAccessExpression);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleElementBinding, SyntaxKind.ElementBindingExpression);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleArrayCreation, SyntaxKind.ArrayCreationExpression);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleAttribute, SyntaxKind.Attribute);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleAnonymousMethod, SyntaxKind.AnonymousMethodExpression);
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleLambda, SyntaxKind.ParenthesizedLambdaExpression);
+        }
+
+        private static void HandleBaseMethodDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var declaration = (BaseMethodDeclarationSyntax)context.Node;
+            HandleParameterListSyntax(context, declaration.ParameterList);
+        }
+
+        private static void HandleInvocation(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+            HandleArgumentListSyntax(context, invocation.ArgumentList);
+        }
+
+        private static void HandleObjectCreation(SyntaxNodeAnalysisContext context)
+        {
+            var objectCreation = (ObjectCreationExpressionSyntax)context.Node;
+            HandleArgumentListSyntax(context, objectCreation.ArgumentList);
+        }
+
+        private static void HandleIndexerDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var indexerDeclaration = (IndexerDeclarationSyntax)context.Node;
+            BracketedParameterListSyntax argumentListSyntax = indexerDeclaration.ParameterList;
+            SeparatedSyntaxList<ParameterSyntax> arguments = argumentListSyntax.Parameters;
+
+            if (arguments.Count > 2)
+            {
+                Analyze(context, argumentListSyntax.Parameters);
+            }
+        }
+
+        private static void HandleElementAccess(SyntaxNodeAnalysisContext context)
+        {
+            var elementAccess = (ElementAccessExpressionSyntax)context.Node;
+            HandleBracketedArgumentListSyntax(context, elementAccess.ArgumentList);
+        }
+
+        private static void HandleArrayCreation(SyntaxNodeAnalysisContext context)
+        {
+            var arrayCreation = (ArrayCreationExpressionSyntax)context.Node;
+
+            foreach (var rankSpecifier in arrayCreation.Type.RankSpecifiers)
+            {
+                SeparatedSyntaxList<ExpressionSyntax> sizes = rankSpecifier.Sizes;
+                if (sizes.Count < 3)
+                {
+                    continue;
+                }
+
+                ExpressionSyntax previousParameter = sizes[1];
+                int firstParameterLine = sizes[0].GetLine();
+                int previousLine = previousParameter.GetLine();
+                Func<int, int, bool> lineCondition;
+
+                if (firstParameterLine == previousLine)
+                {
+                    // arguments must be on same line
+                    lineCondition = (param1Line, param2Line) => param1Line != param2Line;
+                }
+                else if (previousLine - firstParameterLine == 1)
+                {
+                    // each argument must be on its own line
+                    lineCondition = (param1Line, param2Line) => param1Line == param2Line;
+                }
+                else
+                {
+                    // SA1115 should be handled first
+                    return;
+                }
+
+                for (int i = 2; i < sizes.Count; ++i)
+                {
+                    ExpressionSyntax currentParameter = sizes[i];
+                    int currentLine = currentParameter.GetLine();
+
+                    if (lineCondition(previousLine, currentLine))
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(Descriptor, currentParameter.GetLocation()));
+                        return;
+                    }
+
+                    previousLine = currentLine;
+                }
+            }
+        }
+
+        private static void HandleAttribute(SyntaxNodeAnalysisContext context)
+        {
+            var attribute = (AttributeSyntax)context.Node;
+            AttributeArgumentListSyntax argumentListSyntax = attribute.ArgumentList;
+            if (argumentListSyntax == null)
+            {
+                return;
+            }
+
+            SeparatedSyntaxList<AttributeArgumentSyntax> arguments = argumentListSyntax.Arguments;
+            if (arguments.Count < 3)
+            {
+                return;
+            }
+
+            AttributeArgumentSyntax previousParameter = arguments[1];
+            int firstParameterLine = arguments[0].GetLine();
+            int previousLine = previousParameter.GetLine();
+            Func<int, int, bool> lineCondition;
+
+            if (firstParameterLine == previousLine)
+            {
+                // arguments must be on same line
+                lineCondition = (param1Line, param2Line) => param1Line != param2Line;
+            }
+            else if (previousLine - firstParameterLine == 1)
+            {
+                // each argument must be on its own line
+                lineCondition = (param1Line, param2Line) => param1Line == param2Line;
+            }
+            else
+            {
+                // SA1115 should be handled first
+                return;
+            }
+
+            for (int i = 2; i < arguments.Count; ++i)
+            {
+                AttributeArgumentSyntax currentParameter = arguments[i];
+                int currentLine = currentParameter.GetLine();
+
+                if (lineCondition(previousLine, currentLine))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, currentParameter.GetLocation()));
+                    return;
+                }
+
+                previousLine = currentLine;
+            }
+        }
+
+        private static void HandleAnonymousMethod(SyntaxNodeAnalysisContext context)
+        {
+            var anonymousMethod = (AnonymousMethodExpressionSyntax)context.Node;
+            HandleParameterListSyntax(context, anonymousMethod.ParameterList);
+        }
+
+        private static void HandleLambda(SyntaxNodeAnalysisContext context)
+        {
+            var parenthesizedLambda = (ParenthesizedLambdaExpressionSyntax)context.Node;
+            HandleParameterListSyntax(context, parenthesizedLambda.ParameterList);
+        }
+
+        private static void HandleDelegateDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var delegateDeclaration = (DelegateDeclarationSyntax)context.Node;
+            HandleParameterListSyntax(context, delegateDeclaration.ParameterList);
+        }
+
+        private static void HandleConstructorInitializer(SyntaxNodeAnalysisContext context)
+        {
+            var constructorInitializer = (ConstructorInitializerSyntax)context.Node;
+            HandleArgumentListSyntax(context, constructorInitializer.ArgumentList);
+        }
+
+        private static void HandleElementBinding(SyntaxNodeAnalysisContext context)
+        {
+            var elementBinding = (ElementBindingExpressionSyntax)context.Node;
+            HandleBracketedArgumentListSyntax(context, elementBinding.ArgumentList);
+        }
+
+        private static void HandleArgumentListSyntax(SyntaxNodeAnalysisContext context, ArgumentListSyntax argumentList)
+        {
+            if (argumentList == null)
+            {
+                return;
+            }
+
+            SeparatedSyntaxList<ArgumentSyntax> arguments = argumentList.Arguments;
+            if (arguments.Count > 2)
+            {
+                Analyze(context, arguments);
+            }
+        }
+
+        private static void HandleParameterListSyntax(SyntaxNodeAnalysisContext context, ParameterListSyntax parameterList)
+        {
+            if (parameterList == null)
+            {
+                return;
+            }
+
+            SeparatedSyntaxList<ParameterSyntax> parameters = parameterList.Parameters;
+            if (parameters.Count > 2)
+            {
+                Analyze(context, parameters);
+            }
+        }
+
+        private static void HandleBracketedArgumentListSyntax(SyntaxNodeAnalysisContext context, BracketedArgumentListSyntax bracketedArgumentList)
+        {
+            SeparatedSyntaxList<ArgumentSyntax> arguments = bracketedArgumentList.Arguments;
+            if (arguments.Count > 2)
+            {
+                Analyze(context, arguments);
+            }
+        }
+
+        private static void Analyze(SyntaxNodeAnalysisContext context, SeparatedSyntaxList<ParameterSyntax> parameters)
+        {
+            ParameterSyntax previousParameter = parameters[1];
+            int firstParameterLine = parameters[0].GetLine();
+            int previousLine = previousParameter.GetLine();
+            Func<int, int, bool> lineCondition;
+
+            if (firstParameterLine == previousLine)
+            {
+                // parameters must be on same line
+                lineCondition = (param1Line, param2Line) => param1Line != param2Line;
+            }
+            else if (previousLine - firstParameterLine == 1)
+            {
+                // each parameter must be on its own line
+                lineCondition = (param1Line, param2Line) => param1Line == param2Line;
+            }
+            else
+            {
+                // SA1115 should be handled first
+                return;
+            }
+
+            for (int i = 2; i < parameters.Count; ++i)
+            {
+                ParameterSyntax currentParameter = parameters[i];
+                int currentLine = currentParameter.GetLine();
+
+                if (lineCondition(previousLine, currentLine))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, currentParameter.GetLocation()));
+                    return;
+                }
+
+                previousLine = currentLine;
+            }
+        }
+
+        private static void Analyze(SyntaxNodeAnalysisContext context, SeparatedSyntaxList<ArgumentSyntax> arguments)
+        {
+            ArgumentSyntax previousParameter = arguments[1];
+            int firstParameterLine = arguments[0].GetLine();
+            int previousLine = previousParameter.GetLine();
+            Func<int, int, bool> lineCondition;
+
+            if (firstParameterLine == previousLine)
+            {
+                // arguments must be on same line
+                lineCondition = (param1Line, param2Line) => param1Line != param2Line;
+            }
+            else if (previousLine - firstParameterLine == 1)
+            {
+                // each argument must be on its own line
+                lineCondition = (param1Line, param2Line) => param1Line == param2Line;
+            }
+            else
+            {
+                // SA1115 should be handled first
+                return;
+            }
+
+            for (int i = 2; i < arguments.Count; ++i)
+            {
+                ArgumentSyntax currentParameter = arguments[i];
+                int currentLine = currentParameter.GetLine();
+
+                if (lineCondition(previousLine, currentLine))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, currentParameter.GetLocation()));
+                    return;
+                }
+
+                previousLine = currentLine;
+            }
         }
     }
 }
