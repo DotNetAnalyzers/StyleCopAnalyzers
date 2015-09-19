cc64ff5b2458acca06f670762e26862d5c3d6ddf
Dennis Fischer <fischer_dennis@live.de>
2015-09-19 22:36:13 +0200

    Implement SA1130

--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1130UseLambdaSyntax.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1130UseLambdaSyntax.cs
@@ -3,8 +3,11 @@
 
 namespace StyleCop.Analyzers.ReadabilityRules
 {
+    using System;
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     /// <summary>
@@ -22,6 +25,7 @@ namespace StyleCop.Analyzers.ReadabilityRules
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(ReadabilityResources.SA1130MessageFormat), ReadabilityResources.ResourceManager, typeof(ReadabilityResources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(ReadabilityResources.SA1130Description), ReadabilityResources.ResourceManager, typeof(ReadabilityResources));
         private static readonly string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1130.md";
+        private static readonly ParameterListSyntax EmptyParameterList = SyntaxFactory.ParameterList();
 
         private static readonly DiagnosticDescriptor Descriptor =
             new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.ReadabilityRules, DiagnosticSeverity.Warning, AnalyzerConstants.DisabledNoTests, Description, HelpLink);
@@ -33,7 +37,47 @@ namespace StyleCop.Analyzers.ReadabilityRules
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Implement analysis
+            context.RegisterCompilationStartAction(HandleCompilationStart);
+        }
+
+        private static void HandleCompilationStart(CompilationStartAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionHonorExclusions(HandleAnonymousMethodExpression, SyntaxKind.AnonymousMethodExpression);
+        }
+
+        private static void HandleAnonymousMethodExpression(SyntaxNodeAnalysisContext context)
+        {
+            var anonymousMethod = (AnonymousMethodExpressionSyntax)context.Node;
+
+            if (anonymousMethod.Parent.IsKind(SyntaxKind.Argument))
+            {
+                // invocation -> argument list -> argument -> anonymous method
+                var originalInvocationExpression = anonymousMethod?.Parent?.Parent?.Parent as InvocationExpressionSyntax;
+
+                if (originalInvocationExpression != null)
+                {
+                    // In some cases passing a delegate as an argument to a method is required to call the right overload
+                    // When there is an other overload that takes an expression.
+                    var lambdaExpression = SyntaxFactory.ParenthesizedLambdaExpression(
+                        anonymousMethod.AsyncKeyword,
+                        anonymousMethod.ParameterList ?? EmptyParameterList,
+                        SyntaxFactory.Token(SyntaxKind.EqualsGreaterThanToken),
+                        anonymousMethod.Body);
+
+                    var invocationExpression = originalInvocationExpression.ReplaceNode(anonymousMethod, lambdaExpression);
+
+                    SymbolInfo originalSymbolInfo = context.SemanticModel.GetSymbolInfo(originalInvocationExpression);
+                    Location location = originalInvocationExpression.GetLocation();
+                    SymbolInfo newSymbolInfo = context.SemanticModel.GetSpeculativeSymbolInfo(location.SourceSpan.Start, invocationExpression, SpeculativeBindingOption.BindAsExpression);
+
+                    if (originalSymbolInfo.Symbol != newSymbolInfo.Symbol)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, anonymousMethod.GetLocation()));
         }
     }
 }
