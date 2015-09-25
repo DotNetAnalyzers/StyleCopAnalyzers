b4f4d2727289d4142cedea82731e9122e9901bb0
Sam Harwell <sam@tunnelvisionlabs.com>
2015-09-25 06:53:17 -0500

    Do not separate System using directives when SA1208 is disabled
    Fixes #1539

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1210CombinedSystemDirectivesUnitTests.cs
@@ -0,0 +1,229 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Test.OrderingRules
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.OrderingRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="SA1210UsingDirectivesMustBeOrderedAlphabeticallyByNamespace"/> for the special case
+    /// where <see cref="SA1208SystemUsingDirectivesMustBePlacedBeforeOtherUsingDirectives"/> is disabled.
+    /// </summary>
+    public class SA1210CombinedSystemDirectivesUnitTests : CodeFixVerifier
+    {
+        [Fact]
+        public async Task TestProperOrderedUsingDirectivesInNamespaceDeclarationAsync()
+        {
+            var namespaceDeclaration = @"namespace Food
+{
+    using System;
+    using System.Threading;
+}
+
+namespace Bar
+{
+    using Food;
+    using System;
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(namespaceDeclaration, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestInvalidOrderedUsingDirectivesInNamespaceDeclarationAsync()
+        {
+            var testCode = @"namespace Food
+{
+    using System.Threading;
+    using System;
+}
+
+namespace Bar
+{
+    using Food;
+    using Bar;
+    using System.Threading;
+    using System;
+}";
+
+            var fixedTestCode = @"namespace Food
+{
+    using System;
+    using System.Threading;
+}
+
+namespace Bar
+{
+    using Bar;
+    using Food;
+    using System;
+    using System.Threading;
+}";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(3, 5),
+                this.CSharpDiagnostic().WithLocation(9, 5),
+                this.CSharpDiagnostic().WithLocation(11, 5)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestInvalidOrderedUsingDirectivesWithGlobalKeywordAsync()
+        {
+            var testCode = @"using System.Threading;
+using global::System.IO;
+using global::System.Linq;
+using global::System;
+using XYZ = System.IO;
+
+namespace Food
+{
+    using global::Food;
+    using System;
+}";
+
+            var fixedTestCode = @"namespace Food
+{
+    using global::Food;
+    using global::System;
+    using global::System.IO;
+    using global::System.Linq;
+    using System.Threading;
+    using XYZ = System.IO;
+}";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(1, 1),
+                this.CSharpDiagnostic().WithLocation(3, 1),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestInvalidOrderedUsingDirectivesWithNamespaceAliasQualifierAsync()
+        {
+            var testCode = @"using System.Threading;
+using global::System.IO;
+using global::System.Linq;
+using global::System;
+using global::Food;
+using Food;
+
+namespace Food
+{
+    using global::Food;
+    using System;
+}";
+
+            var fixedTestCode = @"namespace Food
+{
+    using global::Food;
+    using global::System;
+    using global::System.IO;
+    using global::System.Linq;
+    using System.Threading;
+}";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(1, 1),
+                this.CSharpDiagnostic().WithLocation(3, 1),
+                this.CSharpDiagnostic().WithLocation(4, 1),
+                this.CSharpDiagnostic().WithLocation(5, 1)
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestValidOrderedUsingDirectivesWithStaticUsingDirectivesAsync()
+        {
+            var namespaceDeclaration = @"namespace Food
+{
+    using Food;
+    using System;
+    using static System.Uri;
+    using static System.Math;
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(namespaceDeclaration, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestPreprocessorDirectivesAsync()
+        {
+            var testCode = @"
+using System;
+using Microsoft.VisualStudio;
+using MyList = System.Collections.Generic.List<int>;
+
+#if true
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+#else
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+#endif";
+
+            var fixedTestCode = @"using Microsoft.VisualStudio;
+using System;
+using MyList = System.Collections.Generic.List<int>;
+
+#if true
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+#else
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+#endif";
+
+            // else block is skipped
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(2, 1),
+                this.CSharpDiagnostic().WithLocation(7, 1),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<string> GetDisabledDiagnostics()
+        {
+            yield return SA1208SystemUsingDirectivesMustBePlacedBeforeOtherUsingDirectives.DiagnosticId;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1210UsingDirectivesMustBeOrderedAlphabeticallyByNamespace();
+        }
+
+        /// <inheritdoc/>
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new UsingCodeFixProvider();
+        }
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/UsingCodeFixProviderCombinedSystemDirectivesUnitTests.cs
@@ -0,0 +1,428 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Test.OrderingRules
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.OrderingRules;
+    using TestHelper;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for the <see cref="UsingCodeFixProvider"/> for the special case where
+    /// <see cref="SA1208SystemUsingDirectivesMustBePlacedBeforeOtherUsingDirectives"/> is disabled.
+    /// </summary>
+    public class UsingCodeFixProviderCombinedSystemDirectivesUnitTests : CodeFixVerifier
+    {
+        private bool suppressSA1200;
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingAsync()
+        {
+            var testCode = @"using Microsoft.CodeAnalysis;
+using SystemAction = System.Action;
+using static System.Math;
+using System;
+
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+
+using System.Collections.Generic;
+using System.Collections;
+
+namespace NamespaceName
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"namespace NamespaceName
+{
+    using Microsoft.CodeAnalysis;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using static System.Math;
+    using static System.String;
+    using MyFunc = System.Func<int,bool>;
+    using SystemAction = System.Action;
+
+    public class Bar
+    {
+    }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements, but will not move a file header comment.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithFileHeaderAsync()
+        {
+            var testCode = @"// This is a file header.
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"// This is a file header.
+
+namespace Foo
+{
+    using Microsoft.CodeAnalysis;
+    using System;
+
+    public class Bar
+    {
+    }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements, without moving them inside a namespace when SA1200 is suppressed.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithoutMovingAsync()
+        {
+            var testCode = @"using Microsoft.CodeAnalysis;
+using SystemAction = System.Action;
+using static System.Math;
+using System;
+
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+
+using System.Collections.Generic;
+using System.Collections;
+
+namespace NamespaceName
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"using Microsoft.CodeAnalysis;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Math;
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+using SystemAction = System.Action;
+
+namespace NamespaceName
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            this.suppressSA1200 = true;
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements, without moving them inside a namespace
+        /// when SA1200 is suppressed. The file header is not moved by the code fix.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithoutMovingWithFileHeaderAsync()
+        {
+            var testCode = @"// This is a file header.
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"// This is a file header.
+
+using Microsoft.CodeAnalysis;
+using System;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            this.suppressSA1200 = true;
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements, without moving them inside a namespace
+        /// when SA1200 is suppressed. The file header is not moved by the code fix.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithoutMovingWithMultiLineFileHeaderAsync()
+        {
+            var testCode = @"// <copyright file=""Test0.cs"" company=""FooCorp"">
+//   Copyright (c) FooCorp. All rights reserved.
+// </copyright>
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"// <copyright file=""Test0.cs"" company=""FooCorp"">
+//   Copyright (c) FooCorp. All rights reserved.
+// </copyright>
+
+using Microsoft.CodeAnalysis;
+using System;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            this.suppressSA1200 = true;
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements, without moving them inside a namespace
+        /// when SA1200 is suppressed. The file header is not moved by the code fix.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithoutMovingWithMultiLineCommentAsync()
+        {
+            var testCode = @"/*
+ * Copyright by FooCorp Inc.
+ */
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"/*
+ * Copyright by FooCorp Inc.
+ */
+
+using Microsoft.CodeAnalysis;
+using System;
+
+namespace Foo
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            this.suppressSA1200 = true;
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will properly reorder using statements, without moving them inside a namespace when there are multiple namespaces.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithMultipleNamespacesAsync()
+        {
+            var testCode = @"using Microsoft.CodeAnalysis;
+using SystemAction = System.Action;
+using static System.Math;
+using System;
+
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+
+using System.Collections.Generic;
+using System.Collections;
+
+namespace TestNamespace1
+{
+    public class TestClass1
+    {
+    }
+}
+
+namespace TestNamespace2
+{
+}
+";
+
+            var fixedTestCode = @"using Microsoft.CodeAnalysis;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Math;
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+using SystemAction = System.Action;
+
+namespace TestNamespace1
+{
+    public class TestClass1
+    {
+    }
+}
+
+namespace TestNamespace2
+{
+}
+";
+
+            // The code fix is not able to correct all violations due to the use of multiple namespaces in a single file
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(1, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(2, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(3, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(4, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(5, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(6, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(7, 1),
+                this.CSharpDiagnostic(SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId).WithLocation(8, 1),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that the code fix will leave using statements needed for global attributes at the appropriate location.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyUsingReorderingWithGlobalAttributesAsync()
+        {
+            var testCode = @"using Microsoft.CodeAnalysis;
+using SystemAction = System.Action;
+using static System.Math;
+using System.Reflection;
+
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+
+using System.Collections.Generic;
+using System.Collections;
+
+[assembly: AssemblyVersion(""1.0.0.0"")]
+
+namespace NamespaceName
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            var fixedTestCode = @"using Microsoft.CodeAnalysis;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using static System.Math;
+using static System.String;
+using MyFunc = System.Func<int,bool>;
+using SystemAction = System.Action;
+
+[assembly: AssemblyVersion(""1.0.0.0"")]
+
+namespace NamespaceName
+{
+    public class Bar
+    {
+    }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(fixedTestCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedTestCode).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<string> GetDisabledDiagnostics()
+        {
+            if (this.suppressSA1200)
+            {
+                yield return SA1200UsingDirectivesMustBePlacedWithinNamespace.DiagnosticId;
+            }
+
+            yield return SA1208SystemUsingDirectivesMustBePlacedBeforeOtherUsingDirectives.DiagnosticId;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1200UsingDirectivesMustBePlacedWithinNamespace();
+            yield return new SA1208SystemUsingDirectivesMustBePlacedBeforeOtherUsingDirectives();
+            yield return new SA1209UsingAliasDirectivesMustBePlacedAfterOtherUsingDirectives();
+            yield return new SA1210UsingDirectivesMustBeOrderedAlphabeticallyByNamespace();
+            yield return new SA1211UsingAliasDirectivesMustBeOrderedAlphabeticallyByAliasName();
+            yield return new SA1216UsingStaticDirectivesMustBePlacedAtTheCorrectLocation();
+            yield return new SA1217UsingStaticDirectivesMustBeOrderedAlphabetically();
+        }
+
+        /// <inheritdoc/>
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new UsingCodeFixProvider();
+        }
+    }
+}
