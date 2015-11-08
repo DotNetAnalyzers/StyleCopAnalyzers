c2d711e3aefe280b0b07ab4a985d98b63e8a4c00
Sam Harwell <sam@tunnelvisionlabs.com>
2015-11-08 17:11:19 -0600

    Work around dotnet/roslyn#6596 by using file system APIs
    Fixes #1736

--- StyleCop.Analyzers/StyleCop.Analyzers/Settings/SettingsHelper.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/Settings/SettingsHelper.cs
@@ -3,8 +3,10 @@
 
 namespace StyleCop.Analyzers
 {
+    using System;
     using System.Collections.Immutable;
     using System.IO;
+    using System.Reflection;
     using System.Threading;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.Diagnostics;
@@ -17,6 +19,9 @@ namespace StyleCop.Analyzers
     internal static class SettingsHelper
     {
         private const string SettingsFileName = "stylecop.json";
+
+        private static Func<string, bool> fileExists;
+        private static Func<string, string> fileReadAllText;
 
         /// <summary>
         /// Gets the StyleCop settings.
@@ -48,7 +53,7 @@ namespace StyleCop.Analyzers
                 {
                     if (Path.GetFileName(additionalFile.Path).ToLowerInvariant() == SettingsFileName)
                     {
-                        string additionalTextContent = additionalFile.GetText(cancellationToken).ToString();
+                        string additionalTextContent = ReadAdditionalText(additionalFile, cancellationToken);
                         var root = JsonConvert.DeserializeObject<SettingsFile>(additionalTextContent);
                         return root.Settings;
                     }
@@ -60,6 +65,52 @@ namespace StyleCop.Analyzers
             }
 
             return new StyleCopSettings();
+        }
+
+        /// <summary>
+        /// This code works around dotnet/roslyn#6596 by using the file system APIs instead of the Roslyn APIs to read
+        /// the additional text. If the file system APIs are not available, the code falls back to the previous
+        /// behavior.
+        /// </summary>
+        /// <param name="additionalText">The additional text to read.</param>
+        /// <param name="cancellationToken">The cancellation token that the operation will observe.</param>
+        /// <returns>The content of the additional text as a string.</returns>
+        private static string ReadAdditionalText(AdditionalText additionalText, CancellationToken cancellationToken)
+        {
+            if (fileExists == null)
+            {
+                Type fileClass = typeof(string).GetTypeInfo().Assembly.GetType("System.IO.File");
+                if (fileClass != null)
+                {
+                    MethodInfo readAllText = fileClass.GetRuntimeMethod("ReadAllText", new[] { typeof(string) });
+                    MethodInfo exists = fileClass.GetRuntimeMethod("Exists", new[] { typeof(string) });
+                    if (readAllText != null && exists != null)
+                    {
+                        Interlocked.CompareExchange(ref fileReadAllText, (Func<string, string>)readAllText.CreateDelegate(typeof(Func<string, string>)), null);
+                        Interlocked.CompareExchange(ref fileExists, (Func<string, bool>)exists.CreateDelegate(typeof(Func<string, bool>)), null);
+                    }
+                }
+
+                if (fileExists == null)
+                {
+                    // this special case allows for a clean fall back to AdditionalText.GetText()
+                    fileExists = _ => false;
+                }
+            }
+
+            try
+            {
+                if (fileExists(additionalText.Path))
+                {
+                    return fileReadAllText(additionalText.Path);
+                }
+            }
+            catch (IOException)
+            {
+                // fall back to AdditionalFile.GetText()
+            }
+
+            return additionalText.GetText(cancellationToken).ToString();
         }
     }
 }
