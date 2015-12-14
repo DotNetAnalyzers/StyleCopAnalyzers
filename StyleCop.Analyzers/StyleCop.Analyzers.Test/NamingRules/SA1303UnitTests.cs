4e91f97c766c90d47e38c678612b46757d3c2b77
Nate Barbettini <nathanaelb@gmail.com>
2015-12-13 21:31:57 -0800

    Remove stray using directive

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/NamingRules/SA1303UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/NamingRules/SA1303UnitTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using System.Threading.Tasks;
 namespace StyleCop.Analyzers.Test.NamingRules
 {
     using System.Collections.Generic;
