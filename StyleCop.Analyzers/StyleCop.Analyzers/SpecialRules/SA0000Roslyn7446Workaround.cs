24bff5340a7eac17efe7020253357a469a847d09
Sam Harwell <sam@tunnelvisionlabs.com>
2015-12-23 08:06:53 -0600

    Usability improvements for the workaround
    * Rename Roslyn7446WorkaroundAnalyzer to SA0000Roslyn7446Workaround
    * Allow users to configure the behavior of SA0000Roslyn7446Workaround
    * Improve the title and description of SA0000Roslyn7446Workaround
    * Disable the rule by default for improved performance

--- StyleCop.Analyzers/StyleCop.Analyzers/SpecialRules/SA0000Roslyn7446Workaround.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/SpecialRules/SA0000Roslyn7446Workaround.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-namespace StyleCop.Analyzers
+namespace StyleCop.Analyzers.SpecialRules
 {
     using System;
     using System.Collections.Immutable;
@@ -10,25 +10,25 @@ namespace StyleCop.Analyzers
     using Microsoft.CodeAnalysis.Diagnostics;
 
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    internal sealed class Roslyn7446WorkaroundAnalyzer : DiagnosticAnalyzer
+    internal sealed class SA0000Roslyn7446Workaround : DiagnosticAnalyzer
     {
         /// <summary>
-        /// The ID for diagnostics produced by the <see cref="Roslyn7446WorkaroundAnalyzer"/> analyzer.
+        /// The ID for diagnostics produced by the <see cref="SA0000Roslyn7446Workaround"/> analyzer.
         /// </summary>
-        public const string DiagnosticId = "Roslyn7446WorkaroundAnalyzer";
-        private const string Title = "Roslyn Bug 7446 Workaround";
-        private const string MessageFormat = "Roslyn Bug 7446 Workaround";
-        private const string Description = "Roslyn Bug 7446 Workaround";
-        private const string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/Roslyn7446WorkaroundAnalyzer.md";
+        public const string DiagnosticId = "SA0000";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(SpecialResources.SA0000Title), SpecialResources.ResourceManager, typeof(SpecialResources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(SpecialResources.SA0000MessageFormat), SpecialResources.ResourceManager, typeof(SpecialResources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(SpecialResources.SA0000Description), SpecialResources.ResourceManager, typeof(SpecialResources));
+        private static readonly string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA0000Roslyn7446Workaround.md";
 
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.SpacingRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink, WellKnownDiagnosticTags.NotConfigurable);
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.SpecialRules, DiagnosticSeverity.Info, AnalyzerConstants.DisabledByDefault, Description, HelpLink);
 
         private static readonly Action<CompilationStartAnalysisContext> CompilationStartAction = HandleCompilationStart;
 
         private static readonly bool CallGetDeclarationDiagnostics;
 
-        static Roslyn7446WorkaroundAnalyzer()
+        static SA0000Roslyn7446Workaround()
         {
             // dotnet/roslyn#7446 was fixed for Roslyn 1.2
             CallGetDeclarationDiagnostics = typeof(AdditionalText).GetTypeInfo().Assembly.GetName().Version < new Version(1, 2, 0, 0);
