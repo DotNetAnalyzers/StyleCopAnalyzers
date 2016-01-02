2e5b05abf405a92151d54783d6e22de4eaa63300
Sam Harwell <sam@tunnelvisionlabs.com>
2016-01-01 18:21:23 -0600

    Move opencover-report.bat to the build directory

--- build/opencover-report.bat
+++ build/opencover-report.bat
@@ -1,9 +1,9 @@
-@set packages_folder=.\packages
+@set packages_folder=..\packages
 @set opencover_console=%packages_folder%\OpenCover.4.6.247-rc\tools\OpenCover.Console.exe
 @set xunit_runner_console=%packages_folder%\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe
 @set report_generator=%packages_folder%\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe
 @set report_folder=.\OpenCover.Reports
-@set target_dll=.\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\Debug\StyleCop.Analyzers.Test.dll
+@set target_dll=..\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\Debug\StyleCop.Analyzers.Test.dll
 @if not exist %target_dll% goto error_target_dll
 @if not exist %opencover_console% goto error_opencover_console
 @if not exist %xunit_runner_console% goto error_xunit_runner_console
