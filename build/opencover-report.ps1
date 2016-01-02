88651ef0bd9f913449895bbc56623d10536be8d4
Sam Harwell <sam@tunnelvisionlabs.com>
2016-01-01 19:16:14 -0600

    Add the -Debug switch to opencover-report.ps1

--- build/opencover-report.ps1
+++ build/opencover-report.ps1
@@ -1,8 +1,23 @@
+param (
+	[switch]$Debug
+)
+
 # Run a build to ensure everything is up-to-date
-.\build.ps1 -Debug -Incremental
+If ($Debug) {
+	.\build.ps1 -Debug -Incremental
+} Else {
+	.\build.ps1 -Incremental
+}
+
 If (-not $?) {
 	$host.UI.WriteErrorLine('Build failed; coverage analysis aborted.')
 	Exit $LASTEXITCODE
+}
+
+If ($Debug) {
+	$Configuration = 'Debug'
+} Else {
+	$Configuration = 'Release'
 }
 
 $packages_folder = '..\packages'
@@ -10,7 +25,7 @@ $opencover_console = "$packages_folder\OpenCover.4.6.247-rc\tools\OpenCover.Cons
 $xunit_runner_console = "$packages_folder\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe"
 $report_generator = "$packages_folder\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe"
 $report_folder = '.\OpenCover.Reports'
-$target_dll = '..\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\Debug\StyleCop.Analyzers.Test.dll'
+$target_dll = "..\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\$Configuration\StyleCop.Analyzers.Test.dll"
 
 If (Test-Path $report_folder) {
 	Remove-Item -Recurse -Force $report_folder
