7de1b631722e50b8726004982f0e1b2c5a10f84b
Sam Harwell <sam@tunnelvisionlabs.com>
2016-01-01 19:15:43 -0600

    Run a build before coverage analysis
    * Ensures the outputs are up-to-date before proceeding
    * Ensures NuGet packages are restored before proceeding

--- build/opencover-report.ps1
+++ build/opencover-report.ps1
@@ -1,33 +1,16 @@
+# Run a build to ensure everything is up-to-date
+.\build.ps1 -Debug -Incremental
+If (-not $?) {
+	$host.UI.WriteErrorLine('Build failed; coverage analysis aborted.')
+	Exit $LASTEXITCODE
+}
+
 $packages_folder = '..\packages'
 $opencover_console = "$packages_folder\OpenCover.4.6.247-rc\tools\OpenCover.Console.exe"
 $xunit_runner_console = "$packages_folder\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe"
 $report_generator = "$packages_folder\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe"
 $report_folder = '.\OpenCover.Reports'
 $target_dll = '..\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\Debug\StyleCop.Analyzers.Test.dll'
-
-If (-not (Test-Path $target_dll)) {
-	$host.UI.WriteErrorLine('target dll not found (build target?)')
-	$host.UI.WriteErrorLine("$target_dll")
-	Exit 1
-}
-
-If (-not (Test-Path $opencover_console)) {
-	$host.UI.WriteErrorLine('OpenCover Console not found (nuget restore?)')
-	$host.UI.WriteErrorLine("$opencover_console")
-	Exit 1
-}
-
-If (-not (Test-Path $xunit_runner_console)) {
-	$host.UI.WriteErrorLine('OpenCover Console not found (nuget restore?)')
-	$host.UI.WriteErrorLine("$xunit_runner_console")
-	Exit 1
-}
-
-If (-not (Test-Path $report_generator)) {
-	$host.UI.WriteErrorLine('Report Generator not found (nuget restore?)')
-	$host.UI.WriteErrorLine("$report_generator")
-	Exit 1
-}
 
 If (Test-Path $report_folder) {
 	Remove-Item -Recurse -Force $report_folder
