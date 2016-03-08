44bc1738588364871b46964cc3f9ad7365c6a226
Sam Harwell <sam@tunnelvisionlabs.com>
2016-03-08 14:01:32 -0600

    Enable concurrent execution for thread-safe analyzers

--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1121UseBuiltInTypeAlias.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1121UseBuiltInTypeAlias.cs
@@ -143,6 +143,7 @@ namespace StyleCop.Analyzers.ReadabilityRules
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
 
             context.RegisterCompilationStartAction(CompilationStartAction);
         }
