bf3f4ef2b90d53a239693334d43311a944a3e53b
vweijsters <vweijsters@outlook.com>
2016-05-18 09:36:31 +0200

    Updated to work with roslyn-1.2

--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1136EnumValuesShouldBeOnSeparateLines.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1136EnumValuesShouldBeOnSeparateLines.cs
@@ -29,7 +29,6 @@ namespace StyleCop.Analyzers.ReadabilityRules
         private static readonly DiagnosticDescriptor Descriptor =
             new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.ReadabilityRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
-        private static readonly Action<CompilationStartAnalysisContext> CompilationStartAction = HandleCompilationStart;
         private static readonly Action<SyntaxNodeAnalysisContext> HandleEnumDeclarationAction = HandleEnumDeclaration;
 
         /// <inheritdoc/>
@@ -39,12 +38,10 @@ namespace StyleCop.Analyzers.ReadabilityRules
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterCompilationStartAction(CompilationStartAction);
-        }
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
 
-        private static void HandleCompilationStart(CompilationStartAnalysisContext context)
-        {
-            context.RegisterSyntaxNodeActionHonorExclusions(HandleEnumDeclarationAction, SyntaxKind.EnumDeclaration);
+            context.RegisterSyntaxNodeAction(HandleEnumDeclarationAction, SyntaxKind.EnumDeclaration);
         }
 
         private static void HandleEnumDeclaration(SyntaxNodeAnalysisContext context)
