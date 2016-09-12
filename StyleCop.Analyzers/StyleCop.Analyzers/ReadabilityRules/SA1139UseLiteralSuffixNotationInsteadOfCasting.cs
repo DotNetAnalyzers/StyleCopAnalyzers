6ef557fb524c5ade1dff1d279a4ae6fd7ac30d9e
Sam Harwell <sam@tunnelvisionlabs.com>
2016-09-12 11:20:22 -0500

    Remove individual contributor name from file headers

--- StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1139UseLiteralSuffixNotationInsteadOfCasting.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/ReadabilityRules/SA1139UseLiteralSuffixNotationInsteadOfCasting.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-/* Contributor: Tomasz Maczyński */
-
 namespace StyleCop.Analyzers.ReadabilityRules
 {
     using System;
