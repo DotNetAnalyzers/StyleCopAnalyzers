c97a489c674974fe3871e82660bf61588ed76946
Dennis Fischer <fischer_dennis@live.de>
2017-03-29 19:57:07 +0200

    Hopefully fix code coverage path

--- build/opencover-report.ps1
+++ build/opencover-report.ps1
@@ -2,41 +2,10 @@ param (
 	[switch]$Debug
 )
 
-# Run a build to ensure everything is up-to-date
-If ($Debug) {
-	.\build.ps1 -Debug -Incremental
-} Else {
-	.\build.ps1 -Incremental
-}
+cd StyleCop.Analyzers\StyleCop.Analyzers.Test
 
-If (-not $?) {
-	$host.UI.WriteErrorLine('Build failed; coverage analysis aborted.')
-	Exit $LASTEXITCODE
-}
-
-If ($Debug) {
-	$Configuration = 'Debug'
-} Else {
-	$Configuration = 'Release'
-}
-
-$packageConfig = [xml](Get-Content ..\.nuget\packages.config)
-$opencover_version = $packageConfig.SelectSingleNode('/packages/package[@id="OpenCover"]').version
-$reportgenerator_version = $packageConfig.SelectSingleNode('/packages/package[@id="ReportGenerator"]').version
-$xunitrunner_version = $packageConfig.SelectSingleNode('/packages/package[@id="xunit.runner.console"]').version
-
-$packages_folder = '..\packages'
-$opencover_console = "$packages_folder\OpenCover.$opencover_version\tools\OpenCover.Console.exe"
-$xunit_runner_console = "$packages_folder\xunit.runner.console.$xunitrunner_version\tools\xunit.console.x86.exe"
-$report_generator = "$packages_folder\ReportGenerator.$reportgenerator_version\tools\ReportGenerator.exe"
-$report_folder = '.\OpenCover.Reports'
-$target_dll = "..\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\$Configuration\StyleCop.Analyzers.Test.dll"
-
-If (Test-Path $report_folder) {
-	Remove-Item -Recurse -Force $report_folder
-}
-
-mkdir $report_folder | Out-Null
+$packages_folder = "$env:USERPROFILE\.nuget\packages\"
+$opencover_console = "$packages_folder\opencover\4.6.519\tools\OpenCover.Console.exe"
 
 &$opencover_console `
 	-register:user `
@@ -46,10 +15,7 @@ mkdir $report_folder | Out-Null
 	-filter:"+[StyleCop*]*" `
 	-excludebyattribute:*.ExcludeFromCodeCoverage* `
 	-excludebyfile:*\*Designer.cs `
-	-output:"$report_folder\OpenCover.StyleCopAnalyzers.xml" `
-	-target:"$xunit_runner_console" `
-	-targetargs:"$target_dll -noshadow"
-
-&$report_generator -targetdir:$report_folder -reports:$report_folder\OpenCover.*.xml
-
-$host.UI.WriteLine("Open $report_folder\index.htm to see code coverage results.")
+	-output:"OpenCover.StyleCopAnalyzers.xml" `
+	-target:"C:\Program Files\dotnet\dotnet.exe" `
+	-targetargs:"test" `
+    -oldstyle
