a350844776a070ba522a4a001f907d36b4f06020
Björn Hellander <bjorn_hellander@yahoo.se>
2017-05-29 08:32:47 +0200

    Removed unnecessary using statement.

--- StyleCop.Analyzers/StyleCop.Analyzers/NamingRules/SA1303ConstFieldNamesMustBeginWithUpperCaseLetter.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/NamingRules/SA1303ConstFieldNamesMustBeginWithUpperCaseLetter.cs
@@ -4,7 +4,6 @@
 namespace StyleCop.Analyzers.NamingRules
 {
     using System;
-    using System.Collections.Concurrent;
     using System.Collections.Immutable;
     using System.Linq;
     using Microsoft.CodeAnalysis;
