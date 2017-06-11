9727f83809617d2330d89b434b054729cd81c796
Sam Harwell <sam@tunnelvisionlabs.com>
2017-06-11 07:23:58 -0500

    Add tests for spacing around closing braces in tuple expressions

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/SpacingRules/SA1001CSharp7UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/SpacingRules/SA1001CSharp7UnitTests.cs
@@ -55,6 +55,41 @@ public class Foo
         }
 
         /// <summary>
+        /// Verifies spacing around a <c>}</c> character in tuple expressions.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        /// <seealso cref="SA1009CSharp7UnitTests.TestSpacingAroundClosingBraceInTupleExpressionsAsync"/>
+        /// <seealso cref="SA1013CSharp7UnitTests.TestSpacingAroundClosingBraceInTupleExpressionsAsync"/>
+        [Fact]
+        public async Task TestSpacingAroundClosingBraceInTupleExpressionsAsync()
+        {
+            const string testCode = @"using System;
+
+public class Foo
+{
+    public void TestMethod()
+    {
+        var values = (new[] { 3} , new[] { 3} );
+    }
+}";
+            const string fixedCode = @"using System;
+
+public class Foo
+{
+    public void TestMethod()
+    {
+        var values = (new[] { 3}, new[] { 3} );
+    }
+}";
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(7, 34).WithArguments(" not", "preceded");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
         /// Verifies spacing around a <c>&gt;</c> character in tuple types.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
