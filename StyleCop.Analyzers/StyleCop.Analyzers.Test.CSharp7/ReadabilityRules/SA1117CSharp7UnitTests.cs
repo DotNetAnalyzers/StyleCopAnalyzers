21c914ae29bcd74fe7affdbe542c70a92e0f5dd9
Sam Harwell <sam@tunnelvisionlabs.com>
2017-06-11 18:44:07 -0500

    Add support for local functions to SA1117

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/ReadabilityRules/SA1117CSharp7UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/ReadabilityRules/SA1117CSharp7UnitTests.cs
@@ -3,9 +3,63 @@
 
 namespace StyleCop.Analyzers.Test.CSharp7.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.ReadabilityRules;
+    using TestHelper;
+    using Xunit;
 
     public class SA1117CSharp7UnitTests : SA1117UnitTests
     {
+        [Fact]
+        public async Task TestValidLocalFunctionsAsync()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Method()
+    {
+        void LocalFunction1(
+            int a, int b, string s) { }
+
+        void LocalFunction2(
+            int a,
+            int b,
+            string s) { }
+
+        object LocalFunction3(int a, int b, string s) => null;
+    }
+}";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestInvalidLocalFunctionsAsync()
+        {
+            var testCode = @"
+class Foo
+{
+    public void Method()
+    {
+        object LocalFunction(int a, int b,
+ string s) => null;
+    }
+}";
+            var fixedCode = @"
+class Foo
+{
+    public void Method()
+    {
+        object LocalFunction(
+            int a,
+            int b,
+ string s) => null;
+    }
+}";
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(7, 2);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            ////await this.VerifyCSharpFixAsync(testCode, fixedCode, cancellationToken: CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
