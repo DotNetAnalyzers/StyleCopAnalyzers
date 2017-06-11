3761711ebcd3b5ea13079c1affbb23a98dfe2f8b
Sam Harwell <sam@tunnelvisionlabs.com>
2017-06-11 08:16:36 -0500

    Add SA1008 tests for ref expressions

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/SpacingRules/SA1008CSharp7UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/SpacingRules/SA1008CSharp7UnitTests.cs
@@ -3,9 +3,62 @@
 
 namespace StyleCop.Analyzers.Test.CSharp7.SpacingRules
 {
-    using Test.SpacingRules;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using StyleCop.Analyzers.Test.SpacingRules;
+    using TestHelper;
+    using Xunit;
+
+    using static StyleCop.Analyzers.SpacingRules.SA1008OpeningParenthesisMustBeSpacedCorrectly;
 
     public class SA1008CSharp7UnitTests : SA1008UnitTests
     {
+        /// <summary>
+        /// Verifies that spacing for <c>ref</c> expressions is handled properly.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestRefExpressionAsync()
+        {
+            var testCode = @"namespace TestNamespace
+{
+    using System.Threading.Tasks;
+
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            int test = 1;
+            ref int t = ref( test);
+        }
+    }
+}
+";
+
+            var fixedCode = @"namespace TestNamespace
+{
+    using System.Threading.Tasks;
+
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            int test = 1;
+            ref int t = ref (test);
+        }
+    }
+}
+";
+
+            DiagnosticResult[] expectedDiagnostics =
+            {
+                this.CSharpDiagnostic(DescriptorPreceded).WithLocation(10, 28),
+                this.CSharpDiagnostic(DescriptorNotFollowed).WithLocation(10, 28),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expectedDiagnostics, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedCode, numberOfFixAllIterations: 2).ConfigureAwait(false);
+        }
     }
 }
