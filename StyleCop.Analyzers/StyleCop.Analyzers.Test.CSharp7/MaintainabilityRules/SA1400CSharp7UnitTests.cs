689c9797aa03dc23692f0364a7de697d1f8220e1
Sam Harwell <sam@tunnelvisionlabs.com>
2017-06-12 06:30:32 -0500

    Add SA1400 test for local functions

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/MaintainabilityRules/SA1400CSharp7UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/MaintainabilityRules/SA1400CSharp7UnitTests.cs
@@ -3,9 +3,33 @@
 
 namespace StyleCop.Analyzers.Test.CSharp7.MaintainabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.MaintainabilityRules;
+    using Xunit;
 
     public class SA1400CSharp7UnitTests : SA1400UnitTests
     {
+        /// <summary>
+        /// Verifies that local functions, which do not support access modifiers, do not trigger SA1400.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestLocalFunctionAsync()
+        {
+            var testCode = @"
+internal class ClassName
+{
+    public void MethodName()
+    {
+        void LocalFunction()
+        {
+        }
+    }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
