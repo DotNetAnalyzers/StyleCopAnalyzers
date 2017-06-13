2143ed5fb149115b6b743e12c719ed3736c6b262
Sam Harwell <sam@tunnelvisionlabs.com>
2017-06-13 07:22:52 -0500

    Add local functions tests to SA1505, SA1508, SA1509, SA1513

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/LayoutRules/SA1509CSharp7UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/LayoutRules/SA1509CSharp7UnitTests.cs
@@ -3,9 +3,78 @@
 
 namespace StyleCop.Analyzers.Test.CSharp7.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.LayoutRules;
+    using TestHelper;
+    using Xunit;
 
     public class SA1509CSharp7UnitTests : SA1509UnitTests
     {
+        [Fact]
+        public async Task TestLocalFunctionDeclarationOpeningBraceHasBlankLineAsync()
+        {
+            var testCode = @"
+class Foo
+{
+    void Method()
+    {
+        void Bar()
+
+        {
+        }
+    }
+}";
+            var fixedCode = @"
+class Foo
+{
+    void Method()
+    {
+        void Bar()
+        {
+        }
+    }
+}";
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(8, 9);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestLocalFunctionDeclarationOpeningBraceHasTwoBlankLineAsync()
+        {
+            var testCode = @"
+class Foo
+{
+    void Method()
+    {
+        void Bar()
+
+
+        {
+        }
+    }
+}";
+
+            var fixedCode = @"
+class Foo
+{
+    void Method()
+    {
+        void Bar()
+        {
+        }
+    }
+}";
+
+            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(9, 9);
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpDiagnosticAsync(fixedCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
     }
 }
