bd7b8528b462b3c7d204372f3a553b1b7ddeabad
Dennis Fischer <fischer_dennis@live.de>
2017-10-24 02:52:10 +0200

    Support the ref keyword in SA1206

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/OrderingRules/SA1206CSharp7UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp7/OrderingRules/SA1206CSharp7UnitTests.cs
@@ -3,9 +3,37 @@
 
 namespace StyleCop.Analyzers.Test.CSharp7.OrderingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.OrderingRules;
+    using TestHelper;
+    using Xunit;
 
     public class SA1206CSharp7UnitTests : SA1206UnitTests
     {
+        [Fact(Skip = "The version of the compiler used in these tests does not yet support this feature.")]
+        public async Task TestRefKeywordInStructDeclarationAsync()
+        {
+            var testCode = @"private ref struct BitHelper
+{
+}
+";
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact(Skip = "The version of the compiler used in these tests does not yet support this feature.")]
+        public async Task TestRefKeywordInStructDeclarationWrongOrderAsync()
+        {
+            var testCode = @"ref private struct BitHelper
+{
+}
+";
+
+            DiagnosticResult[] expected = new[]
+            {
+                this.CSharpDiagnostic().WithLocation(1, 13).WithArguments("ref", "private"),
+            };
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
