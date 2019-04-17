8528725a591fa6e68e5657443d3d13d06a48cde3
Vincent Weijsters <vweijsters@outlook.com>
2019-04-17 12:41:20 +0200

    Updated opencover-report.ps1

--- build/opencover-report.ps1
+++ build/opencover-report.ps1
@@ -40,6 +40,7 @@ $report_folder = '.\OpenCover.Reports'
 $symbols_folder = '.\OpenCover.Symbols'
 $target_dll = "..\StyleCop.Analyzers\StyleCop.Analyzers.Test\bin\$Configuration\net452\StyleCop.Analyzers.Test.dll"
 $target_dll_csharp7 = "..\StyleCop.Analyzers\StyleCop.Analyzers.Test.CSharp7\bin\$Configuration\net46\StyleCop.Analyzers.Test.CSharp7.dll"
+$target_dll_csharp8 = "..\StyleCop.Analyzers\StyleCop.Analyzers.Test.CSharp8\bin\$Configuration\net472\StyleCop.Analyzers.Test.CSharp8.dll"
 
 If (Test-Path $symbols_folder) {
 	Remove-Item -Recurse -Force $symbols_folder
@@ -47,9 +48,11 @@ If (Test-Path $symbols_folder) {
 
 $symbols_folder_csharp6 = Join-Path $symbols_folder 'CSharp6'
 $symbols_folder_csharp7 = Join-Path $symbols_folder 'CSharp7'
+$symbols_folder_csharp8 = Join-Path $symbols_folder 'CSharp8'
 mkdir $symbols_folder | Out-Null
 mkdir $symbols_folder_csharp6 | Out-Null
 mkdir $symbols_folder_csharp7 | Out-Null
+mkdir $symbols_folder_csharp8 | Out-Null
 
 function Convert-Coverage-Pdb() {
 	param (
@@ -98,6 +101,10 @@ $target_dir = [IO.Path]::GetDirectoryName($target_dll_csharp7)
 Get-ChildItem $target_dir -Filter *.dll | Foreach-Object { Convert-Coverage-Pdb -assembly $_.FullName -outputDir $symbols_folder_csharp7 }
 Get-ChildItem $target_dir -Filter *.dll | Foreach-Object { Extract-Coverage-Pdb -assembly $_.FullName -outputDir $symbols_folder_csharp7 }
 
+$target_dir = [IO.Path]::GetDirectoryName($target_dll_csharp8)
+Get-ChildItem $target_dir -Filter *.dll | Foreach-Object { Convert-Coverage-Pdb -assembly $_.FullName -outputDir $symbols_folder_csharp8 }
+Get-ChildItem $target_dir -Filter *.dll | Foreach-Object { Extract-Coverage-Pdb -assembly $_.FullName -outputDir $symbols_folder_csharp8 }
+
 If (Test-Path $report_folder) {
 	Remove-Item -Recurse -Force $report_folder
 }
@@ -139,6 +146,25 @@ If ($AppVeyor -and -not $?) {
 	-mergebyhash -mergeoutput `
 	-target:"$xunit_runner_console" `
 	-targetargs:"$target_dll_csharp7 -noshadow $AppVeyorArg"
+
+If ($AppVeyor -and -not $?) {
+	$host.UI.WriteErrorLine('Build failed; coverage analysis aborted.')
+	Exit $LASTEXITCODE
+}
+
+&$opencover_console `
+	-register:user `
+	-threshold:1 -oldStyle `
+	-returntargetcode `
+	-hideskipped:All `
+	-filter:"+[StyleCop*]*" `
+	-excludebyattribute:*.ExcludeFromCodeCoverage* `
+	-excludebyfile:*\*Designer.cs `
+	-searchdirs:"$symbols_folder_csharp8" `
+	-output:"$report_folder\OpenCover.StyleCopAnalyzers.xml" `
+	-mergebyhash -mergeoutput `
+	-target:"$xunit_runner_console" `
+	-targetargs:"$target_dll_csharp8 -noshadow $AppVeyorArg"
 
 If ($AppVeyor -and -not $?) {
 	$host.UI.WriteErrorLine('Build failed; coverage analysis aborted.')
