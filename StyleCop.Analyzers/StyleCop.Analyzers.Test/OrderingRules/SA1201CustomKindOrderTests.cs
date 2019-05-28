279481335a55b9843f8fa8a6687fcbb39f80cac6
Max DePalma <madepal@microsoft.com>
2019-05-27 19:39:14 -0700

    added more tests

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1201CustomKindOrderTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1201CustomKindOrderTests.cs
@@ -3,6 +3,7 @@
 
 namespace StyleCop.Analyzers.Test.OrderingRules
 {
+    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis.CSharp;
@@ -18,7 +19,14 @@ namespace StyleCop.Analyzers.Test.OrderingRules
     /// </summary>
     public class SA1201CustomKindOrderTests
     {
-        private const string TestSettings = @"
+        /// <summary>
+        /// Verifies that a custom kind order within a type declaration produces expected diagnostics.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestTypeDeclarationCustomerOrderAsync()
+        {
+            var settings = @"
 {
   ""settings"": {
     ""orderingRules"": {
@@ -29,15 +37,8 @@ namespace StyleCop.Analyzers.Test.OrderingRules
   }
 }
 ";
-
-        /// <summary>
-        /// Verifies that a custom kind order within a type declaration produces expected diagnostics.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact]
-        public async Task TestTypeDeclarationCustomerOrderAsync()
-        {
-            var testCode = @"namespace TestNamespace
+            var testCode = @"
+namespace TestNamespace
 {
     public class TestClass
     {
@@ -57,10 +58,83 @@ namespace StyleCop.Analyzers.Test.OrderingRules
 }
 ";
             var diagnosticResult = Diagnostic()
-                .WithLocation(9, 16)
+                .WithLocation(10, 16)
                 .WithArguments("constructor", "property");
 
-            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None, TestSettings).ConfigureAwait(false);
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None, settings).ConfigureAwait(false);
+            await VerifyCSharpDiagnosticAsync(testCode, diagnosticResult, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that a custom kind order within a namespace declaration produces expected diagnostics.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestNamespaceDeclarationCustomerOrderAsync()
+        {
+            var settings = @"
+{
+  ""settings"": {
+    ""orderingRules"": {
+      ""kindOrder"": {
+        ""namespaceDeclaration"": [ ""enum"", ""interface"", ""class"", ""struct"" ]
+      }
+    }
+  }
+}
+";
+            var testCode = @"
+namespace TestNamespace
+{
+    public class TestClass
+    {
+    }
+    
+    public struct TestStruct
+    {
+    }
+}
+";
+            var diagnosticResult = Diagnostic()
+                .WithLocation(8, 19)
+                .WithArguments("struct", "class");
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None, settings).ConfigureAwait(false);
+            await VerifyCSharpDiagnosticAsync(testCode, diagnosticResult, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that a custom kind order within a compilation unit produces expected diagnostics.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TesCompilationUnitCustomerOrderAsync()
+        {
+            var settings = @"
+{
+  ""settings"": {
+    ""orderingRules"": {
+      ""kindOrder"": {
+        ""compilationUnit"": [ ""enum"", ""interface"", ""class"", ""struct"" ]
+      }
+    }
+  }
+}
+";
+            var testCode = @"
+    public class TestClass
+    {
+    }
+    
+    public struct TestStruct
+    {
+    }
+";
+            var diagnosticResult = Diagnostic()
+                .WithLocation(6, 19)
+                .WithArguments("struct", "class");
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None, settings).ConfigureAwait(false);
             await VerifyCSharpDiagnosticAsync(testCode, diagnosticResult, CancellationToken.None).ConfigureAwait(false);
         }
 
@@ -81,7 +155,7 @@ namespace StyleCop.Analyzers.Test.OrderingRules
             return test.RunAsync(cancellationToken);
         }
 
-        private static Task VerifyCSharpDiagnosticAsync(string source, DiagnosticResult[] expected, CancellationToken cancellationToken, string settings = null)
+        private static Task VerifyCSharpDiagnosticAsync(string source, IEnumerable<DiagnosticResult> expected, CancellationToken cancellationToken, string settings = null)
         {
             var test = new StyleCopDiagnosticVerifier<SA1201ElementsMustAppearInTheCorrectOrder>.CSharpTest
             {
