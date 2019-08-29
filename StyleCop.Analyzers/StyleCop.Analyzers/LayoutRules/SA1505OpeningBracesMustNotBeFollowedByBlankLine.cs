e4c52f614e4110e983fec6bfab7d03d31528dc18
nnpcYvIVl <nnpcYvIVl@users.noreply.github.com>
2019-08-29 13:57:43 -0500

    Update SA1505OpeningBracesMustNotBeFollowedByBlankLine.cs

--- StyleCop.Analyzers/StyleCop.Analyzers/LayoutRules/SA1505OpeningBracesMustNotBeFollowedByBlankLine.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/LayoutRules/SA1505OpeningBracesMustNotBeFollowedByBlankLine.cs
@@ -46,7 +46,7 @@ namespace StyleCop.Analyzers.LayoutRules
         /// </summary>
         public const string DiagnosticId = "SA1505";
         private const string Title = "Opening braces should not be followed by blank line";
-        private const string MessageFormat = "An opening brace should not be followed by a blank line.";
+        private const string MessageFormat = "An opening brace should not be followed by a blank line";
         private const string Description = "An opening brace within a C# element, statement, or expression is followed by a blank line.";
         private const string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1505.md";
 
