4127f488b89cef81fbabccdc355ee233219e3f1c
Anton Paulovich <p.a.n.tosha@gmail.com>
2019-11-17 13:54:18 +0300

    issue #3067 is not reproduced

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/LayoutRules/SA1514CSharp8UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/LayoutRules/SA1514CSharp8UnitTests.cs
@@ -3,9 +3,42 @@
 
 namespace StyleCop.Analyzers.Test.CSharp8.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp7.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+    StyleCop.Analyzers.LayoutRules.SA1514ElementDocumentationHeaderMustBePrecededByBlankLine,
+    StyleCop.Analyzers.LayoutRules.SA1514CodeFixProvider>;
 
     public class SA1514CSharp8UnitTests : SA1514CSharp7UnitTests
     {
+        /// <summary>
+        /// Verifies that method-like declarations with invalid documentation will produce the expected diagnostics.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestValidPropertyDeclarationAsync()
+        {
+            var testCode = @"namespace TestNamespace
+{
+    public class TestClass
+    {
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public string SomeString { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public string AnotherString { get; set; } = null!;
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
