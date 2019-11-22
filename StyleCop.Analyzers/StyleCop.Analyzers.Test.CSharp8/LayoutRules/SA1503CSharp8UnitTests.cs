ef8d748ab1614a6c8141d7c511f58b6117ca05e5
Sam Harwell <sam@tunnelvisionlabs.com>
2019-11-21 16:47:34 -0800

    Add tests for using declaration statements
    Closes #3074
    Closes #3075

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/LayoutRules/SA1503CSharp8UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/LayoutRules/SA1503CSharp8UnitTests.cs
@@ -3,9 +3,32 @@
 
 namespace StyleCop.Analyzers.Test.CSharp8.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp7.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1503BracesMustNotBeOmitted,
+        StyleCop.Analyzers.LayoutRules.SA1503CodeFixProvider>;
 
     public class SA1503CSharp8UnitTests : SA1503CSharp7UnitTests
     {
+        [Fact]
+        [WorkItem(3074, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3074")]
+        public async Task TestNoDiagnosticForUsingDeclarationStatementAsync()
+        {
+            var testCode = @"
+using System.IO;
+public class Foo
+{
+    public void Method()
+    {
+        using var v = new MemoryStream();
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
