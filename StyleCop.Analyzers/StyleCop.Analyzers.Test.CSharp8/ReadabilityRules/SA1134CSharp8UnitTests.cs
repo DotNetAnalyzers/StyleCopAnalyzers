1698fdaac07f0b0ae219bad9f1551e3639c9225e
Vincent Weijsters <vweijsters@outlook.com>
2020-07-28 00:34:30 +0200

    Disabled 'VerifyInvalidMemberSyntaxInCodeFixAsync' in the CSharp8 test project, as compiler behavior has changed.

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/ReadabilityRules/SA1134CSharp8UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/ReadabilityRules/SA1134CSharp8UnitTests.cs
@@ -3,9 +3,19 @@
 
 namespace StyleCop.Analyzers.Test.CSharp8.ReadabilityRules
 {
+    using System.Threading.Tasks;
+
     using StyleCop.Analyzers.Test.CSharp7.ReadabilityRules;
 
     public class SA1134CSharp8UnitTests : SA1134CSharp7UnitTests
     {
+        /// <inheritdoc/>
+        public override Task VerifyInvalidMemberSyntaxInCodeFixAsync()
+        {
+            // Making this test a dummy, as the 3.6.0 compiler actually parses the invalid syntax
+            // into a valid AttributeSyntaxList, with an attribute named ';' (which is an invalid name).
+            // Because of this, the code fix no longer fails, but it ofcourse produces garbage.
+            return Task.CompletedTask;
+        }
     }
 }
