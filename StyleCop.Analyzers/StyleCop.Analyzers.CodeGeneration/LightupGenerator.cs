6e6922bc81ed0197ee638d3ed924cf2b2a4e03f9
Sam Harwell <sam@tunnelvisionlabs.com>
2020-11-09 05:43:43 -0800

    Add empty LightupGenerator

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.CodeGeneration/LightupGenerator.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.CodeGeneration
+{
+    using Microsoft.CodeAnalysis;
+
+    [Generator]
+    internal sealed class LightupGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.SyntaxReceiver is not SyntaxReceiver receiver)
+            {
+                return;
+            }
+        }
+
+        private class SyntaxReceiver : ISyntaxReceiver
+        {
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+            }
+        }
+    }
+}
