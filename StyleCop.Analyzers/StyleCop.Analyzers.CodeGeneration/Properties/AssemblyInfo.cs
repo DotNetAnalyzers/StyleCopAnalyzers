62aaf36e904fbaed6af862814aee406bb6be35a4
Sam Harwell <sam@tunnelvisionlabs.com>
2020-11-09 05:38:31 -0800

    Hook up StyleCop.Analyzers.CodeGeneration

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.CodeGeneration/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: CLSCompliant(false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
