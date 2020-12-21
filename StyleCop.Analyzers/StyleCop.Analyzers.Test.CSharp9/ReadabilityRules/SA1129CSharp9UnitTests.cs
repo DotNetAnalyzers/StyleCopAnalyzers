46d2e37fed1e471446f32c88c6bdaf2530239570
Sam Harwell <sam@tunnelvisionlabs.com>
2020-12-21 07:47:45 -0800

    Support implicit object creation expressions in SA1129 code fix
    Fixes #3277

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1129CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1129CSharp9UnitTests.cs
@@ -9,8 +9,8 @@ namespace StyleCop.Analyzers.Test.CSharp9.ReadabilityRules
     using StyleCop.Analyzers.Test.CSharp8.ReadabilityRules;
     using Xunit;
     using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
-    StyleCop.Analyzers.ReadabilityRules.SA1129DoNotUseDefaultValueTypeConstructor,
-    StyleCop.Analyzers.ReadabilityRules.SA1129CodeFixProvider>;
+        StyleCop.Analyzers.ReadabilityRules.SA1129DoNotUseDefaultValueTypeConstructor,
+        StyleCop.Analyzers.ReadabilityRules.SA1129CodeFixProvider>;
 
     public class SA1129CSharp9UnitTests : SA1129CSharp8UnitTests
     {
@@ -19,13 +19,14 @@ namespace StyleCop.Analyzers.Test.CSharp9.ReadabilityRules
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
         [Fact]
+        [WorkItem(3277, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3277")]
         public async Task VerifyValueTypeWithTargetTypeNewAsync()
         {
             var testCode = @"struct S
 {
     internal static S F()
     {
-        S s = {|#0:new()|};
+        S s = [|new()|];
         return s;
     }
 }
@@ -35,17 +36,13 @@ namespace StyleCop.Analyzers.Test.CSharp9.ReadabilityRules
 {
     internal static S F()
     {
-        S s = default;
+        S s = default(S);
         return s;
     }
 }
 ";
-            DiagnosticResult[] expected =
-            {
-                Diagnostic().WithLocation(0),
-            };
 
-            await VerifyCSharpFixAsync(testCode, expected, fixedTestCode, CancellationToken.None).ConfigureAwait(false);
+            await VerifyCSharpFixAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, fixedTestCode, CancellationToken.None).ConfigureAwait(false);
         }
     }
 }
