57bc54d5d144908a33fa7bf1707b9fd1dfef6f02
Dan Moseley <danmose@microsoft.com>
2020-12-20 18:43:20 -0800

    Test for #3277

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1129CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1129CSharp9UnitTests.cs
@@ -3,9 +3,49 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp8.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+    StyleCop.Analyzers.ReadabilityRules.SA1129DoNotUseDefaultValueTypeConstructor,
+    StyleCop.Analyzers.ReadabilityRules.SA1129CodeFixProvider>;
 
     public class SA1129CSharp9UnitTests : SA1129CSharp8UnitTests
     {
+        /// <summary>
+        /// Verifies that target type new expressions for value types will generate diagnostics.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task VerifyValueTypeWithTargetTypeNewAsync()
+        {
+            var testCode = @"struct S
+{
+    internal static S F()
+    {
+        S s = {|#0:new()|};
+        return s;
+    }
+}
+";
+
+            var fixedTestCode = @"struct S
+{
+    internal static S F()
+    {
+        S s = default;
+        return s;
+    }
+}
+";
+            DiagnosticResult[] expected =
+            {
+                Diagnostic().WithLocation(0),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedTestCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
