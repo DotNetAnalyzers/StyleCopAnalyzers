7754e6b38673ef19ec79ab907a8c23c49c09a29b
Björn Hellander <bjorn.hellander@gmail.com>
2021-11-28 09:22:43 +0100

    Issue #3386: Updated SA1003 to correctly handle a cast inside a range expression

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/SpacingRules/SA1003CSharp8UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/SpacingRules/SA1003CSharp8UnitTests.cs
@@ -3,9 +3,70 @@
 
 namespace StyleCop.Analyzers.Test.CSharp8.SpacingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp7.SpacingRules;
+    using StyleCop.Analyzers.Test.Verifiers;
+    using Xunit;
+
+    using static StyleCop.Analyzers.SpacingRules.SA1003SymbolsMustBeSpacedCorrectly;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.SpacingRules.SA1003SymbolsMustBeSpacedCorrectly,
+        StyleCop.Analyzers.SpacingRules.SA1003CodeFixProvider>;
 
     public class SA1003CSharp8UnitTests : SA1003CSharp7UnitTests
     {
+        /// <summary>
+        /// Verifies that spacing around a range expression double dots isn't required.
+        /// </summary>
+        /// <remarks>
+        /// <para>Double dots of range expressions already provide enough spacing for readability so there is no
+        /// need to surround the range expression with spaces.</para>
+        /// </remarks>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestRangeExpressionAsync()
+        {
+            var testCode = SpecialTypeDefinitions.IndexAndRange + @"
+namespace TestNamespace
+{
+    using System;
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            var test1 = .. (int)1;
+        }
+    }
+}
+";
+
+            var fixedCode = SpecialTypeDefinitions.IndexAndRange + @"
+namespace TestNamespace
+{
+    using System;
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            var test1 = ..(int)1;
+        }
+    }
+}
+";
+            var expectedResults = new DiagnosticResult[]
+            {
+                Diagnostic(DescriptorNotPrecededByWhitespace).WithLocation(26, 28).WithArguments("(int)"),
+            };
+
+            await VerifyCSharpFixAsync(
+                LanguageVersion.CSharp8,
+                testCode,
+                expectedResults,
+                fixedCode,
+                CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
