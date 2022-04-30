09125485c75ee2a37def1d921fa2ea9edd0770e2
Björn Hellander <bjorn.hellander@gmail.com>
2022-04-30 07:16:17 +0200

    Removed redundant code

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp11/ReadabilityRules/SA1101CSharp11UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp11/ReadabilityRules/SA1101CSharp11UnitTests.cs
@@ -3,15 +3,7 @@
 
 namespace StyleCop.Analyzers.Test.CSharp11.ReadabilityRules
 {
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Microsoft.CodeAnalysis.CSharp;
     using StyleCop.Analyzers.Test.CSharp10.ReadabilityRules;
-    using StyleCop.Analyzers.Test.Verifiers;
-    using Xunit;
-    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
-        StyleCop.Analyzers.ReadabilityRules.SA1101PrefixLocalCallsWithThis,
-        StyleCop.Analyzers.ReadabilityRules.SA1101CodeFixProvider>;
 
     public class SA1101CSharp11UnitTests : SA1101CSharp10UnitTests
     {
