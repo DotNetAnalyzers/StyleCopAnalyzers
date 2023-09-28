d1d8ee998642876665f87b7b2fafe8550f33d608
Sam Harwell <Sam.Harwell@microsoft.com>
2023-09-28 14:22:12 -0500

    Generate derived test classes

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/AnalyzerConfigurationCSharp10UnitTests.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Test.CSharp10
+{
+    using StyleCop.Analyzers.Test.CSharp9;
+
+    public partial class AnalyzerConfigurationCSharp10UnitTests : AnalyzerConfigurationCSharp9UnitTests
+    {
+    }
+}
---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp8/Lightup/IsPatternExpressionSyntaxWrapperCSharp8UnitTests.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Test.CSharp8.Lightup
+{
+    using StyleCop.Analyzers.Test.CSharp7.Lightup;
+
+    public partial class IsPatternExpressionSyntaxWrapperCSharp8UnitTests : IsPatternExpressionSyntaxWrapperCSharp7UnitTests
+    {
+    }
+}
