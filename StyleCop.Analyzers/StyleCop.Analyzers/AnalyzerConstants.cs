34c82db3f3931796774373d9338069f7b2f9e885
Sam Harwell <Sam.Harwell@microsoft.com>
2023-12-27 11:06:48 -0600

    Remove unnecessary property AnalyzerConstants.DisabledNoTests

--- StyleCop.Analyzers/StyleCop.Analyzers/AnalyzerConstants.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/AnalyzerConstants.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-#nullable disable
-
 namespace StyleCop.Analyzers
 {
     using System.Diagnostics.CodeAnalysis;
@@ -10,30 +8,6 @@ namespace StyleCop.Analyzers
 
     internal static class AnalyzerConstants
     {
-        static AnalyzerConstants()
-        {
-#if DEBUG
-            // In DEBUG builds, the tests are enabled to simplify development and testing.
-            DisabledNoTests = true;
-#else
-            DisabledNoTests = false;
-#endif
-        }
-
-        /// <summary>
-        /// Gets a reference value which can be passed to
-        /// <see cref="DiagnosticDescriptor(string, string, string, string, DiagnosticSeverity, bool, string, string, string[])"/>
-        /// to disable a diagnostic which is currently untested.
-        /// </summary>
-        /// <value>
-        /// A reference value which can be passed to
-        /// <see cref="DiagnosticDescriptor(string, string, string, string, DiagnosticSeverity, bool, string, string, string[])"/>
-        /// to disable a diagnostic which is currently untested.
-        /// </value>
-        [ExcludeFromCodeCoverageInternal]
-        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1623:Property summary documentation should match accessors.", Justification = "This property behaves more like an opaque value than a Boolean.")]
-        internal static bool DisabledNoTests { get; }
-
         /// <summary>
         /// Gets a reference value which can be passed to
         /// <see cref="DiagnosticDescriptor(string, string, string, string, DiagnosticSeverity, bool, string, string, string[])"/>
