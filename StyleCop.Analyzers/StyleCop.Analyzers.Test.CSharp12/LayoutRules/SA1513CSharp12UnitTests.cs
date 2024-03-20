297d44ce29074d2f14970c609aeb734079a7f0d5
David Grieser <dg@devigus.com>
2024-03-20 15:18:20 +0100

    Moved 'TestJoinIntoClauseSyntaxQueryExpressionAsync' test case

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp12/LayoutRules/SA1513CSharp12UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp12/LayoutRules/SA1513CSharp12UnitTests.cs
@@ -8,7 +8,6 @@ namespace StyleCop.Analyzers.Test.CSharp12.LayoutRules
     using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp11.LayoutRules;
     using Xunit;
-
     using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
         StyleCop.Analyzers.LayoutRules.SA1513ClosingBraceMustBeFollowedByBlankLine,
         StyleCop.Analyzers.LayoutRules.SA1513CodeFixProvider>;
@@ -28,77 +27,6 @@ public class Foo
         {
         }
     ];
-}";
-
-            await VerifyCSharpFixAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, testCode, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        [Fact]
-        public async Task TestJoinIntoClauseSyntaxQueryExpressionAsync()
-        {
-            var testCode = @"
-using System.Collections.Generic;
-using System.Linq;
-
-public class JoinIntoClauseSyntaxQueryExpressionTest
-{
-    public JoinIntoClauseSyntaxQueryExpressionTest()
-    {
-        List<Foo> fooList =
-        [
-            new(1, ""First""),
-            new(2, ""Second""),
-            new(3, ""Third""),
-            new(1, ""Fourth"")
-        ];
-
-        List<Bar> barList =
-        [
-            new(1, ""First""),
-            new(2, ""Second""),
-            new(3, ""Third"")
-        ];
-
-        var query =
-            from foo in fooList
-            join bar in barList
-                on new
-                {
-                    foo.Id,
-                    foo.Name
-                }
-                equals new
-                {
-                    bar.Id,
-                    bar.Name
-                }
-            into grouping
-            from joined in grouping.DefaultIfEmpty()
-            select new
-            {
-                joined.Id,
-                BarName = joined.Name,
-                FooName = foo.Name
-            };
-    }
-
-    private class Foo(
-        int id,
-        string name)
-    {
-        public int Id { get; } = id;
-
-        public string Name { get; } = name;
-    }
-
-    private class Bar(
-        int id,
-        string name)
-    {
-        public int Id { get; } = id;
-
-        public string Name { get; } = name;
-    }
 }";
 
             await VerifyCSharpFixAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, testCode, CancellationToken.None).ConfigureAwait(false);
