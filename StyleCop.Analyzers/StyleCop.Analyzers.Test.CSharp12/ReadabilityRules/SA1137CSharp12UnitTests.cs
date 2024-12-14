d22f8a0fab3f97d417b87c4ceed4a433a25f7f11
Mivee <gerber-lukas@t-online.de>
2024-12-15 00:41:20 +0100

    fixed https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3904 Collectionexpressions are detected as variable declarations and not as InitializerExpression
    - Filter based on the declared variables collectionexpressions out, and then check each element

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp12/ReadabilityRules/SA1137CSharp12UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp12/ReadabilityRules/SA1137CSharp12UnitTests.cs
@@ -3,9 +3,61 @@
 
 namespace StyleCop.Analyzers.Test.CSharp12.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp11.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopDiagnosticVerifier<
+        StyleCop.Analyzers.ReadabilityRules.SA1137ElementsShouldHaveTheSameIndentation>;
 
     public partial class SA1137CSharp12UnitTests : SA1137CSharp11UnitTests
     {
+        [Fact]
+        [WorkItem(3904, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3904")]
+        public async Task TestCollectionInitializationAsync()
+        {
+            var csharp = new CSharpTest()
+            {
+                TestSources =
+                {
+                    @"
+                    class FirstCase
+                    {
+                        private readonly System.Collections.Generic.List<string> example1 = 
+                        [
+                            ""a"",
+                    ""b"",
+                                    ""c"",
+                        ];
+                    }",
+                    @"
+                    class SecondCase
+                    {
+                        private readonly System.Collections.Generic.List<int> example2 = 
+                        [
+                            1,
+                            2,
+                    3,
+                    4,
+                        ];
+                    }",
+                    @"
+                    class ThirdCase
+                    {
+                        private readonly System.Collections.Generic.List<int> example3 = [1, 2, 3, 4];
+                    }",
+                },
+                ExpectedDiagnostics =
+                {
+                    DiagnosticResult.CompilerWarning("SA1137").WithLocation("/0/Test0.cs", 7, 1),
+                    DiagnosticResult.CompilerWarning("SA1137").WithLocation("/0/Test0.cs", 8, 1),
+                    DiagnosticResult.CompilerWarning("SA1137").WithLocation("/0/Test1.cs", 8, 1),
+                    DiagnosticResult.CompilerWarning("SA1137").WithLocation("/0/Test1.cs", 9, 1),
+                },
+            };
+
+            await csharp.RunAsync(CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
