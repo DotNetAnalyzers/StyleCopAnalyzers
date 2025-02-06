4e05ed0924781ee969f3f81cdf7243699015ebd8
Łukasz Mrozek <morskk@gmail.com>
2025-02-06 02:00:31 +0100

    Update SA1013 to allow whitespace before } when it starts a line inside interpolated string
    Closes #3914

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp11/SpacingRules/SA1013CSharp11UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp11/SpacingRules/SA1013CSharp11UnitTests.cs
@@ -3,9 +3,39 @@
 
 namespace StyleCop.Analyzers.Test.CSharp11.SpacingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp10.SpacingRules;
+    using Xunit;
+
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopDiagnosticVerifier<
+        StyleCop.Analyzers.SpacingRules.SA1013ClosingBracesMustBeSpacedCorrectly>;
 
     public partial class SA1013CSharp11UnitTests : SA1013CSharp10UnitTests
     {
+        /// <summary>
+        /// Verifies the behavior of closing braces in interpolated strings.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        [WorkItem(3914, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3914")]
+        public async Task TestSpacingAroundClosingBraceInInterpolatedStringsAsync()
+        {
+            const string testCode = @"using System;
+
+public class Foo
+{
+    public string TestMethod(object value)
+    {
+        // The space before '}' is not checked
+        return $""Some random text {
+                value
+            } and some more random text."";
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
