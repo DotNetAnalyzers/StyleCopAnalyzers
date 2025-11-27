b8587375d995a50c2a53d67679b5ffb2d9f9f2f2
Björn Hellander <bjorn.hellander@gmail.com>
2025-11-27 22:03:06 +0100

    Update SA1649CodeFixProvider to use method WithDocumentName in Solution if it is available #2277

--- StyleCop.Analyzers/StyleCop.Analyzers.CodeFixes/DocumentationRules/SA1649CodeFixProvider.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.CodeFixes/DocumentationRules/SA1649CodeFixProvider.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-#nullable disable
-
 namespace StyleCop.Analyzers.DocumentationRules
 {
     using System.Collections.Immutable;
@@ -14,6 +12,7 @@ namespace StyleCop.Analyzers.DocumentationRules
     using Microsoft.CodeAnalysis.CodeActions;
     using Microsoft.CodeAnalysis.CodeFixes;
     using StyleCop.Analyzers.Helpers;
+    using StyleCop.Analyzers.Lightup;
 
     /// <summary>
     /// Implements a code fix for <see cref="SA1649FileNameMustMatchTypeName"/>.
@@ -27,7 +26,7 @@ namespace StyleCop.Analyzers.DocumentationRules
             ImmutableArray.Create(SA1649FileNameMustMatchTypeName.DiagnosticId);
 
         /// <inheritdoc/>
-        public override FixAllProvider GetFixAllProvider()
+        public override FixAllProvider? GetFixAllProvider()
         {
             // The batch fixer can't handle code fixes that create new files
             return null;
@@ -55,25 +54,32 @@ namespace StyleCop.Analyzers.DocumentationRules
             var syntaxRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
             var expectedFileName = diagnostic.Properties[SA1649FileNameMustMatchTypeName.ExpectedFileNameKey];
 
-            var newSolution = ReplaceDocument(solution, document, document.Id, syntaxRoot, expectedFileName);
+            var newSolution = RenameDocument(solution, document, document.Id, syntaxRoot, expectedFileName);
 
             // Make sure to also update other projects which reference the same file
             foreach (var linkedDocumentId in document.GetLinkedDocumentIds())
             {
-                newSolution = ReplaceDocument(newSolution, null, linkedDocumentId, syntaxRoot, expectedFileName);
+                newSolution = RenameDocument(newSolution, null, linkedDocumentId, syntaxRoot, expectedFileName);
             }
 
             return newSolution;
         }
 
-        private static Solution ReplaceDocument(Solution solution, Document document, DocumentId documentId, SyntaxNode syntaxRoot, string expectedFileName)
+        private static Solution RenameDocument(Solution solution, Document? document, DocumentId documentId, SyntaxNode syntaxRoot, string expectedFileName)
         {
-            document ??= solution.GetDocument(documentId);
+            // First try to use the "new" WithDocumentName method. This will return null if it is not available in the current Roslyn version.
+            var newSolution = solution.WithDocumentName(documentId, expectedFileName);
+            if (newSolution != null)
+            {
+                return newSolution;
+            }
 
+            // Continue by instead removing and re-adding the file again
+            document ??= solution.GetDocument(documentId);
             var newDocumentFilePath = document.FilePath != null ? Path.Combine(Path.GetDirectoryName(document.FilePath), expectedFileName) : null;
             var newDocumentId = DocumentId.CreateNewId(documentId.ProjectId);
 
-            var newSolution = solution
+            newSolution = solution
                 .RemoveDocument(documentId)
                 .AddDocument(newDocumentId, expectedFileName, syntaxRoot, document.Folders, newDocumentFilePath);
             return newSolution;
