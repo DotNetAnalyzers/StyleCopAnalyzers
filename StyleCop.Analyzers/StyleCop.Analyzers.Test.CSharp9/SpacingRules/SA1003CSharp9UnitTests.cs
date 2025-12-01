1142c4a281958e00506e02c563c9d986c9a7562a
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-01 17:47:04 -0600

    Update tests for target-typed conditional expressions
    Closes #3974

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/SpacingRules/SA1003CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/SpacingRules/SA1003CSharp9UnitTests.cs
@@ -3,9 +3,48 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.SpacingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp8.SpacingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.SpacingRules.SA1003SymbolsMustBeSpacedCorrectly;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.SpacingRules.SA1003SymbolsMustBeSpacedCorrectly,
+        StyleCop.Analyzers.SpacingRules.SA1003CodeFixProvider>;
 
     public partial class SA1003CSharp9UnitTests : SA1003CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3974, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3974")]
+        public async Task TestTargetTypedConditionalExpressionSpacingAsync()
+        {
+            var testCode = @"
+class TestClass
+{
+    void M(bool flag)
+    {
+        object value = flag{|#0:?|} null{|#1::|}new();
+    }
+}";
+
+            var fixedCode = @"
+class TestClass
+{
+    void M(bool flag)
+    {
+        object value = flag ? null : new();
+    }
+}";
+
+            DiagnosticResult[] expected =
+            {
+                Diagnostic(DescriptorPrecededByWhitespace).WithArguments("?").WithLocation(0),
+                Diagnostic(DescriptorPrecededByWhitespace).WithArguments(":").WithLocation(1),
+                Diagnostic(DescriptorFollowedByWhitespace).WithArguments(":").WithLocation(1),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
