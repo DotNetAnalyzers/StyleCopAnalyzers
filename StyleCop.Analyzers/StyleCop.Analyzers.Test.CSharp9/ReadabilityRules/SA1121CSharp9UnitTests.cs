2579be459e18822cf506b8077ac0f22316dcc85d
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-02 17:33:55 -0600

    Update SA1121 tests for function pointers

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1121CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1121CSharp9UnitTests.cs
@@ -3,9 +3,45 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp8.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.ReadabilityRules.SA1121UseBuiltInTypeAlias,
+        StyleCop.Analyzers.ReadabilityRules.SA1121CodeFixProvider>;
 
     public partial class SA1121CSharp9UnitTests : SA1121CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3970, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3970")]
+        public async Task TestFunctionPointerParametersAsync()
+        {
+            var testCode = @"using System;
+public class TestClass
+{
+    private unsafe delegate*<{|#0:System.Int64|}, {|#1:System.Int32|}> field1;
+    private unsafe delegate*<{|#2:Int64|}, {|#3:Int32|}> field2;
+}
+";
+
+            var fixedCode = @"using System;
+public class TestClass
+{
+    private unsafe delegate*<long, int> field1;
+    private unsafe delegate*<long, int> field2;
+}
+";
+
+            var expected = new[]
+            {
+                Diagnostic().WithLocation(0),
+                Diagnostic().WithLocation(1),
+                Diagnostic().WithLocation(2),
+                Diagnostic().WithLocation(3),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
