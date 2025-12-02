516b3c529933964edb101bc297d79ef448b71ee2
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-02 10:03:33 -0600

    Update SA1501 tests for attributed local functions

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/LayoutRules/SA1501CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/LayoutRules/SA1501CSharp9UnitTests.cs
@@ -3,9 +3,48 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp8.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1501StatementMustNotBeOnASingleLine,
+        StyleCop.Analyzers.LayoutRules.SA1501CodeFixProvider>;
 
     public partial class SA1501CSharp9UnitTests : SA1501CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3978, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3978")]
+        public async Task TestLocalFunctionWithAttributeOnSingleLineAsync()
+        {
+            var testCode = @"using System;
+
+class TestClass
+{
+    void Outer()
+    {
+        [Obsolete]
+        void Local(){|#0:{|} int value = 0; }
+    }
+}
+";
+
+            var fixedCode = @"using System;
+
+class TestClass
+{
+    void Outer()
+    {
+        [Obsolete]
+        void Local()
+        {
+            int value = 0;
+        }
+    }
+}
+";
+
+            await VerifyCSharpFixAsync(testCode, Diagnostic().WithLocation(0), fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
