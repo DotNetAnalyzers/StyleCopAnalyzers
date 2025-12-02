d9999d9fd51983ded7fc05bc0c85a400cb696fd0
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-02 17:31:30 -0600

    Update SA1018 for function pointers

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/SpacingRules/SA1018CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/SpacingRules/SA1018CSharp9UnitTests.cs
@@ -3,9 +3,45 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.SpacingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp8.SpacingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.SpacingRules.SA1018NullableTypeSymbolsMustNotBePrecededBySpace,
+        StyleCop.Analyzers.SpacingRules.SA1018CodeFixProvider>;
 
     public partial class SA1018CSharp9UnitTests : SA1018CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3970, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3970")]
+        public async Task TestFunctionPointerNullableTypeSpacingAsync()
+        {
+            var testCode = @"#nullable enable
+
+public class TestClass
+{
+    private unsafe delegate*<string {|#0:?|}, void> field1;
+    private unsafe delegate*<string {|#1:?|}[], void> field2;
+}
+";
+
+            var fixedCode = @"#nullable enable
+
+public class TestClass
+{
+    private unsafe delegate*<string?, void> field1;
+    private unsafe delegate*<string?[], void> field2;
+}
+";
+
+            var expected = new[]
+            {
+                Diagnostic().WithLocation(0),
+                Diagnostic().WithLocation(1),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
