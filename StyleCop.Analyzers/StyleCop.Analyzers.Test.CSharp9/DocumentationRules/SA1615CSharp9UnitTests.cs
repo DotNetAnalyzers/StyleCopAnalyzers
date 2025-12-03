b97e8690c15e0c277fc4b85693e1b0eb7c75ba59
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-03 08:06:41 -0600

    Update SA1615 for partial methods

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/DocumentationRules/SA1615CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/DocumentationRules/SA1615CSharp9UnitTests.cs
@@ -3,9 +3,33 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.DocumentationRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp8.DocumentationRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.CustomDiagnosticVerifier<StyleCop.Analyzers.DocumentationRules.SA1615ElementReturnValueMustBeDocumented>;
 
     public partial class SA1615CSharp9UnitTests : SA1615CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3971, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3971")]
+        public async Task TestPartialMethodDeclarationMissingReturnsDocumentationAsync()
+        {
+            var testCode = @"
+/// <summary>
+/// Tests a partial method.
+/// </summary>
+public partial class TestClass
+{
+    /// <summary>Declaration.</summary>
+    public partial {|#0:int|} TestMethod(int value);
+
+    public partial int TestMethod(int value) => value;
+}";
+
+            var expected = Diagnostic().WithLocation(0);
+
+            await VerifyCSharpDiagnosticAsync(testCode, new[] { expected }, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
