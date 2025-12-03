775752dfaeb5d63fb4a807cfa618b7952597cf4f
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-03 08:48:46 -0600

    Update SA1410 for static anonymous functions

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/MaintainabilityRules/SA1410CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/MaintainabilityRules/SA1410CSharp9UnitTests.cs
@@ -3,9 +3,43 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.MaintainabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp8.MaintainabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.MaintainabilityRules.SA1410RemoveDelegateParenthesisWhenPossible,
+        StyleCop.Analyzers.MaintainabilityRules.SA1410SA1411CodeFixProvider>;
 
     public partial class SA1410CSharp9UnitTests : SA1410CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3973, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3973")]
+        public async Task TestStaticAnonymousMethodAsync()
+        {
+            var testCode = @"using System;
+
+public class TestClass
+{
+    public void TestMethod()
+    {
+        Func<int> getValue = static delegate{|#0:()|} { return 3; };
+    }
+}
+";
+
+            var fixedCode = @"using System;
+
+public class TestClass
+{
+    public void TestMethod()
+    {
+        Func<int> getValue = static delegate { return 3; };
+    }
+}
+";
+
+            await VerifyCSharpFixAsync(testCode, Diagnostic().WithLocation(0), fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
