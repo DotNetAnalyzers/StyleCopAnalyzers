cb3ab3ff542d3ab49a5fcf0eb74f0ef01b33b5d0
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-03 08:45:54 -0600

    Update SA1115 for static anonymous functions

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1115CSharp9UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp9/ReadabilityRules/SA1115CSharp9UnitTests.cs
@@ -3,9 +3,34 @@
 
 namespace StyleCop.Analyzers.Test.CSharp9.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp8.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopDiagnosticVerifier<StyleCop.Analyzers.ReadabilityRules.SA1115ParameterMustFollowComma>;
 
     public partial class SA1115CSharp9UnitTests : SA1115CSharp8UnitTests
     {
+        [Fact]
+        [WorkItem(3973, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3973")]
+        public async Task TestStaticAnonymousFunctionParameterAfterBlankLineAsync()
+        {
+            var testCode = @"using System;
+
+public class TestClass
+{
+    public void TestMethod()
+    {
+        Func<int, int, int> func = static (int first,
+
+            {|#0:int second|}) => first + second;
+    }
+}
+";
+
+            var expected = Diagnostic().WithLocation(0);
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
