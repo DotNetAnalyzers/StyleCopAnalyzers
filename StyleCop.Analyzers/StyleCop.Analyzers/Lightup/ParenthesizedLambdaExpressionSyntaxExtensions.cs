04b3264ea2b358338a3dd1804215a62f7d0bd544
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-06 10:51:00 -0600

    Update SA1141 for lambda natural types

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/Lightup/ParenthesizedLambdaExpressionSyntaxExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Lightup
+{
+    using System;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    internal static class ParenthesizedLambdaExpressionSyntaxExtensions
+    {
+        private static readonly Func<ParenthesizedLambdaExpressionSyntax, TypeSyntax> ReturnTypeAccessor;
+        private static readonly Func<ParenthesizedLambdaExpressionSyntax, TypeSyntax, ParenthesizedLambdaExpressionSyntax> WithReturnTypeAccessor;
+
+        static ParenthesizedLambdaExpressionSyntaxExtensions()
+        {
+            ReturnTypeAccessor = LightupHelpers.CreateSyntaxPropertyAccessor<ParenthesizedLambdaExpressionSyntax, TypeSyntax>(typeof(ParenthesizedLambdaExpressionSyntax), nameof(ReturnType));
+            WithReturnTypeAccessor = LightupHelpers.CreateSyntaxWithPropertyAccessor<ParenthesizedLambdaExpressionSyntax, TypeSyntax>(typeof(ParenthesizedLambdaExpressionSyntax), nameof(ReturnType));
+        }
+
+        public static TypeSyntax ReturnType(this ParenthesizedLambdaExpressionSyntax syntax)
+        {
+            return ReturnTypeAccessor(syntax);
+        }
+
+        public static ParenthesizedLambdaExpressionSyntax WithReturnType(this ParenthesizedLambdaExpressionSyntax syntax, TypeSyntax returnType)
+        {
+            return WithReturnTypeAccessor(syntax, returnType);
+        }
+    }
+}
