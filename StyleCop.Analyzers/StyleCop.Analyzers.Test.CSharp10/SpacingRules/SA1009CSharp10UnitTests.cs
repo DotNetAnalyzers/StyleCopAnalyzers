52d8043b5225c2ad447e1b6605195fab0e1b0355
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-06 10:34:16 -0600

    Update SA1009 for lambda natural types

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/SpacingRules/SA1009CSharp10UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/SpacingRules/SA1009CSharp10UnitTests.cs
@@ -3,9 +3,50 @@
 
 namespace StyleCop.Analyzers.Test.CSharp10.SpacingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp9.SpacingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.SpacingRules.SA1009ClosingParenthesisMustBeSpacedCorrectly;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.SpacingRules.SA1009ClosingParenthesisMustBeSpacedCorrectly,
+        StyleCop.Analyzers.SpacingRules.TokenSpacingCodeFixProvider>;
 
     public partial class SA1009CSharp10UnitTests : SA1009CSharp9UnitTests
     {
+        [Fact]
+        [WorkItem(3985, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3985")]
+        public async Task TestLambdaReturnTypeSpacingAsync()
+        {
+            var testCode = @"
+class TestClass
+{
+    void M()
+    {
+        var projector = (int, int{|#0:)|}(int value) => (value, value);
+    }
+}
+";
+
+            var fixedCode = @"
+class TestClass
+{
+    void M()
+    {
+        var projector = (int, int) (int value) => (value, value);
+    }
+}
+";
+
+            await new CSharpTest
+            {
+                TestCode = testCode,
+                FixedCode = fixedCode,
+                ExpectedDiagnostics =
+                {
+                    Diagnostic(DescriptorFollowed).WithLocation(0),
+                },
+            }.RunAsync(CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
